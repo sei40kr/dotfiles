8d625e1025c53f30ac344c3f810a0e8cfada9e6f
Seong Yong-ju <sei40kr@gmail.com>
2017-09-17 23:22:30 +0900

    Updated on 2017-09-17T23:22:33

--- .zsh/rc/plugins.rc.zsh
+++ .zsh/rc/plugins.rc.zsh
@@ -13,65 +13,44 @@ export ZPLUG_HOME="${HOME}/.zplug"
 # Load zplug
 source "${ZPLUG_HOME}/init.zsh"
 
-# ============ Frameworks ============
-zplug 'mafredri/zsh-async', defer:0
-
-# ============ Interactive Interfaces ============
+zplug 'djui/alias-tips'
 zplug 'junegunn/fzf', hook-build:'./install --key-bindings --completion --no-update-rc'
-zplug 'mollifier/anyframe'
-
-# ============ Command Assistants ============
-zplug 'zdharma/fast-syntax-highlighting', defer:2
-
-# ============ Command Completions ============
+zplug 'lib/clipboard', from:oh-my-zsh
 zplug 'lib/completion', from:oh-my-zsh
-zplug 'zsh-users/zsh-autosuggestions', defer:2
-zplug 'zsh-users/zsh-completions'
-zplug 'plugins/rust', from:oh-my-zsh
+zplug 'lukechilds/zsh-nvm'
+zplug 'mafredri/zsh-async', defer:0
+zplug 'mollifier/anyframe'
+zplug 'mollifier/cd-gitroot'
+zplug 'motemen/ghq', dir:"${GOPATH}/src/github.com/motemen/ghq", use:'zsh', hook-build:'go get -u'
 zplug 'plugins/cargo', from:oh-my-zsh
-zplug 'plugins/gulp', from:oh-my-zsh
-zplug 'plugins/ng', from:oh-my-zsh
-zplug 'plugins/react-native', from:oh-my-zsh
-zplug 'plugins/gradle', from:oh-my-zsh
-zplug 'plugins/spring', from:oh-my-zsh
-zplug 'plugins/pip', from:oh-my-zsh
-zplug 'plugins/gem', from:oh-my-zsh
-zplug 'plugins/rake-fast', from:oh-my-zsh
+zplug 'plugins/codeclimate', from:oh-my-zsh
+zplug 'plugins/command-not-found', from:oh-my-zsh
 zplug 'plugins/docker', from:oh-my-zsh
 zplug 'plugins/docker-compose', from:oh-my-zsh
-zplug 'plugins/kubectl', from:oh-my-zsh
-zplug 'plugins/codeclimate', from:oh-my-zsh
-
-# Load zsh configuration
-zplug 'plugins/fancy-ctrl-z', from:oh-my-zsh
-zplug 'plugins/command-not-found', from:oh-my-zsh
-
-# Use aliases
-zplug 'lib/clipboard', from:oh-my-zsh
-zplug 'plugins/ubuntu', from:oh-my-zsh, if:'uname -a | grep -q ubuntu'
 zplug 'plugins/extract', from:oh-my-zsh
-zplug 'plugins/zsh_reload', from:oh-my-zsh
-
-zplug 'plugins/pyenv', from:oh-my-zsh
-zplug 'plugins/rbenv', from:oh-my-zsh
-zplug 'lukechilds/zsh-nvm'
-
-# ============= Version Control ============
-zplug 'plugins/gitfast', from:oh-my-zsh
+zplug 'plugins/fancy-ctrl-z', from:oh-my-zsh
+zplug 'plugins/gem', from:oh-my-zsh
 zplug 'plugins/git-flow', from:oh-my-zsh
-zplug 'motemen/ghq', dir:"${GOPATH}/src/github.com/motemen/ghq", use:'zsh', hook-build:'go get -u'
-zplug 'mollifier/cd-gitroot'
-# Use gitignore.io CLI, gitignore
+zplug 'plugins/gitfast', from:oh-my-zsh
 zplug 'plugins/gitignore', from:oh-my-zsh
-# Use a workspace manager, ghq
-# Use command to go to root directory of current repo
-
-# ============ Appearance ============
-zplug 'sindresorhus/pure', use:pure.zsh, as:theme
+zplug 'plugins/gradle', from:oh-my-zsh
+zplug 'plugins/gulp', from:oh-my-zsh
+zplug 'plugins/kubectl', from:oh-my-zsh
+zplug 'plugins/ng', from:oh-my-zsh
+zplug 'plugins/pip', from:oh-my-zsh
+zplug 'plugins/pyenv', from:oh-my-zsh
+zplug 'plugins/rake-fast', from:oh-my-zsh
+zplug 'plugins/rbenv', from:oh-my-zsh
+zplug 'plugins/react-native', from:oh-my-zsh
+zplug 'plugins/rust', from:oh-my-zsh
+zplug 'plugins/spring', from:oh-my-zsh
+zplug 'plugins/ubuntu', from:oh-my-zsh, if:'uname -a | grep -q ubuntu'
+zplug 'plugins/zsh_reload', from:oh-my-zsh
 zplug 'sei40kr/dircolors-solarized', use:'*.ansi-dark.zsh'
-
-# Use alias tips
-zplug 'djui/alias-tips'
+zplug 'sindresorhus/pure', use:pure.zsh, as:theme
+zplug 'zdharma/fast-syntax-highlighting', defer:2
+zplug 'zsh-users/zsh-autosuggestions', defer:2
+zplug 'zsh-users/zsh-completions'
 
 zplug '~/dev/ws/github.com/sei40kr/zsh-tmux-rename', from:local
 
