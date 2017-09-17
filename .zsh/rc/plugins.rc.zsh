2258a1623fa57669cad12661909ddf3f822b9db0
Seong Yong-ju <sei40kr@gmail.com>
2017-09-17 23:57:14 +0900

    Updated on 2017-09-17T23:57:19

--- .zsh/rc/plugins.rc.zsh
+++ .zsh/rc/plugins.rc.zsh
@@ -13,12 +13,12 @@ export ZPLUG_HOME="${HOME}/.zplug"
 # Load zplug
 source "${ZPLUG_HOME}/init.zsh"
 
+zplug 'denysdovhan/spaceship-zsh-theme', use:spaceship.zsh, as:theme
 zplug 'djui/alias-tips'
 zplug 'junegunn/fzf', hook-build:'./install --key-bindings --completion --no-update-rc'
 zplug 'lib/clipboard', from:oh-my-zsh
 zplug 'lib/completion', from:oh-my-zsh
 zplug 'lukechilds/zsh-nvm'
-zplug 'mafredri/zsh-async', defer:0
 zplug 'mollifier/anyframe'
 zplug 'mollifier/cd-gitroot'
 zplug 'motemen/ghq', dir:"${GOPATH}/src/github.com/motemen/ghq", use:'zsh', hook-build:'go get -u'
@@ -47,7 +47,7 @@ zplug 'plugins/spring', from:oh-my-zsh
 zplug 'plugins/ubuntu', from:oh-my-zsh, if:'uname -a | grep -q ubuntu'
 zplug 'plugins/zsh_reload', from:oh-my-zsh
 zplug 'sei40kr/dircolors-solarized', use:'*.ansi-dark.zsh'
-zplug 'sindresorhus/pure', use:pure.zsh, as:theme
+zplug 'supercrabtree/k'
 zplug 'zdharma/fast-syntax-highlighting', defer:2
 zplug 'zsh-users/zsh-autosuggestions', defer:2
 zplug 'zsh-users/zsh-completions'
