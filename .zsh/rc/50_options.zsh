990bfc9d0f7de2c2515d38ea660b05db9619c84a
Seong Yong-ju <sei40kr@gmail.com>
2017-11-08 12:03:46 +0900

    Updated on 2017-11-08T12:03:52

--- .zsh/rc/50_options.zsh
+++ .zsh/rc/50_options.zsh
@@ -8,6 +8,11 @@ setopt auto_cd
 setopt auto_list
 setopt auto_menu
 setopt auto_pushd
+setopt auto_param_keys
+setopt auto_param_slash
+setopt auto_resume
+setopt complete_aliases
+setopt equals
 setopt extended_history
 setopt glob_dots
 setopt hist_ignore_all_dups
@@ -15,6 +20,7 @@ setopt hist_ignore_space
 setopt hist_reduce_blanks
 setopt interactive_comments
 setopt no_beep
+setopt numeric_glob_sort
 setopt print_eight_bit
 setopt prompt_subst
 setopt pushd_ignore_dups
