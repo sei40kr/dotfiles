2903b4bf7ddc27fb5155049460380e929cd5ed16
Seong Yong-ju <sei40kr@gmail.com>
2017-11-14 23:19:02 +0900

    Updated on 2017-11-14T23:19:08

--- .zsh/rc/30_aliases.zsh
+++ .zsh/rc/30_aliases.zsh
@@ -66,6 +66,10 @@ alias gemb='gem build *.gemspec'
 alias gemp='gem push *.gem'
 # }}}
 
+# git {{{
+alias git='env TERM=xterm git'
+# }}}
+
 # golang {{{
 alias gob='go build'
 alias goc='go clean'
--- .zsh/rc/80_custom.zsh
+++ .zsh/rc/80_custom.zsh
@@ -3,6 +3,23 @@
 # 80_custom.zsh
 # author: Seong Yong-ju ( @sei40kr )
 
+# robbyrussell/oh-my-zsh/colored-man-pages {{{
+man() {
+  command env \
+    TERM=xterm \
+    LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+    LESS_TERMCAP_md=$(printf "\e[1;31m") \
+    LESS_TERMCAP_me=$(printf "\e[0m") \
+    LESS_TERMCAP_se=$(printf "\e[0m") \
+    LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+    LESS_TERMCAP_ue=$(printf "\e[0m") \
+    LESS_TERMCAP_us=$(printf "\e[1;32m") \
+    PAGER="${commands[less]:-$PAGER}" \
+    _NROFF_U=1 \
+    man "$@"
+}
+# }}}
+
 # mollifier/anyframe {{{
 anyframe-widget-cd-ghq-repository() {
   anyframe-source-ghq-repository | \
