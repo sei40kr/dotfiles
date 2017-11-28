99167b518da0c4958ca44500b410f2a3ff9a563a
Seong Yong-ju <sei40kr@gmail.com>
2017-11-28 23:54:55 +0900

    Updated on 2017-11-28T23:55:00

--- .zsh/rc/70_misc.zsh
+++ .zsh/rc/70_misc.zsh
@@ -6,7 +6,6 @@
 # junegunn/fzf {{{
 FZF_DEFAULT_COMMAND='rg --files --hidden --follow --iglob "!.git/**"'
 FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
-FZF_ALT_C_COMMAND='bfs -nocolor -hidden -type d -O4 | sed "s/\.\///"'
 
 if [[ -n "$TMUX" ]]
 then
