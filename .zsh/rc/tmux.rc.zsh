998a1dd87ea6e32e493e4a5346ec8213ae73de4d
Seong Yong-ju <sei40kr@gmail.com>
2017-12-10 17:40:44 +0900

    Use screen instead if tmux doesn't exist

--- .zsh/rc/tmux.rc.zsh
+++ .zsh/rc/tmux.rc.zsh
@@ -11,6 +11,10 @@ then
   tmux has-session -t global 2>/dev/null || tmux new-session -ds global \
       && tmux attach-session -t global
   exit
+elif [[ "${commands[screen]}" == 1 ]]
+then
+  screen -rx || screen -D -RR
+  exit
 fi
 
 # vi: et sw=2 cc=80
