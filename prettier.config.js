30925038972f1ecf14c87845c110e8746360912f
Seong Yong-ju <sei40kr@gmail.com>
2017-12-29 22:16:12 +0900

    Add Prettier configuration

---
+++ prettier.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  // Print spaces between brackets in object literals.
+  bracketSpacing: true,
+
+  // Put the > of a multi-line JSX element at the end of the last line instead of being alone on the next line.",
+  jsxBracketSameLine: false,
+
+  // Specify the line length that the printer will wrap on.
+  printWidth: 80,
+
+  // Print semicolons at the ends of statements.
+  semi: true,
+
+  // Use single quotes instead of double quotes.
+  singleQuote: true,
+
+  // Specify the number of spaces per indentation-level.
+  tabWidth: 2,
+
+  // Print trailing commas wherever possible.
+  trailingComma: 'es5',
+
+  // Indent lines with tabs instead of spaces.
+  useTabs: false,
+};
