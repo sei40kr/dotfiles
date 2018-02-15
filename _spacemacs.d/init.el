678a5a16d42490a3ff5efb262b3eeca0b7bd8343
Seong Yong-ju <sei40kr@gmail.com>
2018-02-15 10:56:21 +0900

    Update Spacemacs config
    Stop loading spacemacs-light. Use italic font for comments.

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -188,8 +188,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(spacemacs-dark
-                         spacemacs-light)
+   dotspacemacs-themes '(spacemacs-dark)
    ;; If non nil the cursor color matches the state color in GUI Emacs.
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
@@ -362,6 +361,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (custom-set-variables '(spacemacs-theme-comment-italic t))
   )
 
 (defun dotspacemacs/user-config ()
