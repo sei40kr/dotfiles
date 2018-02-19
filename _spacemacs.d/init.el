58125fa46c00efc1913d614b93170bcf9b44d3ba
Seong Yong-ju <sei40kr@gmail.com>
2018-02-19 21:47:06 +0900

    Update Spacemacs config
    Keep Emacs server running when "SPC q q" pressed.

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -366,6 +366,7 @@ executes.
  This function is mostly useful for variables that need to be set
 before packages are loaded. If you are unsure, you should try in setting them in
 `dotspacemacs/user-config' first."
+  (evil-leader/set-key "q q" 'spacemacs/frame-killer)
   (custom-set-variables '(spacemacs-theme-comment-italic t))
   )
 
