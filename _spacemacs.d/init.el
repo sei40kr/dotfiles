653bd16141d95b687a78c0d84f1d16caa3d4aada
Seong Yong-ju <sei40kr@gmail.com>
2018-02-28 17:08:30 +0900

    Update Spacemacs config

--- _spacemacs.d/init.el
+++ _spacemacs.d/init.el
@@ -371,16 +371,16 @@ values."
 
     ;; If non nil the frame is fullscreen when Emacs starts up. (default nil)
     ;; (Emacs 24.4+ only)
-    dotspacemacs-fullscreen-at-startup t
+    dotspacemacs-fullscreen-at-startup nil
 
     ;; If non nil `spacemacs/toggle-fullscreen' will not use native fullscreen.
     ;; Use to disable fullscreen animations in OSX. (default nil)
-    dotspacemacs-fullscreen-use-non-native t
+    dotspacemacs-fullscreen-use-non-native nil
 
     ;; If non nil the frame is maximized when Emacs starts up.
     ;; Takes effect only if `dotspacemacs-fullscreen-at-startup' is nil.
     ;; (default nil) (Emacs 24.4+ only)
-    dotspacemacs-maximized-at-startup nil
+    dotspacemacs-maximized-at-startup t
 
     ;; A value from the range (0..100), in increasing opacity, which describes
     ;; the transparency level of a frame when it's active or selected.
