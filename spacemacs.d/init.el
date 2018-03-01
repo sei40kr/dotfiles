6933afac8a6117038014c79b09c67940a1413d8a
Seong Yong-ju <sei40kr@gmail.com>
2018-03-02 02:30:39 +0900

    Update Spacemacs config
    Update search tools: pt -> ag, grep.

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -450,7 +450,7 @@ values."
     ;; List of search tool executable names. Spacemacs uses the first installed
     ;; tool of the list. Supported tools are `ag', `pt', `ack' and `grep'.
     ;; (default '("ag" "pt" "ack" "grep"))
-    dotspacemacs-search-tools '("pt")
+    dotspacemacs-search-tools '("ag" "grep")
 
     ;; Format specification for setting the frame title.
     ;; %a - the `abbreviated-file-name', or `buffer-name'
