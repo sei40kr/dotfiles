78ac6e9646cb282a5c8226604de5499cc457150d
Seong Yong-ju <sei40kr@gmail.com>
2018-03-02 00:19:59 +0900

    Update ZSH config

--- zsh/rc/exports.rc.zsh
+++ zsh/rc/exports.rc.zsh
@@ -1,108 +1,27 @@
-#!/usr/bin/env zsh
-
-# exports.rc.zsh
-# author: Seong Yong-ju ( @sei40kr )
-
 export TERM='xterm-256color-italic'
 
-typeset -U path
-typeset -U fpath
-typeset -U manpath
-typeset -U infopath
-
-# Homebrew/Linuxbrew
-# ------------------
-
-if [[ -d "/home/linuxbrew/.linuxbrew" ]]
-then
-  BREW_PREFIX="/home/linuxbrew/.linuxbrew"
-elif [[ -d "${HOME}/.linuxbrew" ]]
-then
-  BREW_PREFIX="${HOME}/.linuxbrew"
-elif [[ -x '/usr/local/bin/brew' ]]
-then
-  BREW_PREFIX='/usr/local'
+# llvm
+if [[ -d "${BREW_PREFIX}/opt/llvm" ]]; then
+    export LD_LIBRARY_PATH="${BREW_PREFIX}/opt/llvm/lib"
 fi
-
-if [[ -n "$BREW_PREFIX" ]]
-then
-  path=(
-    "${BREW_PREFIX}/bin"
-    "${BREW_PREFIX}/sbin"
-    "${path[@]}"
-  )
-  manpath=(
-    "${BREW_PREFIX}/share/man:${MANPATH}"
-    "${manpath[@]}"
-  )
-  infopath=(
-    "${BREW_PREFIX}/share/info:${INFOPATH}"
-    "${infopath[@]}"
-  )
-
-  XDG_DATA_DIRS="${BREW_PREFIX}/share:${XDG_DATA_DIRS}"
-fi
-
-# If LLVM was installed via Homebrew
-if [[ -d "${BREW_PREFIX}/opt/llvm" ]]
-then
-  path=(
-    "${BREW_PREFIX}/opt/llvm/bin"
-    "${path[@]}"
-  )
-  LD_LIBRARY_PATH="${BREW_PREFIX}/opt/llvm/lib"
-fi
-
-
-# Golang
-# ------
-
-if [[ -d "${HOME}/.go" ]]
-then
-  GOPATH="${HOME}/.go"
-  path=( "${GOPATH}/bin" "${path[@]}" )
-fi
-
-path=(
-  "${HOME}/.local/bin"(N-/)
-  "${HOME}/.cabal/bin"(N-/)
-  "${HOME}/.cargo/bin"(N-/)
-  "${path[@]}"
-)
-
 
 # ghq
-# ---
 if [[ "$OSTYPE" == darwin* ]]; then
-  GHQ_ROOT="${HOME}/Develop"
+    GHQ_ROOT="${HOME}/Develop"
 else
-  GHQ_ROOT="${HOME}/dev/ws"
+    GHQ_ROOT="${HOME}/dev/ws"
 fi
-
-
-# Misc
-# ----
-
-if [[ -d "${HOME}/.zsh" ]]
-then
-  fpath=(
-    "${HOME}/.zsh/completions"
-    "${HOME}/.zsh/functions"
-    "${fpath[@]}"
-  )
-fi
-
-XDG_CONFIG_HOME="${HOME}/.config"
-
-export PATH
-export FPATH
-export MANPATH
-export INFOPATH
-export GOPATH
-export BREW_PREFIX
-export LD_LIBRARY_PATH
-export XDG_CONFIG_HOME
-export XDG_DATA_DIRS
 export GHQ_ROOT
 
-# vi: et sw=2 cc=80
+# xdg-*
+export XDG_CONFIG_HOME="${HOME}/.config"
+if [[ "$BREW_PREFIX" != "" ]]; then
+    export XDG_DATA_DIRS="${BREW_PREFIX}/share:${XDG_DATA_DIRS}"
+fi
+
+typeset -U fpath
+fpath=(
+    "${ZDOTDIR}/completions"
+    "${ZDOTDIR}/functions"
+    "${fpath[@]}"
+)
--- zshenv
+++ zshenv
@@ -1,3 +1,59 @@
-export EDITOR='emacs'
+export EDITOR='emacsclient'
+export ZDOTDIR="${HOME}/.zsh"
 
-# vi: ft=zsh et sw=2 cc=80
+# Homebrew/Linuxbrew
+if [[ "$OSTYPE" == darwin* ]]; then
+    BREW_PREFIX="${HOME}/.linuxbrew"
+else
+    BREW_PREFIX='/usr/local'
+fi
+typeset -U path
+typeset -U manpath
+typeset -U infopath
+if [[ "$BREW_PREFIX" != "" ]]; then
+    path=(
+        "${BREW_PREFIX}/bin"
+        "${BREW_PREFIX}/sbin"
+        "${BREW_PREFIX}/opt/llvm/bin"
+        "${path[@]}"
+    )
+    manpath=(
+        "${BREW_PREFIX}/share/man"
+        "${manpath[@]}"
+    )
+    infopath=(
+        "${BREW_PREFIX}/share/info"
+        "${infopath[@]}"
+    )
+fi
+
+ASDF_DIR="${HOME}/.asdf"
+GOPATH="${HOME}/.go"
+SDKMAN_DIR="${HOME}/.sdkman"
+PYENV_ROOT="${HOME}/.pyenv"
+
+path=(
+    "${HOME}/.local/bin"
+    # asdf
+    "${ASDF_DIR}/bin"
+    # Cargo
+    "${HOME}/.cargo/bin"
+    # Cabal
+    "${HOME}/.cabal/bin"
+    # Go
+    "${GOPATH}/bin"
+    # SDKMAN!
+    "${SDKMAN_DIR}/candidates/ant/current/bin"
+    "${SDKMAN_DIR}/candidates/gradle/current/bin"
+    "${SDKMAN_DIR}/candidates/java/current/bin"
+    "${SDKMAN_DIR}/candidates/kotlin/current/bin"
+    "${SDKMAN_DIR}/candidates/maven/current/bin"
+    # pyenv
+    "${PYENV_ROOT}/bin"
+    "${PYENV_ROOT}/shims"
+    "${path[@]}"
+)
+
+export PATH
+export MANPATH
+export INFOPATH
