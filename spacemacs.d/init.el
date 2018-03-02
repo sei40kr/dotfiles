7558f7a78b6d4d9d5f8db1fb0e77f2563cc8f8c8
Seong Yong-ju <sei40kr@gmail.com>
2018-03-02 13:51:17 +0900

    Update Spacemacs config

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -143,7 +143,7 @@ values."
     ;; wrapped in a layer. If you need some configuration for these
     ;; packages, then consider creating a layer. You can also put the
     ;; configuration in `dotspacemacs/user-config'.
-    dotspacemacs-additional-packages '(evil-terminal-cursor-changer)
+    dotspacemacs-additional-packages '()
 
     ;; A list of packages that cannot be updated.
     dotspacemacs-frozen-packages '()
