88123ebaf88a6b12524ce0fe694fefe239e5d279
Seong Yong-ju <sei40kr@gmail.com>
2018-03-06 05:46:41 +0900

    Add Spacemacs layer
    Add new layer wsp.

---
+++ spacemacs.d/layers/wsp/packages.el
@@ -0,0 +1,14 @@
+;;; packages.el - wsp layer file for Spacemacs
+;;
+;; Copyright (c) 2018 Seong Yong-ju
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;; URL: https://github.com/sei40kr/dotfiles
+;;
+;; This file is not part of GNU Emacs
+;;
+;;; License: MIT
+
+(setq wsp-packages '())
+
+(setq wsp-excluded-packages '())
