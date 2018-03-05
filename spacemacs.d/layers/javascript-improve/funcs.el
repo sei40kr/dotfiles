186026c7472066c6cc5cb218b75344c511ebe4c7
Seong Yong-ju <sei40kr@gmail.com>
2018-03-06 02:55:41 +0900

    Update Spacemacs layer
    Update javascript-improve layer.

---
+++ spacemacs.d/layers/javascript-improve/funcs.el
@@ -0,0 +1,16 @@
+;;; funcs.el - javascript-improve layer file for Spacemacs
+;;
+;; Copyright (c) 2018 Seong Yong-ju
+;;
+;; Author: Seong Yong-ju <sei40kr@gmail.com>
+;;
+;; This file is not part of GNU Emacs
+;;
+;;; License: MIT
+
+(defun spacemacs//import-js-detect ()
+  "Detect importjsd binary and warn if not found."
+  (let ((found (executable-find "importjsd")))
+    (unless found
+      (spacemacs-buffer/warning "importjsd binary not found!"))
+    found))
--- spacemacs.d/layers/javascript-improve/packages.el
+++ spacemacs.d/layers/javascript-improve/packages.el
@@ -8,6 +8,27 @@
 ;;
 ;;; License: MIT
 
-(setq javascript-improve-packages '())
+(setq javascript-improve-packages
+      '(
+        (import-js :toggle (spacemacs//import-js-detect))
+        js2-mode))
 
 (setq javascript-improve-excluded-packages '(company-tern tern web-beautify))
+
+(defun javascript-improve/init-import-js ()
+  (use-package import-js
+    :defer t
+    :init
+    (progn
+      (spacemacs/declare-prefix-for-mode 'js2-mode "mi" "import")
+      (spacemacs/set-leader-keys-for-major-mode 'js2-mode
+        "if" 'import-js-fix
+        "ii" 'import-js-import
+        "ig" 'import-js-goto))))
+
+(defun javascript-improve/pre-init-js2-mode ()
+  (setq-default
+   js-indent-level 2
+   js2-basic-offset 2
+   js2-strict-missing-semi-warning nil
+   js2-strict-trailing-comma-warning nil))
