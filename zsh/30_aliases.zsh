fb9f05d41cb296aa5e9deaef2a531c85fdd83ab1
Seong Yong-ju <sei40kr@gmail.com>
2018-03-16 03:34:33 +0900

    Update ZSH alias
    Add the aliases of Emacs to use daemon feature fully.

--- zsh/30_aliases.zsh
+++ zsh/30_aliases.zsh
@@ -1,2 +1,21 @@
-alias e='emacs -nw';
 alias u='builtin cd ..'
+
+# Emacs
+# cf https://github.com/robbyrussell/oh-my-zsh/blob/master/plugins/emacs/emacs.plugin.zsh
+EMACS_PLUGIN_LAUNCHER="${ZDOTDIR}/plugins/emacs/emacsclient.sh"
+alias emacs='${EMACS_PLUGIN_LAUNCHER} -n'
+alias e=emacs
+# open terminal ${EMACS_PLUGIN_LAUNCHER}
+alias te='${EMACS_PLUGIN_LAUNCHER} -nw'
+# same than M-x eval but from outside Emacs.
+alias eeval='${EMACS_PLUGIN_LAUNCHER} -e'
+# create a new X frame
+alias eframe='emacsclient -a "" -c'
+
+efile() {
+  # TODO: Implement this
+}
+
+ecd() {
+  # TODO: Implement this
+}
