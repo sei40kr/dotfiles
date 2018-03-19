099ed6084e91dfcfdfa094db8fb95255f852f613
Seong Yong-ju <sei40kr@gmail.com>
2018-03-20 02:32:19 +0900

    Update XMonad config

--- tag-linux/xmonad/xmonad.hs
+++ tag-linux/xmonad/xmonad.hs
@@ -6,9 +6,18 @@ myBorderWidth = 2
 myTerminal :: String
 myTerminal = "urxvt"
 
+myLayoutHook :: Choose Tall (Choose (Mirror Tall) Full) a
+myLayoutHook = tiled ||| Mirror tiled ||| Full
+  where
+    tiled = Tall nmaster delta ratio
+    nmaster = 1
+    ratio = toRational (2/(1+sqrt(5)::Double))
+    delta = 3 / 100
+
 main :: IO ()
 main = do
   xmonad $ def {
     borderWidth = myBorderWidth
     , terminal = myTerminal
+    , layoutHook = myLayoutHook
   }
