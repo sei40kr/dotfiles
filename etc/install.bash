715851cef379cdc184ebe8eab92e03970334b929
Seong Yong-ju <sei40kr@gmail.com>
2018-03-25 02:43:29 +0900

    Update installerl script

--- etc/install.bash
+++ etc/install.bash
@@ -3,25 +3,21 @@
 # install.bash
 # Seong Yong-ju <sei40kr@gmail.com>
 
-DOTFILES_DIR="${HOME}/dotfiles"
+set -euo pipefail
+IFS=$'\n\t'
 
-if ! command -v git 1>/dev/null; then
-  echo 'Error: This installer requires Git command.' 1>&2
-  exit 1
-fi
+# shellcheck source=setup/helpers/init-helpers.bash
+. "$(dirname "${BASH_SOURCE[0]}")/setup/helpers/init-helpers.bash"
 
-if [[ ! -d "${DOTFILES_DIR}" ]]; then
-  echo 'Info: dotfiles is not installed. Cloning the repo from remote.'
-  git clone git@github.com:sei40kr/dotfiles.git "${DOTFILES_DIR}"
-fi
 
-cd "$DOTFILES_DIR"
+current_dir="$(dirname "${BASH_SOURCE[0]}")"
 
-find "${DOTFILES_DIR}/etc/setup" \
+find "${current_dir}/setup" \
      -mindepth 1 \
      -maxdepth 1 \
      -type f \
      -name '*.bash' \
+  | sort \
   | while read -r script_path; do
   if [[ -x "$script_path" ]]; then
     bash "${script_path}"
