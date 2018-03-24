aad060335947a3daa91e002d832f728f374bf4d0
Seong Yong-ju <sei40kr@gmail.com>
2018-03-25 01:50:37 +0900

    Add setup scripts
    Add setup scripts for ndenv, pyenv, rbenv.

---
+++ etc/setup/401_version-manager-anyenv-ndenv.bash
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+
+# 401_version-manager-anyenv-ndenv.bash - ndenv installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+set -euo pipefail
+IFS=$'\n\t'
+
+
+## Check dependencies
+
+if [[ ! -x "${HOME}/.anyenv/bin/anyenv" ]]; then
+  echo 'Error: ndenv installer requires anyenv.' 1>&2
+  exit 1
+fi
+
+# Initialize anyenv.
+eval "$("${HOME}/.anyenv/bin/anyenv" init - bash)"
+
+
+## Install ndenv
+
+echo 'Info: Installing ndenv.'
+anyenv install -s ndenv
+
+# Initialize ndenv
+eval "$("${ANYENV_ROOT}/envs/ndenv/bin/ndenv" init - bash)"
+
+
+## Install the stable version of Node
+
+node_version='v8.10.0'
+
+echo "Info: Installing Node ${node_version}"
+ndenv install -s "$node_version"
+
+# Set the installed version of Node as default
+ndenv global "${node_version}"
+
+
+## Install NPM packages
+
+npm_packages=(
+  create-react-app
+  create-react-native-app
+  eslint-cli
+  eslint_d
+  flow-bin
+  flow-language-server
+  gatsby-cli
+  generate
+  generate-gitignore
+  generate-license
+  generate-project
+  gulp
+  import-js
+  jsforce
+  markdownlint-cli
+  prettier
+  prettier-eslint-cli
+  sfdx-cli
+  stylelint-cli
+  typescript
+  typescript-language-server
+  vmd
+  webpack
+)
+
+echo 'Info: Installing NPM packages'
+
+# Use yarn command if it's available. Add --global-folder option to tell yarn
+# destination directory.
+# cf https://github.com/yarnpkg/yarn/issues/1027
+if command -v yarn 2>/dev/null; then
+  yarn global add "${npm_packages[@]}" --global-folder="$(yarn global bin)"
+else
+  npm install -g "${npm_packages[@]}"
+fi
+
+ndenv rehash
---
+++ etc/setup/402_version-manager-anyenv-pyenv.bash
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+# 402_version-manager-anyenv-pyenv.bash - pyenv installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+set -euo pipefail
+IFS=$'\n\t'
+
+
+## Check dependencies
+
+if [[ ! -x "${HOME}/.anyenv/bin/anyenv" ]]; then
+  echo 'Error: pyenv installer requires anyenv.' 1>&2
+  exit 1
+fi
+
+# Initialize anyenv.
+eval "$("${HOME}/.anyenv/bin/anyenv" init - bash)"
+
+
+## Install pyenv
+
+echo 'Info: Installing pyenv.'
+anyenv install -s pyenv
+
+# Initialize pyenv
+eval "$("${ANYENV_ROOT}/envs/pyenv/bin/pyenv" init - bash)"
+
+
+## Install the stable versions of Python2/3
+
+py2_version='2.7.14'
+py3_version='3.6.4'
+
+echo "Info: Installing Python v${py2_version}"
+pyenv install -s "$py2_version"
+
+echo "Info: Installing Python v${py3_version}"
+pyenv install -s "$py3_version"
+
+# Set the installed versions of Python2/3 as default
+pyenv global "${py2_version}" "${py3_version}"
+
+
+## Install Python packages
+
+pip2_packages=(
+  ansible-lint
+)
+pip3_packages=(
+  asciinema
+  autopep8
+  aws-shell
+  cmakelint
+  cpplint
+  flake8
+  jedi
+  jupyter
+  mycli
+  pgcli
+  proselint
+  ptpython
+  pygments
+  pylint
+  python-language-server
+  vim-vint
+  yamllint
+  yapf
+)
+
+# Install Python2 packages
+echo 'Info: Install Python2 packages'
+pip2 install "${pip2_packages[@]}"
+
+# Install Python3 packages
+echo 'Info: Install Python3 packages'
+pip3 install "${pip3_packages[@]}"
+
+pyenv rehash
