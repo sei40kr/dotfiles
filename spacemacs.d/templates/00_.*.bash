4bc63d661d8d11ccd15a5f4a6642aa9dc7b46132
Seong Yong-ju <sei40kr@gmail.com>
2018-03-25 01:17:23 +0900

    Update file template
    Update file template for Bash script.

--- spacemacs.d/templates/00_.*.bash
+++ spacemacs.d/templates/00_.*.bash
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# `(file-name-nondirectory (buffer-file-name))`
+# `(file-name-nondirectory (buffer-file-name))` - $1
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 set -euo pipefail
 IFS=$'\n\t'
+
+$0
