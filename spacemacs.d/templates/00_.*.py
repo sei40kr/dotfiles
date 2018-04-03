4cec9a7257a35331c49da8e5e6458dedae430a11
Seong Yong-ju <sei40kr@gmail.com>
2018-04-03 17:01:12 +0900

    Add some file templates of Spacemacs
    Add the file templates of Python, Ruby.

---
+++ spacemacs.d/templates/00_.*.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+# `(file-name-nondirectory (buffer-file-name))` --- $0
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+import sys
+
+
+def main(arg):
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
