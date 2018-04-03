d10062549d0995f78702f2c93ca7a17bda7596fe
Seong Yong-ju <sei40kr@gmail.com>
2018-04-03 18:05:33 +0900

    Update a file template of Spacemacs
    Update the file template for Bash.

--- spacemacs.d/templates/00_.*.bash
+++ spacemacs.d/templates/00_.*.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# `(file-name-nondirectory (buffer-file-name))` - $0
+# `(file-name-nondirectory (buffer-file-name))` --- $0
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 set -euo pipefail
