fe7f9ca7c4243d3859c3036ab85e9699e2faf61e
Seong Yong-ju <sei40kr@gmail.com>
2018-04-03 17:20:12 +0900

    Update a file template of Spacemacs
    Update the file template of Bash. Remove some placeholders of caret, because it
    only works for "$0" in yatemplate.

--- spacemacs.d/templates/00_.*.bash
+++ spacemacs.d/templates/00_.*.bash
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
-# `(file-name-nondirectory (buffer-file-name))` - $1
+# `(file-name-nondirectory (buffer-file-name))` - $0
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 set -euo pipefail
 IFS=$'\n\t'
-
-$0
