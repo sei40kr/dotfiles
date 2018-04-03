09fe30e2b8e5bbcb32e341f393ca590043ff4e4f
Seong Yong-ju <sei40kr@gmail.com>
2018-04-04 04:54:25 +0900

    Add a file template of Spacemacs
    Add the file template of C++.

---
+++ spacemacs.d/templates/00_.*.cpp
@@ -0,0 +1,9 @@
+#include <string>
+#include <iostream>
+
+// `(file-name-nondirectory (buffer-file-name))` --- $0
+// author: Seong Yong-ju <sei40kr@gmail.com>
+
+int main(int argc, char *argv[]) {
+  return 0;
+}
