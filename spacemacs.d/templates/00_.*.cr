5f3004bc1a37e8585817e1619179d1cbdfa6a8d9
Seong Yong-ju <sei40kr@gmail.com>
2018-04-03 18:04:51 +0900

    Add some file templates of Spacemacs
    Add the file templates of Crystal, ZSH.

---
+++ spacemacs.d/templates/00_.*.cr
@@ -0,0 +1,2 @@
+# `(file-name-nondirectory (buffer-file-name))` --- $0
+# author: Seong Yong-ju <sei40kr@gmail.com>
---
+++ spacemacs.d/templates/00_.*.zsh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+
+# `(file-name-nondirectory (buffer-file-name))` --- $0
+# author: Seong Yong-ju <sei40kr@gmail.com>
