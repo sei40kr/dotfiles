621472f325fffa9b04a220a3c924b7b40aff3ca4
Seong Yong-ju <sei40kr@gmail.com>
2018-04-05 18:30:48 +0900

    Update a file template for Spacemacs
    Update the file template of Python.

--- spacemacs.d/templates/00_.*.py
+++ spacemacs.d/templates/00_.*.py
@@ -1,13 +1,19 @@
 #!/usr/bin/env python
 # coding: utf-8
-
-# `(file-name-nondirectory (buffer-file-name))`
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
 import sys
 
+"""
+`(file-name-nondirectory (buffer-file-name))` --- $0
+"""
 
-def main(arg):
+__author__ = 'Seong Yong-ju'
+__copyright__ = 'Copyright (c) 2018 Seong Yong-ju.'
+__maintainer__ = 'Seong Yong-ju'
+__email__ = 'sei40kr@gmail.com'
 
-if __name__ == '__main__':
-  sys.exit(main(sys.argv[1:]))
+
+def main():
+    """
+    """
+    if __name__ == '__main__':
+        sys.exit(main(sys.argv[1:]))
