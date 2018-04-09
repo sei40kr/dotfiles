e83cf7ca432d1921508a379123d2eaa199410021
Seong Yong-ju <sei40kr@gmail.com>
2018-04-10 03:25:52 +0900

    Update a file template for Spacemacs
    Update the file template of Perl.

--- spacemacs/spacemacs.d/templates/00_.*.pl
+++ spacemacs/spacemacs.d/templates/00_.*.pl
@@ -1,7 +1,11 @@
 #!/usr/bin/env perl
 # -*- mode: perl -*-
+use utf8;
 use strict;
 use warnings;
 
 # `(file-name-nondirectory (buffer-file-name))` --- $0
 # author: Seong Yong-ju <sei40kr@gmail.com>
+
+# Output as UTF-8
+binmode STDOUT, ':utf8'
