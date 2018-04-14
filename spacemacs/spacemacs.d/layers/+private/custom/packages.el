9fa68b6f7ad2ea41695767450ad7ad3df860d1d7
Seong Yong-ju <sei40kr@gmail.com>
2018-04-15 01:12:39 +0900

    Remove redundant setting in spacemacs custom layer

--- spacemacs/spacemacs.d/layers/+private/custom/packages.el
+++ spacemacs/spacemacs.d/layers/+private/custom/packages.el
@@ -57,8 +57,6 @@
   (with-eval-after-load 'evil
     (evil-global-set-key 'normal (kbd "C-s") 'save-buffer)
     (evil-global-set-key 'insert (kbd "C-h") 'evil-delete-backward-char)
-    (define-key minibuffer-local-map (kbd "C-a") 'move-beginning-of-line)
-    (define-key minibuffer-local-map (kbd "C-e") 'move-end-of-line)
     (define-key minibuffer-local-map (kbd "C-h") 'delete-backward-char)
     (define-key minibuffer-local-map (kbd "C-w") 'backward-kill-word)))
 
