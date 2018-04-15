2cae5cb709ae7ef6d36804feda4da482129f74fa
Seong Yong-ju <sei40kr@gmail.com>
2018-04-15 12:52:56 +0900

    Update custom spacemacs layer
    Minor formatting.

--- spacemacs/spacemacs.d/layers/+private/custom/packages.el
+++ spacemacs/spacemacs.d/layers/+private/custom/packages.el
@@ -146,11 +146,12 @@
   (setq
     neo-smart-open t
     neo-theme 'arrow
-    projectile-switch-project-action #'(lambda ()
-                                         (interactive)
-                                         (projectile-dired)
-                                         (require 'neotree)
-                                         (neotree-hide)))
+    projectile-switch-project-action
+    #'(lambda ()
+        (interactive)
+        (projectile-dired)
+        (require 'neotree)
+        (neotree-hide)))
   (unless (and (eq system-type 'gnu/linux) (executable-find "xdg-open"))
     (with-eval-after-load 'neotree
       (define-key neotree-mode-map (kbd "o") nil))))
