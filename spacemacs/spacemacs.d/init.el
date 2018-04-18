ba7a77d25ffb705c4166b7800c7816daa7e2f16e
Seong Yong-ju <sei40kr@gmail.com>
2018-04-18 21:22:03 +0900

    Add the WSP layer of Spacemacs

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -171,7 +171,8 @@ values."
        spacemacs-kotlin
        spacemacs-quickrun
        spacemacs-salesforce
-       custom)
+       custom
+       wsp)
 
     ;; List of additional packages that will be installed without being
     ;; wrapped in a layer. If you need some configuration for these
