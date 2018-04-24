78e32fae4f9fa478987138dce6316c1ae64dfd7d
Seong Yong-ju <sei40kr@gmail.com>
2018-04-25 01:49:25 +0900

    Update the custom layer for Spacemacs
    Disable the annoying confirmations by neotree.

--- spacemacs/spacemacs.d/layers/+private/custom/packages.el
+++ spacemacs/spacemacs.d/layers/+private/custom/packages.el
@@ -188,6 +188,11 @@
 
 (defun custom/post-init-neotree ()
   (setq
+    neo-confirm-create-directory 'off-p
+    neo-confirm-create-file 'off-p
+    neo-confirm-delete-directory-recursively 'off-p
+    neo-confirm-delete-file 'y-or-n-p
+    neo-confirm-kill-buffers-for-files-in-directory 'off-p
     neo-force-change-root t
     neo-smart-open t
     neo-theme 'arrow)
