426b35f80530ec4c53716ce43ed3245412e9e7b8
Seong Yong-ju <sei40kr@gmail.com>
2018-05-02 15:16:49 +0900

    Update the Spacemacs config
    Change the font from "UbuntuMono Nerd Font Mono" to "Source Han Code JP".

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -283,8 +283,8 @@ values."
     dotspacemacs-colorize-cursor-according-to-state t
     ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
     ;; quickly tweak the mode-line size to make separators look not too crappy.
-    dotspacemacs-default-font '("UbuntuMono Nerd Font Mono"
-                                 :size 18
+    dotspacemacs-default-font '("Source Han Code JP"
+                                 :size 15
                                  :weight normal
                                  :width normal
                                  :powerline-scale 1.0)
