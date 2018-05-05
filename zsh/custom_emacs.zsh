d64fb8236dab124405094af7e0c88695a9ca43b3
Seong Yong-ju <sei40kr@gmail.com>
2018-05-05 16:50:18 +0900

    Add the Emacs command

--- zsh/custom_emacs.zsh
+++ zsh/custom_emacs.zsh
@@ -2,29 +2,13 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 if [[ "$OSTYPE" = linux* ]]; then
-  export XLIB_SKIP_ARGB_VISUALS=1
+  alias e='XLIB_SKIP_ARGB_VISUALS=1 e'
 fi
-
-e() {
-  if [[ "$2" = '-' ]]; then
-    tempfile="$(mktemp "tmp.emacs.XXXXXXX" --tmpdir)"
-    cat - >"$tempfile"
-    emacsclient -a '' -c "$tempfile"
-  else
-    emacsclient -a '' -e '(frame-list)' 2>/dev/null \
-      | grep -oE '#<frame\s+[+>]+>' \
-      && emacsclient -a '' "$@" \
-      || emacsclient -a '' -c "$@"
-  fi
-}
 
 alias emacs='e -n'
 alias te='e -nw'
 
 alias eframe="emacsclient -a '' -c"
-
 alias ekill="emacsclient -e '(kill-emacs)'"
 
-efile() {
-  emacsclient -e '(buffer-file-name (window-buffer))' | tr -d '"'
-}
+alias efile="emacsclient -e '(buffer-file-name (window-buffer))' | tr -d '\"'"
