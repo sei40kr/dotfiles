3e838c782f18a11f43e25383338df78e4a914832
Seong Yong-ju <sei40kr@gmail.com>
2018-05-09 00:42:27 +0900

    Add new ranger command :mount

--- ranger/commands.py
+++ ranger/commands.py
@@ -8,10 +8,11 @@
 # -----------------------------------------------------------------------------
 
 from __future__ import (absolute_import, division, print_function)
-import os
+import os, time
+from ranger.core.loader import CommandLoader, Loadable
 from ranger.api.commands import Command
-from ranger.core.loader import CommandLoader
-
+from ranger.ext.signals import SignalDispatcher
+from ranger.ext.shell_escape import *
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
 # command.  Try typing ":my_edit<ENTER>" in ranger!
@@ -124,6 +125,54 @@ class compress(Command):
             'compress ' + os.path.basename(self.fm.thisdir.path) + ext
             for ext in extension
         ]
+
+
+class MountLoader(Loadable, SignalDispatcher):
+    """
+    Wait until a directory is mounted
+    """
+
+    def __init__(self, path):
+        SignalDispatcher.__init__(self)
+        descr = "Waiting for dir '" + path + "' to be mounted"
+        Loadable.__init__(self, self.generate(), descr)
+        self.path = path
+
+    def generate(self):
+        available = False
+        while not available:
+            try:
+                if os.path.ismount(self.path):
+                    available = True
+            except:
+                pass
+            yield
+            time.sleep(0.03)
+        self.signal_emit('after')
+
+
+class mount(Command):
+    def execute(self):
+        selected_files = self.fm.thisdir.get_selection()
+
+        if not selected_files:
+            return
+
+        space = ' '
+        self.fm.execute_command("cdemu -b system unload 0")
+        self.fm.execute_command("cdemu -b system load 0 " + \
+                space.join([shell_escape(f.path) for f in selected_files]))
+
+        mountpath = "/media/virtualrom/"
+
+        def mount_finished(path):
+            currenttab = self.fm.current_tab
+            self.fm.tab_open(9, mountpath)
+            self.fm.tab_open(currenttab)
+
+        obj = MountLoader(mountpath)
+        obj.signal_bind('after', mount_finished)
+        self.fm.loader.add(obj)
 
 
 class empty(Command):
