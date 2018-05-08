0f94630cff43b928ac0a5c7ee930d12a64803ff4
Seong Yong-ju <sei40kr@gmail.com>
2018-05-09 00:37:26 +0900

    Update the ranger config
    Add extracthere command.

--- ranger/commands.py
+++ ranger/commands.py
@@ -8,12 +8,9 @@
 # -----------------------------------------------------------------------------
 
 from __future__ import (absolute_import, division, print_function)
-
-# You can import any python module as needed.
 import os
-
-# You always need to import ranger.api.commands here to get the Command class:
 from ranger.api.commands import Command
+from ranger.core.loader import CommandLoader
 
 
 # Any class that is a subclass of "Command" will be integrated into ranger as a
@@ -60,6 +57,39 @@ class my_edit(Command):
         # This is a generic tab-completion function that iterates through the
         # content of the current directory.
         return self._tab_directory_content()
+
+
+class extracthere(Command):
+    def execute(self):
+        """ Extract copied files to current directory """
+        copied_files = tuple(self.fm.copy_buffer)
+
+        if not copied_files:
+            return
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = copied_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-X', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(copied_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags \
+                + [f.path for f in copied_files], descr=descr)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
 
 
 class empty(Command):
