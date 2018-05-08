ed5f44758f440f5a2bb2c29e4971c6434c948f0b
Seong Yong-ju <sei40kr@gmail.com>
2018-05-09 01:44:18 +0900

    Add a update script
    Add a script to update the submodules in dotfiles.

---
+++ etc/scripts/update.bash
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+# update.bash
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+set -euo pipefail
+IFS=$'\n\t'
+
+DOTFILES_PATH="$(realpath "$(dirname "$0")/../..")"
+
+cd "$DOTFILES_PATH"
+
+git submodule --init --remote --merge
