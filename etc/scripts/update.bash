34f2b0017a4737ab8883eff5451ed7f0b8892c26
Seong Yong-ju <sei40kr@gmail.com>
2018-05-10 01:13:50 +0900

    Update the updater
    Remove the submodule initializations while update, because it will be unexpected
    behavior for user (me).

--- etc/scripts/update.bash
+++ etc/scripts/update.bash
@@ -10,5 +10,5 @@ DOTFILES_PATH="$(realpath "$(dirname "$0")/../..")"
 
 cd "$DOTFILES_PATH"
 
-git submodule --init --remote --merge
+git submodule --remote --merge
 zsh -lc 'brew update && brew upgrade --all && brew cleanup'
