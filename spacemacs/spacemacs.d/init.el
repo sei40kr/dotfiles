1e6af3863889a613a8efe9dcd50c8853ee1124c8
Seong Yong-ju <sei40kr@gmail.com>
2018-05-13 11:13:46 +0900

    Update the Spacemacs config
    Unload spacemacs-kotlin, wsp.

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -184,11 +184,12 @@ values."
        ;; better-web
        lsp-intellij
        spacemacs-ghq
-       spacemacs-kotlin
+       ;; spacemacs-kotlin
        spacemacs-quickrun
        spacemacs-salesforce
        custom
-       wsp)
+       ;; wsp
+       )
 
     ;; List of additional packages that will be installed without being
     ;; wrapped in a layer. If you need some configuration for these
