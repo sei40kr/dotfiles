2de39f788724c8cacff4eb337f902e8ff83d1729
Seong Yong-ju <sei40kr@gmail.com>
2018-05-17 02:21:54 +0900

    Update the Spacemacs config
    Exclude persp-mode because it's not used and overrides the tabbar config.

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -211,7 +211,8 @@ values."
        counsel-gtags
        flycheck-pos-tip
        ggtags
-       helm-gtags)
+       helm-gtags
+       persp-mode)
 
     ;; Defines the behaviour of Spacemacs when installing packages.
     ;; Possible values are `used-only', `used-but-keep-unused' and `all'.
