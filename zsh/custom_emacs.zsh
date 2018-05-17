cb8241841c7a6b57ebf9f72f786675ea3572e7ee
Seong Yong-ju <sei40kr@gmail.com>
2018-05-17 17:31:59 +0900

    Update the ZSH aliases
    Fix the aliases for Emacs. Fix the TERM variable.

--- zsh/custom_emacs.zsh
+++ zsh/custom_emacs.zsh
@@ -6,10 +6,11 @@ if [[ "$OSTYPE" = linux* ]]; then
   export EDITOR='XLIB_SKIP_ARGB_VISUALS=1 e -nw'
 fi
 
+alias e='TERM=xterm-256color \e'
 alias emacs='e -n'
 alias te='e -nw'
 
-alias eframe="emacsclient -a '' -c"
+alias eframe="TERM=xterm-256color emacsclient -a '' -c"
 alias ekill="emacsclient -e '(kill-emacs)'"
 
 alias efile="emacsclient -e '(buffer-file-name (window-buffer))' | tr -d '\"'"
