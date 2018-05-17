2a1f56bd5483fdf731f69deaaaf8043bdc2d33bb
Seong Yong-ju <sei40kr@gmail.com>
2018-05-18 02:17:07 +0900

    Add dotfiles installer for Arch Linux

---
+++ etc/scripts/install_arch.bash
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install_arch.bash --- dotfiles installer for Arch Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+set -euo pipefail
+IFS=$'\n\t'
+
+DOTFILES_PATH="$(realpath "$(dirname "$0")/../..")"
+
+# i3
+mkdir -p "${HOME}/.config/i3"
+ln -sF "${DOTFILES_PATH}/i3/config" "${HOME}/.config/i3/config"
+
+# i3status
+mkdir -p "${HOME}/.config/i3status"
+ln -sF "${DOTFILES_PATH}/i3status/config" "${HOME}/.config/i3status/config"
