60be661828934facc74d62f4a0fef13d2c55ac60
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 00:52:38 +0900

    Update the installer for Arch Linux
    Ensure the directories exist.

--- etc/scripts/install_arch.bash
+++ etc/scripts/install_arch.bash
@@ -9,6 +9,7 @@ IFS=$'\n\t'
 DOTFILES_PATH="$(realpath "$(dirname "$0")/../..")"
 
 # dropbox
+mkdir -p "${HOME}/Dropbox/notes"
 systemctl --user enable dropbox
 
 # i3
@@ -18,3 +19,6 @@ ln -sF "${DOTFILES_PATH}/i3/config" "${HOME}/.config/i3/config"
 # i3status
 mkdir -p "${HOME}/.config/i3status"
 ln -sF "${DOTFILES_PATH}/i3status/config" "${HOME}/.config/i3status/config"
+
+# scrot
+mkdir -p "${HOME}/screenshots"
