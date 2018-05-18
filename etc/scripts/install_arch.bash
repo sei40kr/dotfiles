2dcb85801cadbfc40d52fd78205a66503f576983
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 00:56:08 +0900

    Update the installer for Arch Linux
    Create a symlink for compton, dmenu, dunst, fcitx, feh, xorg.

--- etc/scripts/install_arch.bash
+++ etc/scripts/install_arch.bash
@@ -8,9 +8,27 @@ IFS=$'\n\t'
 
 DOTFILES_PATH="$(realpath "$(dirname "$0")/../..")"
 
+# compton
+ln -sF "${DOTFILES_PATH}/compton/compton.conf" "${HOME}/.config/compton.conf"
+
+# dmenu
+ln -sF "${DOTFILES_PATH}/dmenu/dmenurc" "${HOME}/.dmenurc"
+
 # dropbox
 mkdir -p "${HOME}/Dropbox/notes"
 systemctl --user enable dropbox
+
+# dunst
+ln -sF "${DOTFILES_PATH}/dunst/dunstrc" "${HOME}/.dmenurc"
+
+# fcitx
+mkdir -p "${HOME}/.config/fcitx"
+ln -sF "${DOTFILES_PATH}/fcitx/config" "${HOME}/.config/fcitx/config"
+
+# feh
+mkdir -p "${HOME}/.local/share/backgrounds"
+cp -sF "${DOTFILES_PATH}/share/backgrounds"/*.png "${HOME}/.local/share/backgrounds"
+ln -sF "${DOTFILES_PATH}/feh/fehbg" "${HOME}/.fehbg"
 
 # i3
 mkdir -p "${HOME}/.config/i3"
@@ -22,3 +40,7 @@ ln -sF "${DOTFILES_PATH}/i3status/config" "${HOME}/.config/i3status/config"
 
 # scrot
 mkdir -p "${HOME}/screenshots"
+
+# xorg
+ln -sF "${DOTFILES_PATH}/xorg/Xresources" "${HOME}/.Xresources"
+ln -sF "${DOTFILES_PATH}/xorg/xprofile" "${HOME}/.xprofile"
