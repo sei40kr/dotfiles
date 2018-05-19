4c01f02b77349fa47ccbd6cbf30f099939f52e3d
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 17:52:44 +0900

    Update the ZSH config
    Add my-fzf-insert-files to improve the file listing.

--- zsh/custom_fzf.zsh
+++ zsh/custom_fzf.zsh
@@ -7,12 +7,21 @@ export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="bfs -nocolor -mindepth 1 -type d"
 
 bindkey '^r' fzf-insert-history
-bindkey '^t' fzf-insert-files
 bindkey '\ec' fzf-change-directory
 
 bindkey '^x^b' fzf-git-checkout-branch
 
-fzf-ghq-look() {
+my-fzf-insert-files() {
+  __fzf::widget::init || return 1
+
+  eval "$FZF_DEFAULT_COMMAND" |
+    __fzf::widget::select -m | \
+    __fzf::widget::insert -q
+}
+zle -N my-fzf-insert-files
+bindkey '^t' my-fzf-insert-files
+
+my-fzf-ghq-look() {
   __fzf::widget::init 'ghq look' || return 1
 
   ghq list | \
@@ -21,5 +30,5 @@ fzf-ghq-look() {
 
   __fzf::widget::exec
 }
-zle -N fzf-ghq-look
-bindkey '^x^g' fzf-ghq-look
+zle -N my-fzf-ghq-look
+bindkey '^x^g' my-fzf-ghq-look
