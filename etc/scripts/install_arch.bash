77de19ec253c6419c7d5668d165dfa1ae6226c4f
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 13:53:22 +0900

    Add the psd config
    Add psd config. Update the installer for Arch Linux to install psd.

--- etc/scripts/install_arch.bash
+++ etc/scripts/install_arch.bash
@@ -41,6 +41,11 @@ ln -sF "${DOTFILES_PATH}/i3/config" "${HOME}/.config/i3/config"
 mkdir -p "${HOME}/.config/i3status"
 ln -sF "${DOTFILES_PATH}/i3status/config" "${HOME}/.config/i3status/config"
 
+# psd
+mkdir -p "${HOME}/.config/psd"
+ln -sF "${DOTFILES_PATH}/psd/psd.conf" "${HOME}/.config/psd/psd.conf"
+systemctl --user enable psd.service
+
 # scrot
 mkdir -p "${HOME}/screenshots"
 
