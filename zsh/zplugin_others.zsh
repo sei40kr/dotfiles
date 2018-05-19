c54cc2b5291bf92b35bb36aabd1a806002f1a009
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 22:11:49 +0900

    Update the ZSH config
    Tweak the Pure theme to show current mode in the prompt.

--- zsh/zplugin_others.zsh
+++ zsh/zplugin_others.zsh
@@ -18,10 +18,27 @@ zplugin ice from'gh-r' as'program'; zplugin light junegunn/fzf-bin
 
 # pure
 
-PURE_PROMPT_SYMBOL='»'
+VIM_PROMPT='»'
 PURE_GIT_DOWN_ARROW='↑'
 PURE_GIT_UP_ARROW='↓'
-zplugin ice pick'async.zsh' lucid src'pure.zsh' wait'!0'
+zplugin ice pick'async.zsh' lucid src'pure.zsh' wait'!0' atload'
+PROMPT="%(?.%F{magenta}.%F{red})\${VIM_PROMPT}%f "
+
+prompt_pure_update_vim_prompt() {
+    zle || {
+        print "error: pure_update_vim_prompt must be called when zle is active"
+        return 1
+    }
+    VIM_PROMPT=${${KEYMAP/vicmd/«}/(main|viins)/»}
+    zle .reset-prompt
+}
+
+function zle-line-init zle-keymap-select {
+    prompt_pure_update_vim_prompt
+}
+zle -N zle-line-init
+zle -N zle-keymap-select'
+
 zplugin light sindresorhus/pure
 
 # zsh-autosuggestions
