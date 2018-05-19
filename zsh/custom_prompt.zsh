daa9c9d5e3f7d02b177f2293bd066f91949915cb
Seong Yong-ju <sei40kr@gmail.com>
2018-05-20 00:02:44 +0900

    Update the ZSH config
    Show the work hours more naturally.

--- zsh/custom_prompt.zsh
+++ zsh/custom_prompt.zsh
@@ -7,7 +7,7 @@ print-toggl-current() {
   toggl --cache --csv current | \
     awk -F',' '
       $1 == "Description" { if (length($2) > 20) print substr($2, 0, 20) "..."; else print $2 }
-      $1 == "Duration" { print $2 }' ORS=' '
+      $1 == "Duration" { split($2, t, ":"); print t[1] " hrs " t[2] " min" }' ORS=' '
 }
 
 ZLE_RPROMPT_INDENT=0
