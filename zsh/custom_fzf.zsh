aa837457672aff973ea74b3ad4ca80523fc09884
Seong Yong-ju <sei40kr@gmail.com>
2018-05-19 21:14:15 +0900

    Update the ZSH config
    Add custom colors for FZF.

--- zsh/custom_fzf.zsh
+++ zsh/custom_fzf.zsh
@@ -2,7 +2,12 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 export FZF_DEFAULT_COMMAND='git rev-parse 2>/dev/null && git ls-files -co --exclude-standard || fd -t f -c never'
-export FZF_DEFAULT_OPTS='--reverse --inline-info'
+export FZF_DEFAULT_OPTS="
+  --reverse
+  --inline-info
+  --color fg:'#b2b2b2',bg:'#262626',hl:'#268bd2',fg+:'#b2b2b2',bg+:'#444444',hl+:'#268bd2'
+  --color info:'#b1951d',prompt:'#b1951d',pointer:'#b2b2b2',marker:'#b2b2b2'
+  --no-bold"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="bfs -nocolor -mindepth 1 -type d"
 
