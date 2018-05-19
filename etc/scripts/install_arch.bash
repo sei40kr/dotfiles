66c164299177da7e42a7d42a92febdb5d3da2624
Seong Yong-ju <sei40kr@gmail.com>
2018-05-20 01:45:53 +0900

    Replace i3status with i3blocks

--- etc/scripts/install_arch.bash
+++ etc/scripts/install_arch.bash
@@ -40,9 +40,8 @@ ln -sF "${DOTFILES_PATH}/fontconfig" "${HOME}/.config/fontconfig"
 mkdir -p "${HOME}/.config/i3"
 ln -sF "${DOTFILES_PATH}/i3/config" "${HOME}/.config/i3/config"
 
-# i3status
-mkdir -p "${HOME}/.config/i3status"
-ln -sF "${DOTFILES_PATH}/i3status/config" "${HOME}/.config/i3status/config"
+# i3blocks
+ln -sF "${DOTFILES_PATH}/i3blocks/i3blocks.conf" "${HOME}/.i3blocks.conf"
 
 # psd
 mkdir -p "${HOME}/.config/psd"
