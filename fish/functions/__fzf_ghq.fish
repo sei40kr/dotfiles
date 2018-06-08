906061ea4b95f4da4398c41963da669d27240624
Seong Yong-ju <sei40kr@gmail.com>
2018-06-09 00:20:06 +0900

    Update the fish shell config
    Add a new function __fzf_ghq.

---
+++ fish/functions/__fzf_ghq.fish
@@ -0,0 +1,15 @@
+function __fzf_ghq
+    if not type -q ghq
+        echo 'ERROR: ghq is not installed.' >&2
+        return 1
+    end
+
+    if not type -q fzf
+        echo 'ERROR: fzf is not installed.' >&2
+        return 1
+    end
+
+    ghq list | fzf | read ghq_repo
+    and cd (ghq root)/{$ghq_repo}
+    and commandline -f repaint
+end
---
+++ fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,3 @@
+function fish_user_key_bindings
+    bind \cx\cg __fzf_ghq
+end
