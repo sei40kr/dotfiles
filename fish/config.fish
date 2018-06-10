aa8267f681fd2a520bf68fb6b15889eab5dede0b
Seong Yong-ju <sei40kr@gmail.com>
2018-06-11 02:29:12 +0900

    Add a fisherman plugin
    Add of-my-fish/theme-bobthefish.

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,16 @@
 
 # config.fish
 # author: Seong Yong-ju <sei40kr@gmail.com>
+
+## bobthefish
+
+# Font options
+set -g theme_powerline_fonts no
+set -g theme_nerd_fonts yes
+# Prompt options
+set -g theme_newline_cursor yes
+# Color scheme options
+set -g theme_color_scheme base16-dark
+# VCS options
+set -g theme_display_git_dirty_verbose no
+set -g theme_display_git_master_branch no
