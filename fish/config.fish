6aca9d61f0a69502ce2e3dc9d074ed112bc14fc6
Seong Yong-ju <sei40kr@gmail.com>
2018-06-14 06:33:29 +0900

    Update the fish shell function __tmux_rename
    Don't run while $TMUX is 0 (tmux is not running).

--- fish/config.fish
+++ fish/config.fish
@@ -18,6 +18,10 @@ set -g theme_display_git_master_branch no
 ## tmux
 
 function __tmux_rename -v PWD
+    if [ -z "$TMUX" ]
+        return
+    end
+
     set -lx tmux_window_id (tmux display-message -p '#{window_id}')
     set -lx cwd $PWD
     fish -c 'tmux rename-window -t $tmux_window_id (basename (git -C $cwd rev-parse --show-toplevel ^/dev/null; or echo $cwd)) &'
