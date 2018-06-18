e0252e5953fd519b98bef5b127a8bf2d19afa66f
Seong Yong-ju <sei40kr@gmail.com>
2018-06-18 20:28:42 +0900

    Update the Spacemacs config
    Change the transparency from 96 to 90.

--- spacemacs/spacemacs.d/init.el
+++ spacemacs/spacemacs.d/init.el
@@ -479,12 +479,12 @@ It should only modify the values of Spacemacs settings."
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's active or selected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
-   dotspacemacs-active-transparency 96
+   dotspacemacs-active-transparency 90
 
    ;; A value from the range (0..100), in increasing opacity, which describes
    ;; the transparency level of a frame when it's inactive or deselected.
    ;; Transparency can be toggled through `toggle-transparency'. (default 90)
-   dotspacemacs-inactive-transparency 96
+   dotspacemacs-inactive-transparency 90
 
    ;; If non-nil show the titles of transient states. (default t)
    dotspacemacs-show-transient-state-title t
