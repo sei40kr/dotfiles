6e3a71e56faee7b8d1761a66684af0823f62ea41
Seong Yong-ju <sei40kr@gmail.com>
2018-06-24 22:33:32 +0900

    Update bootstrap.fish
    Detect environment variables automatically.

--- deploy/bootstrap.bash
+++ deploy/bootstrap.bash
@@ -6,7 +6,30 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-basepath="$(dirname "$(realpath "$0")")"
+: ${PYENV_ROOT:=${HOME}/.pyenv}
 
-echo 'Setting up the local environment ...'
-ANSIBLE_NOCOWS=1 ansible-playbook -i "${basepath}/inventory" "${basepath}/defaults/main.yml" "$@"
+if [[ ! -d "${PYENV_ROOT}" ]]; then
+    echo "ERROR: pyenv is not found in PYENV_ROOT: PYENV_ROOT=${PYENV_ROOT}"
+    exit 1
+fi
+
+if [[ ! -x "${PYENV_ROOT}/shims/python3" ]]; then
+    echo "ERROR: python3 is not installed via pyenv."
+    exit 1
+fi
+
+if [[ ! -x "${PYENV_ROOT}/shims/ansible-playbook" ]]; then
+    echo "ERROR: ansible-playbook is not installed via pyenv."
+    exit 1
+fi
+
+basedir="$(dirname "$(realpath "$0")")"
+dotfiles_path="$(realpath "$basedir/..")"
+
+ANSIBLE_NOCOWS=1 "${PYENV_ROOT}/shims/ansible-playbook" \
+              -e ansible_connection=local \
+              -e ansible_python_interpreter="${PYENV_ROOT}/shims/python3" \
+              -e dotfiles_path="$dotfiles_path" \
+              -i "${basedir}/inventory" \
+              "${basedir}/defaults/main.yml" \
+              "$@"
