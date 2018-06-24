2b22ad51c76c512ae3677dd5bca747116add9016
Seong Yong-ju <sei40kr@gmail.com>
2018-06-24 22:50:14 +0900

    Update run-playbook.bash
    Install pyenv automatically.

--- deploy/run-playbook.bash
+++ deploy/run-playbook.bash
@@ -6,11 +6,35 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+_yes_or_no() {
+    read -p "${1} [y/n] " ans
+    case "$ans" in
+        [Yy]* )
+            return 0
+            ;;
+        [Nn]* )
+            return 1
+            ;;
+        * )
+            echo 'Please answer y or n.'
+            _yes_or_no "$1"
+    esac
+}
+
 : ${PYENV_ROOT:=${HOME}/.pyenv}
 
 if [[ ! -d "${PYENV_ROOT}" ]]; then
-    echo "ERROR: pyenv is not found in PYENV_ROOT: PYENV_ROOT=${PYENV_ROOT}" >&2
-    exit 1
+    echo "INFO: pyenv is not found in PYENV_ROOT: PYENV_ROOT=${PYENV_ROOT}"
+
+    if _yes_or_no 'Would you like install pyenv?'; then
+        if ! hash git 2>/dev/null; then
+            echo "ERROR: git is not installed." >&2
+            exit 1
+        fi
+
+        git clone https://github.com/pyenv/pyenv.git "$PYENV_ROOT"
+        echo "INFO: pyenv is installed to ${PYENV_ROOT}"
+    fi
 fi
 
 if [[ ! -x "${PYENV_ROOT}/shims/python3" ]]; then
