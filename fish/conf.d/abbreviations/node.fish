48443bcdcd9919faab1774392081959b58833783
Seong Yong-ju <sei40kr@gmail.com>
2018-06-24 17:09:11 +0900

    Add fish shell abbreviations for react-native

--- fish/conf.d/abbreviations/node.fish
+++ fish/conf.d/abbreviations/node.fish
@@ -25,3 +25,21 @@ abbr -a ycc yarn cache clean
 abbr -a yh yarn help
 abbr -a yo yarn outdated
 abbr -a yui yarn upgrade-interactive
+
+
+# react-native
+
+abbr -a rn react-native
+abbr -a rns react-native start
+abbr -a rnlink react-native link
+
+abbr -a rnand react-native run-android
+abbr -a rnios react-native run-ios
+abbr -a rnios4s react-native run-ios --simulator \'iPhone 4s\'
+abbr -a rnios5 react-native run-ios --simulator \'iPhone 5\'
+abbr -a rnios5s react-native run-ios --simulator \'iPhone 5s\'
+abbr -a rnios6 react-native run-ios --simulator \'iPhone 6\'
+abbr -a rnios6s react-native run-ios --simulator \'iPhone 6s\'
+
+abbr -a rnland react-native log-android
+abbr -a rnlios react-native log-ios
