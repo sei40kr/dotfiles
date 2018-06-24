5a1703714f7aba8f27bbffe68954874073035b1e
Seong Yong-ju <sei40kr@gmail.com>
2018-06-24 20:03:48 +0900

    Update a fish shell key binding
    Remove key binding for __fzf_complete.

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -7,7 +7,6 @@ function fish_user_key_bindings
     bind \ct __fzf_find_file
     bind \cr __fzf_reverse_isearch
     bind \ec '__fzf_cd --hidden'
-    bind \t __fzf_complete
     bind \cx\cb __fzf_git_checkout
     bind \cx\cg __fzf_ghq
     bind \cx. __fzf_edit_dotfile
