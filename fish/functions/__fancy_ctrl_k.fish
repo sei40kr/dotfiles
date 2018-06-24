e850953b320ebfb1b94144b20555bac42fdc27b2
Seong Yong-ju <sei40kr@gmail.com>
2018-06-24 18:20:57 +0900

    Update fish shell function __fancy_ctrl_k
    Do kill-line when the command-line is not empty.

--- fish/functions/__fancy_ctrl_k.fish
+++ fish/functions/__fancy_ctrl_k.fish
@@ -2,6 +2,10 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 function __fancy_ctrl_k
-    commandline -- fg
-    commandline -f execute
+    if [ -z (commandline) ]
+        commandline -- fg
+        commandline -f execute
+    else
+        commandline -f kill-line
+    end
 end
