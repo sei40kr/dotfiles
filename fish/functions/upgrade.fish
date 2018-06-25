be0658c5f573578c7f62cfc00fbb629140eea545
Seong Yong-ju <sei40kr@gmail.com>
2018-06-25 10:45:21 +0900

    Update fish shell function upgrade

--- fish/functions/upgrade.fish
+++ fish/functions/upgrade.fish
@@ -2,9 +2,20 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 function upgrade
-    if type -q trizen
-        trizen -Syu
-    else
-        sudo pacman -Syu
+    switch (uname)
+        case Linux
+            if type -q trizen
+                trizen -Syu
+            else
+                sudo pacman -Syu
+            end
+        case Darwin
+            if not type -q brew
+                echo 'ERROR: Homebrew is not installed.' >&2
+                return 1
+            end
+
+            brew update
+            and brew upgrade
     end
 end
