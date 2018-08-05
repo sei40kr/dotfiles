476aded9444bba1528b66505b4209350eab9b605
Seong Yong-ju <sei40kr@gmail.com>
2018-08-05 22:41:56 +0900

    Add Profile-sync-daemon module for new installer

---
+++ install-scripts/modules/psd/arch.bash
@@ -0,0 +1,4 @@
+# arch.bash --- Profile-sync-daemon installer for ArchLinux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+trizen_sync_pkg profile-sync-daemon
---
+++ install-scripts/modules/psd/linux.bash
@@ -0,0 +1,4 @@
+# linux.bash --- Profile-sync-daemon installer for Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+make_symlink psd/psd.conf .config/psd/psd.conf
