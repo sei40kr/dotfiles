927fa9bc1da9c3eb85546165bdfb580f0455432e
Seong Yong-ju <sei40kr@gmail.com>
2018-08-06 20:58:17 +0900

    Add fcitx to new installer

---
+++ install-scripts/modules/fcitx/arch.bash
@@ -0,0 +1,6 @@
+# arch.bash --- fcitx installer for ArchLinux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+pacman_sync_pkg fcitx-im
+pacman_sync_pkg fcitx-configtool
+pacman_sync_pkg fcitx-mozc
---
+++ install-scripts/modules/fcitx/linux.bash
@@ -0,0 +1,5 @@
+# linux.bash --- fcitx installer for Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+make_symlink fcitx/conf/fcitx-classic-ui.config .config/fcitx/conf/fcitx-classic-ui.config
+make_symlink fcitx/config .config/fcitx/config
