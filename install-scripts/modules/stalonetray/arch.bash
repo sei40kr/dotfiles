a6e83c49bad48fdaee22eb3c08e1e32f0397924a
Seong Yong-ju <sei40kr@gmail.com>
2018-08-06 21:22:59 +0900

    Add Stalonetray for new installer

---
+++ install-scripts/modules/stalonetray/arch.bash
@@ -0,0 +1,4 @@
+# arch.bash --- Stalonetray installer for ArchLinux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+pacman_sync_pkg stalonetray
---
+++ install-scripts/modules/stalonetray/linux.bash
@@ -0,0 +1,4 @@
+# linux.bash --- Stalonetray installer for Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+make_symlink stalonetray/stalonetrayrc .stalonetrayrc
