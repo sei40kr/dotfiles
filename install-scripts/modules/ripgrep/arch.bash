0fe91102f323cff76d898ac23b5935501d40a1be
Seong Yong-ju <sei40kr@gmail.com>
2018-08-06 21:25:52 +0900

    Add ripgrep for new installer

---
+++ install-scripts/modules/ripgrep/arch.bash
@@ -0,0 +1,4 @@
+# arch.bash --- ripgrep installer for ArchLinux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+pacman_sync_pkg ripgrep
---
+++ install-scripts/modules/ripgrep/default.bash
@@ -0,0 +1,4 @@
+# default.bash --- ripgrep installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+make_symlink ripgrep/ripgreprc .ripgreprc
