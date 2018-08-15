062fffed2d433c507032f3f73df128be84ef6926
Seong Yong-ju <sei40kr@gmail.com>
2018-08-15 21:33:04 +0900

    Add new installer for feh

---
+++ install-scripts/modules/feh/arch.bash
@@ -0,0 +1,4 @@
+# arch.bash --- feh installer for Arch Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+pacman_sync_pkg feh
---
+++ install-scripts/modules/feh/linux.bash
@@ -0,0 +1,4 @@
+# linux.bash --- feh installer for Linux
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+make_symlink feh/fehbg .fehbg
