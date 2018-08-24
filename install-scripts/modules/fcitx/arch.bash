2dd7eed3fa2530e9d1ed7c8a99b6ed0773857838
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:07:50 +0900

    Update the fcitx installer
    Replace fcitx-im package group with fcitx-* packages. Support the latest
    installer update.

--- install-scripts/modules/fcitx/arch.bash
+++ install-scripts/modules/fcitx/arch.bash
@@ -1,6 +1,12 @@
 # arch.bash --- fcitx installer for ArchLinux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-pacman_sync_pkg fcitx-im
+require_facades pacman
+
+pacman_sync_pkg fcitx
+pacman_sync_pkg fcitx-gtk2
+pacman_sync_pkg fcitx-gtk3
+pacman_sync_pkg fcitx-qt4
+pacman_sync_pkg fcitx-qt5
 pacman_sync_pkg fcitx-configtool
 pacman_sync_pkg fcitx-mozc
--- install-scripts/modules/fcitx/linux.bash
+++ install-scripts/modules/fcitx/linux.bash
@@ -1,5 +1,7 @@
 # linux.bash --- fcitx installer for Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink fcitx/conf/fcitx-classic-ui.config .config/fcitx/conf/fcitx-classic-ui.config
-make_symlink fcitx/config .config/fcitx/config
+require_facades symlink
+
+symlink_make fcitx/conf/fcitx-classic-ui.config .config/fcitx/conf/fcitx-classic-ui.config
+symlink_make fcitx/config .config/fcitx/config
