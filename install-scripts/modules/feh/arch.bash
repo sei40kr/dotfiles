4498a6b689a416a9109a9afe9862b799d6170ea1
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:10:47 +0900

    Update the feh installer
    Create a symlink for background image. Support the latest installer update.

--- install-scripts/modules/feh/arch.bash
+++ install-scripts/modules/feh/arch.bash
@@ -1,4 +1,6 @@
 # arch.bash --- feh installer for Arch Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades pacman
+
 pacman_sync_pkg feh
--- install-scripts/modules/feh/linux.bash
+++ install-scripts/modules/feh/linux.bash
@@ -1,4 +1,6 @@
 # linux.bash --- feh installer for Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink feh/fehbg .fehbg
+require_facades symlink
+
+symlink_make feh/ac-01-3840x2160.jpg .local/share/backgrounds/ac-01-3840x2160.jpg
