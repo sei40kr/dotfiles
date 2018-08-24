d2cb60d82a424cee5a59fca70fd9b0d8b9b10856
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:09:15 +0900

    Update the Profile-sync-daemon installer
    Support the latest installer update.

--- install-scripts/modules/psd/arch.bash
+++ install-scripts/modules/psd/arch.bash
@@ -1,4 +1,6 @@
 # arch.bash --- Profile-sync-daemon installer for ArchLinux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades trizen
+
 trizen_sync_pkg profile-sync-daemon
--- install-scripts/modules/psd/linux.bash
+++ install-scripts/modules/psd/linux.bash
@@ -1,4 +1,6 @@
 # linux.bash --- Profile-sync-daemon installer for Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink psd/psd.conf .config/psd/psd.conf
+require_facades symlink
+
+symlink_make psd/psd.conf .config/psd/psd.conf
