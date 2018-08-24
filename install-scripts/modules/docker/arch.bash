79a30b72196f327e9d92dd571a0ee395e04e1f11
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:07:21 +0900

    Update the Docker installer
    Support the latest installer update.

--- install-scripts/modules/docker/arch.bash
+++ install-scripts/modules/docker/arch.bash
@@ -1,6 +1,8 @@
 # arch.bash --- Docker installer for Arch Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades pacman systemctl
+
 pacman_sync_pkg docker
 pacman_sync_pkg docker-compose
 
