11ae7b2bc377077c0148f08816c9a1d575511f15
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:10:26 +0900

    Update the ripgrep installer
    Support the latest installer update.

--- install-scripts/modules/ripgrep/arch.bash
+++ install-scripts/modules/ripgrep/arch.bash
@@ -1,4 +1,6 @@
 # arch.bash --- ripgrep installer for ArchLinux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades pacman
+
 pacman_sync_pkg ripgrep
--- install-scripts/modules/ripgrep/default.bash
+++ install-scripts/modules/ripgrep/default.bash
@@ -1,4 +1,6 @@
 # default.bash --- ripgrep installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink ripgrep/ripgreprc .ripgreprc
+require_facades symlink
+
+symlink_make ripgrep/ripgreprc .ripgreprc
