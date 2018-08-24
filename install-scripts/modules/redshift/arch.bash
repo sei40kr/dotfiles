ef93189310882e46972ba6b413961d5b858b13f7
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:09:32 +0900

    Update the Redshift installer
    Add .service suffix to the service name. Support the latest installer update.

--- install-scripts/modules/redshift/arch.bash
+++ install-scripts/modules/redshift/arch.bash
@@ -1,6 +1,8 @@
 # arch.bash --- Redshift installer for ArchLinux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades pacman systemctl
+
 pacman_sync_pkg redshift
 
-systemctl_enable_usrsvc redshift
+systemctl_enable_usrsvc redshift.service
--- install-scripts/modules/redshift/linux.bash
+++ install-scripts/modules/redshift/linux.bash
@@ -1,5 +1,7 @@
 # linux.bash --- Redshift installer for Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink redshift/redshift.conf .config/redshift/redshift.conf
-make_symlink redshift/hooks/brightness.sh .config/redshift/hooks/brightness.sh
+require_facades symlink
+
+symlink_make redshift/redshift.conf .config/redshift/redshift.conf
+symlink_make redshift/hooks/brightness.sh .config/redshift/hooks/brightness.sh
