0687a7322d6cc425750f4fbc9f1ab336da36da06
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:08:47 +0900

    Update the Fontconfig installer
    Support the latest installer update.

--- install-scripts/modules/fontconfig/arch.bash
+++ install-scripts/modules/fontconfig/arch.bash
@@ -1,4 +1,6 @@
 # arch.bash --- Fontconfig installer for ArchLinux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+require_facades pacman
+
 pacman_sync_pkg fontconfig
--- install-scripts/modules/fontconfig/linux.bash
+++ install-scripts/modules/fontconfig/linux.bash
@@ -1,9 +1,11 @@
 # linux.bash --- Fontconfig installer for Linux
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-make_symlink fontconfig/fonts.conf .config/fontconfig/fonts.conf
-make_symlink fontconfig/conf.d/10-hinting-none.conf .config/fontconfig/conf.d/10-hinting-none.conf
-make_symlink fontconfig/conf.d/10-sub-pixel-rgb.conf .config/fontconfig/conf.d/10-sub-pixel-rgb.conf
-make_symlink fontconfig/conf.d/11-lcdfilter-default.conf .config/fontconfig/conf.d/11-lcdfilter-default.conf
-make_symlink fontconfig/conf.d/66-courier-bitmap-ja.conf .config/fontconfig/conf.d/66-courier-bitmap-ja.conf
-make_symlink fontconfig/conf.d/66-terminus-ja.conf .config/fontconfig/conf.d/66-terminus-ja.conf
+require_facades symlink
+
+symlink_make fontconfig/fonts.conf .config/fontconfig/fonts.conf
+symlink_make fontconfig/conf.d/10-hinting-none.conf .config/fontconfig/conf.d/10-hinting-none.conf
+symlink_make fontconfig/conf.d/10-sub-pixel-rgb.conf .config/fontconfig/conf.d/10-sub-pixel-rgb.conf
+symlink_make fontconfig/conf.d/11-lcdfilter-default.conf .config/fontconfig/conf.d/11-lcdfilter-default.conf
+symlink_make fontconfig/conf.d/66-courier-bitmap-ja.conf .config/fontconfig/conf.d/66-courier-bitmap-ja.conf
+symlink_make fontconfig/conf.d/66-terminus-ja.conf .config/fontconfig/conf.d/66-terminus-ja.conf
