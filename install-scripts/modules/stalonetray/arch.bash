24d4a34ba749da3863f8f4ebb60a0d29570ac963
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 03:06:27 +0900

    Remove stalonetray installation
    Remove the files and installer module.

--- install-scripts/modules/stalonetray/arch.bash
+++
@@ -1,4 +0,0 @@
-# arch.bash --- Stalonetray installer for ArchLinux
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-pacman_sync_pkg stalonetray
--- install-scripts/modules/stalonetray/linux.bash
+++
@@ -1,4 +0,0 @@
-# linux.bash --- Stalonetray installer for Linux
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-make_symlink stalonetray/stalonetrayrc .stalonetrayrc
