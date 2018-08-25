0693dcfd76d8d25ac02aaf02841cca5cb94cc41f
Seong Yong-ju <sei40kr@gmail.com>
2018-08-25 12:02:31 +0900

    Update the installer
    Hide the commands when --dry-run is specified.

--- install-scripts/facade-utility.bash
+++ install-scripts/facade-utility.bash
@@ -2,14 +2,13 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 facade_exec_cmd() {
-    is_verbose || do_dry_run && echo ">" "$@"
+    is_verbose && echo ">" "$@"
 
     if ! do_dry_run; then
         command "$@" 1>/dev/null
 
         # If the command didn't complete successfully
         if [[ "$?" != 0 ]]; then
-            # Show the command even without --verbose option
             is_verbose || echo ">" "$@" >&2
             die 'An error occured while installation.'
         fi
