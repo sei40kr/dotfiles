0fc8ace08157cd795e30be85fc2f865e40e7367d
Seong Yong-ju <sei40kr@gmail.com>
2018-09-20 01:33:16 +0900

    Update the misc installer
    Add some packages to install.

--- install-scripts/modules/misc.bash
+++ install-scripts/modules/misc.bash
@@ -1,6 +1,13 @@
 # misc.bash
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+pacman_sync_facade fd
+
+trizen_sync_facade bfs
+
+pip3_install_facade asciinema
+pip3_install_facade aws-shell
+
 yarn_global_add_facade create-react-app
 yarn_global_add_facade create-react-native-app
 yarn_global_add_facade flow-bin
