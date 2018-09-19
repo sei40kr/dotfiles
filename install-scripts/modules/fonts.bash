493a416e040064fda94d2230f62f07488f372b84
Seong Yong-ju <sei40kr@gmail.com>
2018-09-20 02:18:45 +0900

    Update the fonts installer
    Install fonts using pacman only on Arch Linux.

--- install-scripts/modules/fonts.bash
+++ install-scripts/modules/fonts.bash
@@ -1,8 +1,10 @@
 # fonts.bash --- Fonts
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-pacman_sync_facade nerd-fonts-fira-code
-pacman_sync_facade noto-fonts
-pacman_sync_facade noto-fonts-cjk
-pacman_sync_facade terminus-font
-pacman_sync_facade ttf-liberation
+if is_arch; then
+    pacman_sync_facade nerd-fonts-fira-code
+    pacman_sync_facade noto-fonts
+    pacman_sync_facade noto-fonts-cjk
+    pacman_sync_facade terminus-font
+    pacman_sync_facade ttf-liberation
+fi
