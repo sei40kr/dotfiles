3af9e2003234d64b5ec01ac86ba28c753cc9ff56
Seong Yong-ju <sei40kr@gmail.com>
2018-09-20 01:13:41 +0900

    Add misc installer
    Add an installer module that installs misc tools.

---
+++ install-scripts/modules/misc.bash
@@ -0,0 +1,16 @@
+# misc.bash
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+yarn_global_add_facade create-react-app
+yarn_global_add_facade create-react-native-app
+yarn_global_add_facade flow-bin
+yarn_global_add_facade gatsby-cli
+yarn_global_add_facade generate
+yarn_global_add_facade generate-editorconfig
+yarn_global_add_facade generate-gitignore
+yarn_global_add_facade generate-license
+yarn_global_add_facade generate-project
+yarn_global_add_facade gulp
+yarn_global_add_facade typescript
+yarn_global_add_facade webpack
+yarn_global_add_facade yo
