44f41cc69ec513c526b58aa834923f1d7227ca29
Seong Yong-ju <sei40kr@gmail.com>
2018-09-19 00:28:31 +0000

    Update the fcitx installer

--- install-scripts/modules/fcitx/linux.bash
+++
@@ -1,7 +0,0 @@
-# linux.bash --- fcitx installer for Linux
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-require_facades symlink
-
-symlink_make fcitx/conf/fcitx-classic-ui.config .config/fcitx/conf/fcitx-classic-ui.config
-symlink_make fcitx/config .config/fcitx/config
