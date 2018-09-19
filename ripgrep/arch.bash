562305bd6608d9e71bc5d095ec1d9101cf50daf6
Seong Yong-ju <sei40kr@gmail.com>
2018-09-19 00:15:17 +0000

    Remove the older ripgrep installer

--- ripgrep/arch.bash
+++
@@ -1,6 +0,0 @@
-# arch.bash --- ripgrep installer for ArchLinux
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-require_facades pacman
-
-pacman_sync_pkg ripgrep
--- ripgrep/default.bash
+++
@@ -1,6 +0,0 @@
-# default.bash --- ripgrep installer
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-require_facades symlink
-
-symlink_make ripgrep/ripgreprc .ripgreprc
