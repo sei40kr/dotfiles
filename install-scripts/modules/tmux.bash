d21e8bf29b5ae9420936e60e724e18f4da42d76b
Seong Yong-ju <sei40kr@gmail.com>
2018-09-24 19:42:12 +0900

    Fix TMUX installer
    Install tmux using pacman only on Arch Linux.

--- install-scripts/modules/tmux.bash
+++ install-scripts/modules/tmux.bash
@@ -1,8 +1,10 @@
 # tmux.bash --- TMUX installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-pacman_sync_facade tmux
+if is_arch; then
+    pacman_sync_facade tmux
 
-ln_facade "${DOTFILES_PATH}/tmux/tmux.conf" "${HOME}/.tmux.conf"
-ln_facade "${DOTFILES_PATH}/tmux/tmux-cpu-usage" "${HOME}/.local/bin/tmux-cpu-usage"
-ln_facade "${DOTFILES_PATH}/tmux/tmux-mem-usage" "${HOME}/.local/bin/tmux-mem-usage"
+    ln_facade "${DOTFILES_PATH}/tmux/tmux.conf" "${HOME}/.tmux.conf"
+    ln_facade "${DOTFILES_PATH}/tmux/tmux-cpu-usage" "${HOME}/.local/bin/tmux-cpu-usage"
+    ln_facade "${DOTFILES_PATH}/tmux/tmux-mem-usage" "${HOME}/.local/bin/tmux-mem-usage"
+fi
