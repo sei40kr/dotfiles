c9f7df9f5e4bae6388acb0f22af839e5e1ddcc20
Seong Yong-ju <sei40kr@gmail.com>
2018-09-24 16:12:12 +0900

    Update the xmonad config
    Include shift-key to the mask.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -44,7 +44,7 @@ myBorderWidth = 2
 -- "windows key" is usually mod4Mask.
 --
 myModMask       :: KeyMask
-myModMask       = mod1Mask
+myModMask       = mod1Mask .|. shiftMask
 
 -- NOTE: from 0.9.1 on numlock mask is set automatically. The numlockMask
 -- setting should be removed from configs.
@@ -114,36 +114,35 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
     -- close focused window
   , ((mod4Mask, xK_q), kill)
      -- Rotate through the available layout algorithms
-  , ((modm .|. shiftMask, xK_space), sendMessage NextLayout)
+  , ((modm, xK_space), sendMessage NextLayout)
     --  Reset the layouts on the current workspace to default
-  , ( (modm .|. controlMask .|. shiftMask, xK_space)
-    , setLayout $ XMonad.layoutHook conf)
+  , ((modm .|. controlMask, xK_space), setLayout $ XMonad.layoutHook conf)
     -- Move focus to the next window
   , ((mod4Mask .|. shiftMask, xK_Tab), windows W.focusDown)
     -- Move focus to the next window
-  , ((modm .|. shiftMask, xK_j), windows W.focusDown)
+  , ((modm, xK_j), windows W.focusDown)
     -- Move focus to the previous window
-  , ((modm .|. shiftMask, xK_k), windows W.focusUp)
+  , ((modm, xK_k), windows W.focusUp)
     -- Move focus to the master window
-  , ((modm .|. shiftMask, xK_m), windows W.focusMaster)
+  , ((modm, xK_m), windows W.focusMaster)
     -- Swap the focused window and the master window
-  , ((modm .|. shiftMask, xK_Return), windows W.swapMaster)
+  , ((modm, xK_Return), windows W.swapMaster)
     -- Swap the focused window with the next window
-  , ((modm .|. controlMask .|. shiftMask, xK_j), windows W.swapDown)
+  , ((modm .|. controlMask, xK_j), windows W.swapDown)
     -- Swap the focused window with the previous window
-  , ((modm .|. controlMask .|. shiftMask, xK_k), windows W.swapUp)
+  , ((modm .|. controlMask, xK_k), windows W.swapUp)
     -- Shrink the master area
-  , ((modm .|. shiftMask, xK_h), sendMessage Shrink)
+  , ((modm, xK_h), sendMessage Shrink)
     -- Expand the master area
-  , ((modm .|. shiftMask, xK_l), sendMessage Expand)
+  , ((modm, xK_l), sendMessage Expand)
     -- Push window back into tiling
     -- , ((modm, xK_t), withFocused $ windows . W.sink)
     -- Increment the number of windows in the master area
-  , ((modm .|. shiftMask, xK_comma), sendMessage (IncMasterN 1))
+  , ((modm, xK_comma), sendMessage (IncMasterN 1))
     -- Deincrement the number of windows in the master area
-  , ((modm .|. shiftMask, xK_period), sendMessage (IncMasterN (-1)))
+  , ((modm, xK_period), sendMessage (IncMasterN (-1)))
     -- Restart xmonad
-  , ((modm .|. shiftMask, xK_q), spawn "xmonad --recompile; xmonad --restart")
+  , ((modm, xK_q), spawn "xmonad --recompile; xmonad --restart")
     -- Capture a screenshot
   , ((mod4Mask .|. shiftMask, xK_3), spawn "scrot '%Y-%m-%d-%T-screenshot.png'")
     -- Capture a screenshot selecting a window or rectangle with the mouse
@@ -154,16 +153,16 @@ myKeys conf@XConfig {XMonad.modMask = modm} =
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
-  [ ((modm .|. shiftMask .|. m, k), windows $ f i)
-  | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_5]
+  [ ((modm .|. m, k), windows $ f i)
+  | (i, k) <-
+      zip (XMonad.workspaces conf) $ take (length myWorkspaces) [xK_1 .. xK_9]
   , (f, m) <- [(W.greedyView, 0), (W.shift, controlMask)]
   ] ++
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-  [ ( (modm .|. shiftMask .|. m, key)
-    , screenWorkspace sc >>= flip whenJust (windows . f))
+  [ ((modm .|. m, key), screenWorkspace sc >>= flip whenJust (windows . f))
   | (key, sc) <- zip [xK_w, xK_e, xK_r] [0 ..]
   , (f, m) <- [(W.view, 0), (liftM2 (.) W.view W.shift, controlMask)]
   ]
