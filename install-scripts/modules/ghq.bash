fb5806158375413793a6ebdfc836541077156c5d
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:06:47 +0000

    Update ghq installer
    Install ghq on macOS using Homebrew.

--- install-scripts/modules/ghq.bash
+++ install-scripts/modules/ghq.bash
@@ -1,6 +1,8 @@
 # ghq.bash --- ghq installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_install_facade ghq
+elif is_arch; then
     trizen_sync_facade ghq
 fi
