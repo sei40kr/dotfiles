4b2ff9b54f0966dde172922e4ad8320e2e519bc8
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:34:41 +0000

    Update the Docker installer
    Install docker on macOS using Homebrew.

--- install-scripts/modules/docker.bash
+++ install-scripts/modules/docker.bash
@@ -1,7 +1,9 @@
 # docker.bash --- Docker installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_cask_install_facade docker
+elif is_arch; then
     pacman_sync_facade docker
     pacman_sync_facade docker-compose
 
