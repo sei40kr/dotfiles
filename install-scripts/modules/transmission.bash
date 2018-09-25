b9eaeba9d54347e56591e968541e99e39fccac67
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:27:13 +0000

    Update the Transmission installer
    Install transmission on macOS using Homebrew.

--- install-scripts/modules/transmission.bash
+++ install-scripts/modules/transmission.bash
@@ -1,7 +1,9 @@
 # transmission.bash --- Transmission
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_cask_install_facade transmission
+elif is_arch; then
     pacman_sync_facade transmission-cli
     pacman_sync_facade transmission-gtk
 fi
