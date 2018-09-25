e2cf65bc9237b0469960dddd59e07f39e9bedffb
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:26:03 +0000

    Update the Google Chrome installer
    Install google-chrome on macOS using Homebrew.

--- install-scripts/modules/google-chrome.bash
+++ install-scripts/modules/google-chrome.bash
@@ -1,6 +1,8 @@
 # google-chrome.bash --- Google Chrome installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_cask_install_facade google-chrome
+elif is_arch; then
     trizen_sync_facade google-chrome
 fi
