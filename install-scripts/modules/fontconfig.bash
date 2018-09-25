4f1a8cbdb3f57fb0bfb6bbb52ffbdd4472f8d80b
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 23:48:13 +0000

    Fix the Fontconfig installer
    Install fontconfig using pacman only on Arch Linux.

--- install-scripts/modules/fontconfig.bash
+++ install-scripts/modules/fontconfig.bash
@@ -1,7 +1,9 @@
 # fontconfig.bash --- Fontconfig
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-pacman_sync_facade fontconfig
+if is_arch; then
+    pacman_sync_facade fontconfig
 
-ln_facade "${DOTFILES_PATH}/fontconfig/conf.d" "${XDG_CONFIG_HOME}/fontconfig/conf.d"
-ln_facade "${DOTFILES_PATH}/fontconfig/fonts.conf" "${XDG_CONFIG_HOME}/fontconfig/fonts.conf"
+    ln_facade "${DOTFILES_PATH}/fontconfig/conf.d" "${XDG_CONFIG_HOME}/fontconfig/conf.d"
+    ln_facade "${DOTFILES_PATH}/fontconfig/fonts.conf" "${XDG_CONFIG_HOME}/fontconfig/fonts.conf"
+fi
