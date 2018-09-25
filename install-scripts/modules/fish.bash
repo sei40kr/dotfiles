51f6a2dba03843c42d089bedcee02510694146e7
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:04:39 +0000

    Update fish shell installer
    Install fish on macOS using Homebrew.

--- install-scripts/modules/fish.bash
+++ install-scripts/modules/fish.bash
@@ -1,10 +1,15 @@
 # fish.bash --- fish shell
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_install_facade fish
+    brew_install_facade fzf
+elif is_arch; then
     pacman_sync_facade fish
     pacman_sync_facade fzf
+fi
 
+if is_macos || is_arch; then
     ln_facade "${DOTFILES_PATH}/fish/config.fish" "${XDG_CONFIG_HOME}/config.fish"
 
     # TODO Install other configurations
