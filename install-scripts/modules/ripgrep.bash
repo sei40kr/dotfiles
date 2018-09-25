ba2860368ba04da6171dfe7d6fb2ab276c7f2159
Seong Yong-ju <sei40kr@gmail.com>
2018-09-25 03:16:26 +0000

    Update the ripgrep installer
    Install ripgrep-bin on macOS using Homebrew.

--- install-scripts/modules/ripgrep.bash
+++ install-scripts/modules/ripgrep.bash
@@ -1,8 +1,13 @@
 # ripgrep.bash --- ripgrep installer
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-if is_arch; then
+if is_macos; then
+    brew_tap_facade burntsushi/ripgrep https://github.com/BurntSushi/ripgrep.git
+    brew_install_facade ripgrep-bin
+elif is_arch; then
     pacman_sync_facade ripgrep
+fi
 
+if is_macos || is_arch; then
     ln_facade "${DOTFILES_PATH}/ripgrep/ripgreprc" "${HOME}/.ripgreprc"
 fi
