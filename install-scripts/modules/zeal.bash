4894334be2525050ba89d5217d0144b75a97dbe4
Seong Yong-ju <sei40kr@gmail.com>
2018-09-27 13:52:08 +0900

    Update Zeal installer

--- install-scripts/modules/zeal.bash
+++
@@ -1,6 +0,0 @@
-# zeal.bash --- Zeal
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-if is_arch; then
-    pacman_sync_facade zeal
-fi
---
+++ install-scripts/modules/zeal.pl
@@ -0,0 +1,12 @@
+# zeal.pl --- Zeal
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_arch) {
+    pacman_sync('zeal');
+}
+
+1;
