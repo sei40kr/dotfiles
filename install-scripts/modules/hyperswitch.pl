15b4767e3d0a7b6da2146996c35171a4bed63cd3
Seong Yong-ju <sei40kr@gmail.com>
2018-09-27 15:18:46 +0900

    Add HyperSwitch installer

---
+++ install-scripts/modules/hyperswitch.pl
@@ -0,0 +1,12 @@
+# hyperswitch.pl --- HyperSwitch installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&macos) {
+    brew_cask_install('hyperswitch');
+}
+
+1;
