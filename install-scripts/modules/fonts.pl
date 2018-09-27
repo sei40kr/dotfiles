a18193d5b9537bd53ae2af1dd86f7af04efbba40
Seong Yong-ju <sei40kr@gmail.com>
2018-09-27 15:01:32 +0900

    Fix the Fonts installer
    Install nerd-fonts-fira-code using trizen, not pacman.

--- install-scripts/modules/fonts.pl
+++ install-scripts/modules/fonts.pl
@@ -6,12 +6,13 @@ use strict;
 use warnings;
 
 if (&is_arch) {
-    pacman_sync('nerd-fonts-fira-code');
     pacman_sync('noto-fonts');
     pacman_sync('noto-fonts-cjk');
     pacman_sync('noto-fonts-emoji');
     pacman_sync('terminus-font');
     pacman_sync('ttf-liberation');
+
+    trizen_sync('nerd-fonts-fira-code');
 }
 
 1;
