1843d3289c0e24637651c4404cb6052007ea094b
Seong Yong-ju <sei40kr@gmail.com>
2018-09-27 13:50:57 +0900

    Update Xorg installer

---
+++ install-scripts/modules/xorg.pl
@@ -0,0 +1,17 @@
+# xorg.pl --- Xorg installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_arch) {
+    pacman_sync('dbus');
+    pacman_sync('xorg');
+    pacman_sync('xorg-init');
+
+    ln('xorg/xinitrc', "${ENV{HOME}}/.xinitrc");
+    ln('xorg/xsession', "${ENV{HOME}}/.xsession");
+}
+
+1;
