ae99b333fdf791a11197c45719458293174bd20f
Seong Yong-ju <sei40kr@gmail.com>
2018-09-29 15:23:06 +0900

    Update the installer utility
    Add an internal function to clone/update a Git repository.

--- install-scripts/utility.pl
+++ install-scripts/utility.pl
@@ -4,6 +4,8 @@
 use utf8;
 use strict;
 use warnings;
+use File::Basename qw(dirname);
+use File::Path qw(rmtree);
 use Term::ANSIColor;
 
 my %options = (
@@ -76,14 +78,20 @@ sub is_verbose {
     return $options{verbose};
 }
 
-sub is_exec {
+sub exec_path {
     my $cmd = $_[0];
 
     foreach my $path (@exec_paths) {
-        return 1 if ( -e "${path}/${cmd}" );
+        return $path if ( -e "${path}/${cmd}" );
     }
 
-    return 0;
+    return undef;
+}
+
+sub is_exec {
+    my $cmd = $_[0];
+
+    return exec_path($cmd);
 }
 
 sub run_cmd {
@@ -98,18 +106,69 @@ sub run_cmd {
         my $verbose = &is_verbose;
         open $proc, '-|', @cmd;
         while (<$proc>) {
-            print if ($verbose eq 1);
-        };
+            print if ( $verbose eq 1 );
+        }
         close $proc or return 0;
     }
 
     return 1;
 }
 
-sub register_reducer {
-    my $reducer = $_[0];
+my sub git_get_url {
+    my ( $path, $remote ) = @_;
 
-    push( @reducers, $reducer );
+    my $url = `git -C "${path}" remote get-url ${remote} 2>/dev/null`;
+    if ( $? ne 0 ) {
+        return undef;
+    }
+    else {
+        chomp $url;
+        return $url;
+    }
+}
+
+my sub git_current_branch {
+    my $path = $_[0];
+    my $branch;
+
+    open PROC, '-|', qw(git -C), $path, qw(branch);
+    while ( my $line = <PROC> ) {
+          if ( substr( $line, 0, 2 ) eq '* ' ) {
+              $branch = substr $line, 2;
+              chomp $branch;
+              last;
+          }
+    }
+    close PROC;
+
+    return $branch;
+}
+
+sub git_clone_internal {
+      my ( $repo, $branch, $dest ) = @_;
+
+      my $remote_url = git_get_url( $dest, 'origin' );
+      unless ( defined($remote_url) and $remote_url eq $repo ) {
+          printf "> rmtree('%s');\n", $dest if ( &is_dry_run or &is_verbose );
+          rmtree($dest) unless (&is_dry_run);
+
+          my $parent_dir = dirname($dest);
+          printf "> mkpath('%s');\n", $parent_dir
+            if ( &is_dry_run or &is_verbose );
+          mkpath($parent_dir) unless (&is_dry_run);
+
+          run_cmd( qw(git clone -q --recurse-submodules -b),
+              $branch, $repo, $dest );
+      }
+      elsif ( &do_update and git_current_branch($dest) eq $branch ) {
+          run_cmd(qw(git pull -q --recurse-submodules=yes --ff-only -r true));
+      }
+}
+
+sub register_reducer {
+      my $reducer = $_[0];
+
+      push( @reducers, $reducer );
 }
 
 1;
