73d860f54ed2b5d3718fca266a7b6739dadd9d07
Seong Yong-ju <sei40kr@gmail.com>
2018-09-29 15:33:01 +0900

    Update the Yarn facade
    Install yarn automatically if not found.

--- install-scripts/facades/50_yarn.pl
+++ install-scripts/facades/50_yarn.pl
@@ -13,16 +13,30 @@ sub yarn_global_add {
     push( @yarn_global_add_intermediate, $pkg );
 }
 
+my sub install_yarn_or_err {
+    if (&is_macos) {
+        run_cmd(qw(brew install yarn --without-node));
+    }
+    elsif (&is_arch) {
+        run_cmd(
+            qw(sudo pacman -S --needed --noconfirm --noprogressbar --assume-installed nodejs yarn)
+        );
+    }
+    else {
+        error('yarn not found.');
+    }
+}
+
 my sub yarn_global_add_reducer {
     return if ( scalar(@yarn_global_add_intermediate) eq 0 );
 
-    is_exec('yarn') or error('yarn not found.');
+    is_exec('yarn') or &install_yarn_or_err;
 
     log_wait('Installing Yarn packages ...');
 
     my @cmd = qw(yarn global add -s --noprogress --non-interactive);
     push( @cmd, '--latest' ) if (&do_update);
-    run_cmd(@cmd, @yarn_global_add_intermediate);
+    run_cmd( @cmd, @yarn_global_add_intermediate );
 }
 
 register_reducer( \&yarn_global_add_reducer );
