6614b7c76997cb3ab374abae357c33c93427e590
Seong Yong-ju <sei40kr@gmail.com>
2018-10-03 21:16:09 +0900

    Update the gem facade
    Install rbenv and Ruby automatically if not found.

--- install-scripts/facades/50_gem.pl
+++ install-scripts/facades/50_gem.pl
@@ -13,18 +13,55 @@ sub gem_install {
     push( @gem_install_intermediate, $gem );
 }
 
+my sub install_rbenv {
+    # TODO Update rbenv itself when option --update given
+    log_wait('Installing rbenv ...');
+
+    git_clone_internal( 'https://github.com/rbenv/rbenv.git',
+        'master', "${ENV{RBENV_ROOT}}" );
+    git_clone_internal( 'https://github.com/rbenv/ruby-build.git',
+        'master', "${ENV{RBENV_ROOT}}/plugins/ruby-build" );
+}
+
+my sub ruby_stable_version {
+    my $rbenv_proc;
+    my $stable_version;
+
+    # TODO Think about a case when --dry-run and --update options given
+    open $rbenv_proc, '-|', qw(rbenv install -l);
+    while (<$rbenv_proc>) {
+        $stable_version = $1 if ( $_ =~ /^\s*((?:\d+\.){2}\d+)$/ );
+    }
+    close $rbenv_proc;
+
+    return $stable_version;
+}
+
+my sub install_ruby {
+    my $stable_version = &ruby_stable_version;
+
+    log_wait("Installing Ruby ${stable_version} ...");
+
+    run_cmd( "${ENV{RBENV_ROOT}}/bin/rbenv", qw(install -s), $stable_version );
+    run_cmd( "${ENV{RBENV_ROOT}}/bin/rbenv", qw(global),     $stable_version );
+}
+
 my sub gem_install_reducer {
     return if ( scalar(@gem_install_intermediate) eq 0 );
 
-    is_exec('gem') or error('gem not found.');
+    &install_rbenv unless ( -x "${ENV{RBENV_ROOT}}/bin/rbenv" );
+    &install_ruby if ( !-x "${ENV{RBENV_ROOT}}/shims/gem" or &do_update );
+
+    is_exec("${ENV{RBENV_ROOT}}/shims/gem") or error('gem not found.');
 
     log_wait('Installing Rubygems ...');
 
-    my @cmd = qw(gem install -q --silent);
-    push( @cmd, qw(--conservative --minimal-deps) ) unless (&do_update);
-    run_cmd(@cmd, @gem_install_intermediate);
+    my @gem_args = qw(install -q --silent);
+    push( @gem_args, qw(--conservative --minimal-deps) ) unless (&do_update);
+    run_cmd( "${ENV{RBENV_ROOT}}/shims/gem",
+        @gem_args, @gem_install_intermediate );
 }
 
-register_reducer(\&gem_install_reducer);
+register_reducer( \&gem_install_reducer );
 
 1;
