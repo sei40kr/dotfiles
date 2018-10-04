123ee3cf7627bb6826ff5de371fa981872d1a11a
Seong Yong-ju <sei40kr@gmail.com>
2018-10-04 22:10:38 +0900

    Update the Yarn facade
    Install nvm, Node.js, Yarn automatically if not found.

--- install-scripts/facades/50_yarn.pl
+++ install-scripts/facades/50_yarn.pl
@@ -13,7 +13,29 @@ sub yarn_global_add {
     push( @yarn_global_add_intermediate, $pkg );
 }
 
+my sub install_nvm {
+    log_wait('Installing nvm ...');
+
+    git_clone_internal( 'https://github.com/creationix/nvm.git',
+        'master', "${ENV{NVM_DIR}}" );
+}
+
+my sub install_node {
+    log_wait('Installing Node ...');
+
+    run_cmd( qw(sh -c),
+        "'${ENV{NVM_DIR}}/nvm.sh && nvm install --no-progress stable'" );
+    run_cmd( qw(sh -c),
+        "'${ENV{NVM_DIR}}/nvm.sh && nvm alias default stable'" );
+    run_cmd(
+        qw(sh -c),
+"'export NVM_SYMLINK_CURRENT=1 && ${ENV{NVM_DIR}}/nvm.sh && nvm use --delete-prefix default'"
+    );
+}
+
 my sub install_yarn_or_err {
+    log_wait('Installing Yarn ...');
+
     if (&is_macos) {
         run_cmd(qw(brew install yarn --without-node));
     }
@@ -32,10 +54,16 @@ my sub install_yarn_or_err {
 my sub yarn_global_add_reducer {
     return if ( scalar(@yarn_global_add_intermediate) eq 0 );
 
-    is_exec('yarn') or &install_yarn_or_err;
+    &install_nvm unless ( -x "${ENV{NVM_DIR}}/nvm.sh" );
+    my $current_node = "${ENV{NVM_DIR}}/current/bin/node";
+    &install_node unless ( -x $current_node );
+    &install_yarn_or_err unless ( is_exec('yarn') );
 
     log_wait('Installing Yarn packages ...');
 
+    error('node not found.') if ( !-x $current_node or &is_dry_run );
+
+    # TODO Use the Node.js installed via nvm
     my @cmd = qw(yarn global add -s --noprogress --non-interactive);
     push( @cmd, '--latest' ) if (&do_update);
     run_cmd( @cmd, @yarn_global_add_intermediate );
