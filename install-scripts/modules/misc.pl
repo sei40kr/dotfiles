4378c4ca7ba6029ac8709b5156591dfe2d95469d
Seong Yong-ju <sei40kr@gmail.com>
2018-10-08 02:19:45 +0900

    Aggregate the amethyst, appcleaner, kindle installers to misc

--- install-scripts/modules/misc.pl
+++ install-scripts/modules/misc.pl
@@ -12,8 +12,11 @@ if (&is_macos) {
     brew_tap('tavianator/tap');
     brew_install('bfs');
 
+    brew_cask_install('amethyst');
+    brew_cask_install('appcleaner');
     brew_cask_install('discord');
     brew_cask_install('gitter');
+    brew_cask_install('kindle');
     brew_cask_install('plex-media-server');
     brew_cask_install('slack');
 }
