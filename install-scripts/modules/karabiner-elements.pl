82480b31c66689ab56b9945b6593cb59cf0b296b
Seong Yong-ju <sei40kr@gmail.com>
2018-10-08 02:22:21 +0900

    Add Karabiner Elements installer

---
+++ install-scripts/modules/karabiner-elements.pl
@@ -0,0 +1,14 @@
+# karabiner-elements.pl --- Karabiner Elements installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_cask_install('karabiner-elements');
+
+    ln('karabiner-elements/karabiner.json', '${ENV{XDG_CONFIG_HOME}}/karabiner/karabiner.json')
+}
+
+1;
