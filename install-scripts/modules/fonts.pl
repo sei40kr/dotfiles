d00a5edc5dd4c1e4c58fe635bfc6d41843601024
Seong Yong-ju <sei40kr@gmail.com>
2018-10-08 18:51:20 +0900

    Update the fonts installer
    Add some fonts to install on macOS.

--- install-scripts/modules/fonts.pl
+++ install-scripts/modules/fonts.pl
@@ -5,16 +5,31 @@ use utf8;
 use strict;
 use warnings;
 
+# 11 Best Programming Fonts - ITNEXT
+# https://itnext.io/11-best-programming-fonts-724283a9ed57
 if (&is_macos) {
     brew_tap('homebrew/cask-fonts');
-    brew_cask_install('font-source-han-code-jp');
-    brew_cask_install('font-terminus');
-} elsif (&is_arch) {
+    brew_cask_install('font-anonymous-pro');          # Anonymous Pro
+    brew_cask_install('font-fantasque-sans-mono');    # Fantasque Sans Mono
+    brew_cask_install('font-fira-code');              # Fira Code
+    brew_cask_install('font-hack');                   # Hack
+    brew_cask_install('font-inconsolata');            # Inconsolata
+    brew_cask_install('font-inconsolata-dz');         # Inconsolata-dz
+    brew_cask_install('font-migmix-1m');              # MigMix 1M
+    brew_cask_install('font-migmix-2m');              # MigMix 2M
+    brew_cask_install('font-roboto-mono');            # Roboto Mono
+    brew_cask_install('font-source-han-code-jp');     # Source Han Code JP
+
+    # brew_cask_install('font-input');                  # Input
+    # brew_cask_install('font-inconsolata-g');          # Inconsolata-g
+}
+elsif (&is_arch) {
     pacman_sync('noto-fonts');
     pacman_sync('noto-fonts-cjk');
     pacman_sync('noto-fonts-emoji');
-    pacman_sync('terminus-font');
     pacman_sync('ttf-liberation');
+
+    # TODO Add fonts
 
     trizen_sync('nerd-fonts-fira-code');
 }
