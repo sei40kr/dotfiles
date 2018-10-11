cc60329d31935242280e40362f35620dff296b88
Seong Yong-ju <sei40kr@gmail.com>
2018-10-11 23:31:52 +0900

    Update ripgrep installer
    Create a symlink for rg completions.

--- install-scripts/modules/ripgrep.pl
+++ install-scripts/modules/ripgrep.pl
@@ -6,14 +6,21 @@ use warnings;
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 if (&is_macos) {
-    brew_tap('burntsushi/ripgrep', 'https://github.com/BurntSushi/ripgrep.git');
+    brew_tap( 'burntsushi/ripgrep',
+        'https://github.com/BurntSushi/ripgrep.git' );
     brew_install('ripgrep-bin');
-} elsif (&is_arch) {
+
+    ln(
+"${ENV{BREW_PREFIX}}/opt/ripgrep-bin/share/fish/vendor_completions.d/rg.fish",
+        "${ENV{XDG_CONFIG_HOME}}/fish/completions/rg.fish"
+    );
+}
+elsif (&is_arch) {
     pacman_sync('ripgrep');
 }
 
-if (&is_macos or &is_arch) {
-    ln('ripgrep/ripgreprc', "${ENV{HOME}}/.ripgreprc");
+if ( &is_macos or &is_arch ) {
+    ln( 'ripgrep/ripgreprc', "${ENV{HOME}}/.ripgreprc" );
 }
 
 1;
