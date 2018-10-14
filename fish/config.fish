5dec04bb90ad1d7bd6ccced75724f965f9b10ece
Seong Yong-ju <sei40kr@gmail.com>
2018-10-14 20:30:00 +0900

    Update the fish config
    Add pure config to specify colors in a prompt.

--- fish/config.fish
+++ fish/config.fish
@@ -21,3 +21,12 @@ set -g FZF_ENABLE_OPEN_PREVIEW 0
 if [ -d "$PERLBREW_ROOT" ]
     . $PERLBREW_ROOT/etc/perlbrew.fish
 end
+
+# pure
+set -g pure_color_normal (set_color 'c5c8c6')
+set -g pure_color_red (set_color 'a54242')
+set -g pure_color_green (set_color '8c9440')
+set -g pure_color_yellow (set_color 'de935f')
+set -g pure_color_blue (set_color '5f819d')
+set -g pure_color_cyan (set_color '5e8d87')
+set -g pure_color_gray (set_color '6c6c6c')
