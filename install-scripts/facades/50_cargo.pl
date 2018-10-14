46bf3985169738dafc392565bfe74310f75ce04d
Seong Yong-ju <sei40kr@gmail.com>
2018-10-14 22:50:40 +0900

    Add cargo facades

---
+++ install-scripts/facades/50_cargo.pl
@@ -0,0 +1,28 @@
+# 50_cargo.pl --- cargo facade
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+my @cargo_install_intermediate = ();
+
+sub cargo_install {
+    my $pkg = $_[0];
+
+    push( @cargo_install_intermediate, $pkg );
+}
+
+my sub cargo_install_reducer {
+    return if ( scalar(@cargo_install_intermediate) eq 0 );
+
+    log_wait('Installing Rust binaries ...');
+
+    my $cargo = "${ENV{CARGO_HOME}}/bin/cargo";
+    error('cargo not found.') unless ( is_exec($cargo) );
+
+    @cargo_args = qw(install -q);
+    push( @cargo_args, @cargo_install_intermediate );
+
+    run_cmd( $cargo, @cargo_args );
+}
+
+register_reducer( \&cargo_install_reducer );
+
+1;
