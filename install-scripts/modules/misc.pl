1fcbb2c29b8cdace456c954f12bd9a937c5d6467
Seong Yong-ju <sei40kr@gmail.com>
2018-10-15 23:30:45 +0900

    Update the misc installer
    Install dropbox, near-lock, spark.

--- install-scripts/modules/misc.pl
+++ install-scripts/modules/misc.pl
@@ -30,10 +30,13 @@ if (&is_macos) {
     brew_cask_install('amethyst');
     brew_cask_install('appcleaner');
     brew_cask_install('discord');
+    brew_cask_install('dropbox');
     brew_cask_install('gitter');
     brew_cask_install('kindle');
+    brew_cask_install('near-lock');
     brew_cask_install('plex-media-server');
     brew_cask_install('slack');
+    brew_cask_install('spark');
 }
 elsif (&is_arch) {
     pacman_sync('fd');
