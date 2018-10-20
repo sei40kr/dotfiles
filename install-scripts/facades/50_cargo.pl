1e40b8f510524d334e47b36d3fc98ca8392fecb7
Seong Yong-ju <sei40kr@gmail.com>
2018-10-20 16:33:58 +0900

    Update the Cargo facade
    Add a function to install a Cargo package using nightly version of Rust.

--- install-scripts/facades/50_cargo.pl
+++ install-scripts/facades/50_cargo.pl
@@ -2,13 +2,20 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 my @cargo_install_intermediate = ();
+my @cargo_nightly_install_intermediate = ();
 
 sub cargo_install {
     my $pkg = $_[0];
 
-    # TODO Enable to specify nightly Rust toolchain
     push( @cargo_install_intermediate, $pkg );
 }
+
+sub cargo_nightly_install {
+    my $pkg = $_[0];
+
+    push( @cargo_nightly_install_intermediate, $pkg );
+}
+
 
 my sub cargo_install_reducer {
     return if ( scalar(@cargo_install_intermediate) eq 0 );
@@ -28,6 +35,22 @@ my sub cargo_install_reducer {
     run_cmd( $cargo, @cargo_args );
 }
 
+my sub cargo_nightly_install_reducer {
+    return if ( scalar(@cargo_nightly_install_intermediate) eq 0 );
+
+    log_wait('Installing Rust binaries ...');
+
+    my $cargo = "${ENV{CARGO_HOME}}/bin/cargo";
+
+    error('cargo not found.') unless ( is_exec($cargo) );
+
+    @cargo_args = qw(+nightly install -fq);
+    push( @cargo_args, @cargo_nightly_install_intermediate );
+
+    run_cmd( $cargo, @cargo_args );
+}
+
 register_reducer( \&cargo_install_reducer );
+register_reducer( \&cargo_nightly_install_reducer );
 
 1;
