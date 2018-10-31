a33b0d786aa152de0058e728936157ff702b8104
Seong Yong-ju <sei40kr@gmail.com>
2018-10-31 00:18:48 +0000

    fix(spacemacs): load flycheck-popup-tip explicitly

--- spacemacs/my-config.el
+++ spacemacs/my-config.el
@@ -51,8 +51,8 @@
    auto-save-default nil))
 
 (defun my/config ()
-  ;; Load my snippets
   (require 'competitive-programming-snippets)
+  (require 'flycheck-popup-tip)
   (require 'jest-snippets)
   (require 'react-snippets)
   (require 'redux-snippets)
