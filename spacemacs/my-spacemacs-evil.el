e1fa8a2ba6c885e8c60e41e436b69e99058f2ba5
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 01:33:40 +0900

    feat(spacemacs): update spacemacs-evil config
    Disable C-h to delete a char backward in normal mode.

--- spacemacs/my-spacemacs-evil.el
+++ spacemacs/my-spacemacs-evil.el
@@ -4,14 +4,19 @@
   (setq
    evil-want-C-i-jump t
    evil-want-C-u-scroll t
-
    ;; evil-escape
    evil-escape-key-sequence "jk"))
 
 (defun my/config-spacemacs-evil ()
   ;; Set C-h, C-w key bindings like Vim
-  (bind-key* "C-h" #'delete-backward-char)
-  (bind-key* "C-w" #'backward-kill-word)
+  (bind-key* (kbd "C-h") nil)
+  (evil-global-set-key 'insert (kbd "C-h") #'backward-delete-char-untabify)
+  (evil-global-set-key 'visual (kbd "C-h") #'evil-backward-char)
+  (evil-global-set-key 'hybrid (kbd "C-h") #'backward-delete-char-untabify)
+  (bind-key ("C-h") #'delete-backward-char minibuffer-local-map)
   (with-eval-after-load 'company
-    (bind-key "C-h" nil company-active-map)
-    (bind-key "C-w" nil company-active-map)))
+    (bind-key (kbd "C-h") nil company-active-map)
+    (bind-key (kbd "C-w") nil company-active-map))
+  (with-eval-after-load 'helm
+   (bind-key (kbd "C-h") #'delete-backward-char helm-map)
+   (bind-key (kbd "C-w") #'backward-kill-word helm-map)))
