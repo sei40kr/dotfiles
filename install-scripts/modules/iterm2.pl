dcf4ea7def786ecc84567583c6166ac5781bea5e
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 03:53:15 +0900

    feat(installer): update iterm2 installer
    Download imgcat.

--- install-scripts/modules/iterm2.pl
+++ install-scripts/modules/iterm2.pl
@@ -8,7 +8,14 @@ use warnings;
 if (&is_macos) {
     brew_cask_install('iterm2');
 
-    ln('iterm2', "${ENV{HOME}}/iterm2_profile");
+    ln( 'iterm2', "${ENV{HOME}}/iterm2_profile" );
+
+    # Download imgcat
+    curl(
+        'https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat',
+        "${ENV{HOME}}/.local/bin/imgcat"
+    );
+    # TODO Give executable permission to imgcat
 
     # Specify the preferences directory
     defaults_write_string( 'com.googlecode.iterm2.plist', 'PrefsCustomFolder',
