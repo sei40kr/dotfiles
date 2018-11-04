0a0244d155e8910855e1deecb2846e7c249ec4be
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 10:41:31 +0900

    feat(installer): add xrasengan installer

---
+++ install-scripts/modules/xrasengan.pl
@@ -0,0 +1,20 @@
+# xrasengan.pl --- xrasengan
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/install-scripts/lib";
+
+if (&is_arch) {
+    pacman_sync('arandr');
+
+    curl(
+        'https://raw.githubusercontent.com/geyslan/xrasengan/master/xrasengan',
+        "${ENV{HOME}}/.local/bin/xrasengan"
+    );
+    # TODO Give an executable permission to xrasengan
+}
+
+1;
