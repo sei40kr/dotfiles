f2ed4408763ad9b4b1ccae560d7b66ff33383705
Seong Yong-ju <sei40kr@gmail.com>
2018-11-05 00:30:58 +0900

    feat(fish): update function __fzf_ghq

--- fish/functions/__fzf_ghq.fish
+++ fish/functions/__fzf_ghq.fish
@@ -1,6 +1,11 @@
 # __fzf_ghq.fish
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+function __fzf_ghq_cd --argument-names path
+    commandline -- cd\ (string escape $path)
+    commandline -f execute
+end
+
 function __fzf_ghq
     begin
         echo '~/.dotfiles'
@@ -8,12 +13,29 @@ function __fzf_ghq
         ghq list --full-path | while read path
             string replace $HOME '~' $path
         end
-    end | eval (__fzfcmd) $FZF_DEFAULT_OPTS | read path
+    end | eval (__fzfcmd) $FZF_DEFAULT_OPTS | read repo_path
 
-    if [ -n $path ]
-        commandline -- cd\ (string escape (string replace '~' $HOME $path))
-        commandline -f execute
+    if [ -z $repo_path ]
+        return
     end
 
-    commandline -f repaint
+    if [ -z $TMUX ]
+        __fzf_ghq_cd $repo_path
+    end
+
+    set -l repo_path (string replace '~' $HOME $repo_path)
+    set -l repo_name (basename $repo_path)
+
+    tmux switch-client -t $repo_name ^/dev/null
+    or begin
+        set -l current_session (tmux display-message -p '#S')
+
+        if not string match -qr '^\d+$' $current_session
+            tmux new-session -dc $repo_path -s $repo_name
+            tmux switch-client -t $repo_name
+        else
+            __fzf_ghq_cd $repo_path
+            tmux rename-session -t $current_session $repo_name
+        end
+    end
 end
