9703db905c691d6ba34b56f7da6eb58f5480ec75
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 13:10:05 +0900

    chore(installer): add .dir-locals.el into install-scripts
    Add .dir-locals.el to set Perl library paths.

---
+++ install-scripts/.dir-locals.el
@@ -0,0 +1,7 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((cperl-mode
+  (eval . (setq flycheck-perl-include-path (list
+                                            (concat (projectile-project-root)
+                                                    "/install-scripts/lib"))))))
