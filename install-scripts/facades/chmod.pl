c07347adf17807e68cb1ae38486423d49bd0cc36
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 22:31:16 +0900

    feat(installer): add chmod facade

---
+++ install-scripts/facades/chmod.pl
@@ -0,0 +1,40 @@
+# chmod.pl
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+my @chmod_intermediate = ();
+
+sub chmod_facade {
+    my ( $mode, $file ) = @_;
+
+    push(
+        @chmod_intermediate,
+        {
+            mode => $mode,
+            file => $file,
+        }
+    );
+}
+
+my sub chmod_reducer {
+    return if ( scalar(@chmod_intermediate) eq 0 );
+
+    log_wait('Changing file mode ...');
+
+    foreach my $item (@chmod_intermediate) {
+
+        if ( &is_dry_run || &is_verbose ) {
+            printf "> chmod %04d, '%s';\n", $item->{mode}, $item->{file};
+        }
+        unless (&is_dry_run) {
+            chmod $item->{mode}, $item->{file};
+        }
+    }
+}
+
+register_reducer( 30, \&chmod_reducer );
+
+1;
