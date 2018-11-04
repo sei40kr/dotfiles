fb2fd60bcbc20c21eaf059b62630ad8fc8c5f839
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 22:03:25 +0900

    feat(installer): update fonts installer
    Install font-input.

--- install-scripts/modules/fonts.pl
+++ install-scripts/modules/fonts.pl
@@ -15,12 +15,12 @@ if (&is_macos) {
     brew_cask_install('font-hack');                   # Hack
     brew_cask_install('font-inconsolata');            # Inconsolata
     brew_cask_install('font-inconsolata-dz');         # Inconsolata-dz
+    brew_cask_install('font-input');                  # Input
     brew_cask_install('font-migmix-1m');              # MigMix 1M
     brew_cask_install('font-migmix-2m');              # MigMix 2M
     brew_cask_install('font-roboto-mono');            # Roboto Mono
     brew_cask_install('font-source-han-code-jp');     # Source Han Code JP
 
-    # brew_cask_install('font-input');                  # Input
     # brew_cask_install('font-inconsolata-g');          # Inconsolata-g
 }
 elsif (&is_arch) {
