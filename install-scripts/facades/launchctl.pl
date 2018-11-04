3113e5c25c217db781b63c399ae2d9760b72a404
Seong Yong-ju <sei40kr@gmail.com>
2018-11-04 17:43:08 +0900

    feat(installer): update launchctl facade
    Add a facade to load a system-wide service.

--- install-scripts/facades/launchctl.pl
+++ install-scripts/facades/launchctl.pl
@@ -7,12 +7,29 @@ use warnings;
 use File::Basename qw(dirname);
 use Cwd qw(realpath);
 
-my @launchctl_load_intermediate = ();
+my @sudo_launchctl_load_intermediate = ();
+my @launchctl_load_intermediate      = ();
+
+sub sudo_launchctl_load {
+    my $src = $_[0];
+
+    push( @sudo_launchctl_load_intermediate, $src );
+}
 
 sub launchctl_load {
     my $src = $_[0];
 
     push( @launchctl_load_intermediate, $src );
+}
+
+my sub sudo_launchctl_load_reducer {
+    return if ( scalar(@sudo_launchctl_load_intermediate) eq 0 );
+
+    error('launchctl not found.') unless (&is_macos);
+
+    log_wait('Enabling launchctl system-wide services ...');
+
+    Command::run( qw(sudo launchctl load -w), @sudo_launchctl_load_intermediate );
 }
 
 my sub launchctl_load_reducer {
@@ -25,6 +42,7 @@ my sub launchctl_load_reducer {
     Command::run( qw(launchctl load -w), @launchctl_load_intermediate );
 }
 
-register_reducer( 40, \&launchctl_load_reducer );
+register_reducer( 40, \&sudo_launchctl_load_reducer );
+register_reducer( 41, \&launchctl_load_reducer );
 
 1;
