e04c5b0999d16ab767302935bf4fb57fef5e8758
Seong Yong-ju <sei40kr@gmail.com>
2018-11-05 08:25:53 +0000

    feat(installer): update curl facade
    Add curl_sh facade that runs curl and pipe it to sh.

--- install-scripts/facades/curl.pl
+++ install-scripts/facades/curl.pl
@@ -5,7 +5,8 @@ use utf8;
 use strict;
 use warnings;
 
-my @curl_intermediate = ();
+my @curl_intermediate    = ();
+my @curl_sh_intermediate = ();
 
 sub curl {
     my ( $url, $dest ) = @_;
@@ -19,6 +20,16 @@ sub curl {
     );
 }
 
+sub curl_sh {
+    my $url = $_[0];
+
+    push( @curl_intermediate, $url );
+}
+
+my sub curl_existence_check {
+    error('curl not found') unless ( is_exec('curl') );
+}
+
 my sub curl_reducer {
     my @curl_args = ();
     foreach my $item (@curl_intermediate) {
@@ -30,11 +41,23 @@ my sub curl_reducer {
 
     log_wait('Transferring data from a server ...');
 
-    error('curl not found') unless ( is_exec('curl') );
+    &curl_existence_check;
 
     Command::run( 'curl', qw(-SsLK /dev/null), @curl_args );
 }
 
+my sub curl_sh_reducer {
+    return if ( scalar(@curl_sh_intermediate) eq 0 );
+
+    log_wait('Transferring data from a server ...');
+
+    &curl_existence_check;
+
+    Command::run_pipe( [ qw(curl -SsLK /dev/null), $_->{url} ], ['sh'] )
+      foreach @curl_sh_intermediate;
+}
+
 register_reducer( 20, \&curl_reducer );
+register_reducer( 30, \&curl_sh_reducer );
 
 1;
