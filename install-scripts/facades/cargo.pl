9b91f771fdff7b42cb39d3ed52cdaeee004b9dda
Seong Yong-ju <sei40kr@gmail.com>
2018-11-07 22:32:30 +0900

    feat(installer): update cargo facade
    Print a warning before running Cargo without --update option.

--- install-scripts/facades/cargo.pl
+++ install-scripts/facades/cargo.pl
@@ -4,6 +4,9 @@
 use utf8;
 use strict;
 use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/install-scripts/lib";
+use InstallHelper::Logger;
 
 my @cargo_install_intermediate         = ();
 my @cargo_nightly_install_intermediate = ();
@@ -30,11 +33,9 @@ my sub cargo_install_reducer {
     # TODO Install rustup
     error('cargo not found.') unless ( is_exec($cargo) );
 
-    # TODO Skip update checking unless --update given
-    my @cargo_args = qw(install -fq);
-    push( @cargo_args, @cargo_install_intermediate );
+    log_warn("cargo can't skip checking package updates.") unless (&do_update);
 
-    Command::run( $cargo, @cargo_args );
+    Command::run( $cargo, qw(install -fq), @cargo_install_intermediate );
 }
 
 my sub cargo_nightly_install_reducer {
@@ -46,10 +47,13 @@ my sub cargo_nightly_install_reducer {
 
     error('cargo not found.') unless ( is_exec($cargo) );
 
-    my @cargo_args = qw(+nightly install -fq);
-    push( @cargo_args, @cargo_nightly_install_intermediate );
+    log_warn("cargo can't skip checking package updates.") unless (&do_update);
 
-    Command::run( $cargo, @cargo_args );
+    Command::run(
+        $cargo,
+        qw(+nightly install -fq),
+        @cargo_nightly_install_intermediate
+    );
 }
 
 register_reducer( 61, \&cargo_install_reducer );
