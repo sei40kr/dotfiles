2d096d1493fbd86a0bd20bb0a2a9b65ea8454dfb
Seong Yong-ju <sei40kr@gmail.com>
2018-11-10 00:30:43 +0900

    feat(fish): update function __fzf_edit_dotfile

--- fish/functions/__fzf_edit_dotfile.fish
+++ fish/functions/__fzf_edit_dotfile.fish
@@ -2,17 +2,7 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 function __fzf_edit_dotfile
-    if [ -z "$DOTFILES_PATH" ]
-        echo 'ERROR: $DOTFILES_PATH not defined.' >&2
-        return 1
-    end
-
-    if not command -qs fzf
-        echo 'ERROR: fzf not found.' >&2
-        return 1
-    end
-
-    git -C "$DOTFILES_PATH" ls-files -co --exclude-standard | eval (__fzfcmd) $FZF_DEFAULT_OPTS | read dotfile
+    git -C ~/.dotfiles ls-files -co --exclude-standard | eval (__fzfcmd) $FZF_DEFAULT_OPTS | read dotfile
     and commandline -- $EDITOR\ $dotfile
     and commandline -f execute
 end
