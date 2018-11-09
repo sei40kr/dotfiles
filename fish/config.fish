e70d494a5aef8caa37090d6fa3a4ec6e8962ba9d
Seong Yong-ju <sei40kr@gmail.com>
2018-11-10 00:21:17 +0900

    feat(fish): update fish config
    Remove some variables.

--- fish/config.fish
+++ fish/config.fish
@@ -3,18 +3,11 @@
 
 set -U fish_term24bit 1
 
-# dotfiles
 set -g EDITOR emacs
-set -g DOTFILES_PATH (dirname (dirname (realpath (status --current-filename))))
-
-# fish-ghq
-set -g GHQ_SELECTOR fzf
 
 # fzf
-set -g FZF_DEFAULT_OPTS '--height 40'
 set -g FZF_FIND_FILE_OPTS '--reverse --inline-info'
 set -g FZF_TMUX 0
-set -g FZF_COMPLETE 0
 set -g FZF_ENABLE_OPEN_PREVIEW 0
 
 # perlbrew
