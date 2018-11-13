1f17d01f636670d4594ecd601ebd198394a812f3
Seong Yong-ju <sei40kr@gmail.com>
2018-11-13 01:36:18 +0000

    feat(installer): remove old clang, jetbrains-toolbox installer

--- install-scripts/modules/clang.bash
+++
@@ -1,8 +0,0 @@
-# clang.bash --- Clang installer
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-if is_arch; then
-    pacman_sync_facade clang
-
-    ln_facade "${DOTFILES_PATH}/clang-format/clang-format" "${HOME}/.clang-format"
-fi
--- install-scripts/modules/jetbrains-toolbox.bash
+++
@@ -1,8 +0,0 @@
-# jetbrains-toolbox.bash --- JetBrains Toolbox
-# author: Seong Yong-ju <sei40kr@gmail.com>
-
-if is_macos; then
-    brew_cask_install_facade jetbrains-toolbox
-elif is_arch; then
-    trizen_sync_facade jetbrains-toolbox
-fi
