73b3c4ebde37d66455d509705836700586c0b211
Seong Yong-ju <sei40kr@gmail.com>
2018-12-14 18:00:19 +0900

    feat(installer): update rustup facade
    Add a facade for `rustup toolchain install`.

--- install-scripts/facades/rustup.pl
+++ install-scripts/facades/rustup.pl
@@ -5,7 +5,14 @@ use utf8;
 use strict;
 use warnings;
 
-my %rustup_component_add_intermediate = ();
+my @rustup_toolchain_install_intermediate = ();
+my %rustup_component_add_intermediate     = ();
+
+sub rustup_toolchain_install {
+    my $toolchain = $_[0];
+
+    push( @rustup_toolchain_install_intermediate, $toolchain );
+}
 
 sub rustup_component_add {
     my ( $component, $toolchain ) = @_;
@@ -16,22 +23,48 @@ sub rustup_component_add {
     push( @{ $rustup_component_add_intermediate{$toolchain} }, $component );
 }
 
+my sub find_rustup_exec {
+    foreach my $dirpath ("${ENV{HOME}}/.cargo/bin") {
+        return "${dirpath}/rustup" if ( -x "${dirpath}/rustup" );
+    }
+}
+
+my sub rustup_toolchain_install_reducer {
+    return if ( scalar( (@rustup_toolchain_install_intermediate) ) eq 0 );
+
+    log_wait('Installing Rust toolchains ...');
+
+    my $rustup_exec = &find_rustup_exec;
+    error('rustup is not installed.') unless ( defined($rustup_exec) );
+
+    log_warn(
+"rustup can't skip checking updates for these Rust versions: stable, beta, and nightly."
+      )
+      if ( !&do_update
+        && grep( /^stable|beta|nightly$/,
+            @rustup_toolchain_install_intermediate ) );
+    Command::run( $rustup_exec, 'toolchain', 'install', $_ )
+      foreach @rustup_toolchain_install_intermediate;
+}
+
 my sub rustup_component_add_reducer {
     return if ( scalar( ( keys %rustup_component_add_intermediate ) ) eq 0 );
 
-    log_wait('Adding Rust toolchain components ...');
+    log_wait("Adding the Rust toolchains' components ...");
 
-    error('rustup not found.') unless ( is_exec('rustup') );
+    my $rustup_exec = &find_rustup_exec;
+    error('rustup is not installed.') unless ( defined($rustup_exec) );
 
-    log_warn("rustup can't skip checking component updates.")
+    log_warn("rustup can't skip checking toolchains' component updates.")
       unless (&do_update);
 
     foreach my $toolchain ( keys %rustup_component_add_intermediate ) {
-        Command::run( qw(rustup component add --toolchain),
+        Command::run( $rustup_exec, 'component', 'add', '--toolchain',
             $toolchain, @{ $rustup_component_add_intermediate{$toolchain} } );
     }
 }
 
+register_reducer( 50, \&rustup_toolchain_install_reducer );
 register_reducer( 60, \&rustup_component_add_reducer );
 
 1;
