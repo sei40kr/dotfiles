db309882fbe5038d9dee414d4c7fa37f40e5d7f1
Seong Yong-ju <sei40kr@gmail.com>
2018-12-14 17:50:10 +0900

    feat(installer): add goenv facade

---
+++ install-scripts/facades/goenv.pl
@@ -0,0 +1,55 @@
+# goenv.pl --- goenv facade
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+my @goenv_install_intermediate = ();
+my $goenv_global_intermediate;
+
+sub goenv_install {
+    my $version = $_[0];
+
+    push( @goenv_install_intermediate, $version );
+}
+
+sub goenv_global {
+    $goenv_global_intermediate = $_[0];
+}
+
+my sub find_goenv_exec {
+    foreach
+      my $dirpath ( "${ENV{HOME}}/.goenv/bin", "/usr/local/bin", "/usr/bin" )
+    {
+        return "${dirpath}/goenv" if ( -x "${dirpath}/goenv" );
+    }
+}
+
+my sub goenv_install_reducer {
+    return if ( scalar(@goenv_install_intermediate) eq 0 );
+
+    log_wait('Installing Go ...');
+
+    my $goenv_exec = &find_goenv_exec;
+    error('goenv is not installed.') unless ( defined($goenv_exec) );
+
+    Command::run( $goenv_exec, 'install', '-s', $_ )
+      foreach @goenv_install_intermediate;
+}
+
+my sub goenv_global_reducer {
+    return unless ( defined($goenv_global_intermediate) );
+
+    log_wait('Setting the global Go version ...');
+
+    my $goenv_exec = &find_goenv_exec;
+    error('goenv is not installed.') unless ( defined($goenv_exec) );
+
+    Command::run( $goenv_exec, 'global', $goenv_global_intermediate );
+}
+
+register_reducer( 50, \&goenv_install_reducer );
+register_reducer( 51, \&goenv_global_reducer );
+
+1;
