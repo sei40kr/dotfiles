6a936f39b13f688488e45c46b91f8efb525030c7
Seong Yong-ju <sei40kr@gmail.com>
2018-12-14 17:59:16 +0900

    chore(installer): update gem facade
    Remove rbenv, Ruby installation.

--- install-scripts/facades/gem.pl
+++ install-scripts/facades/gem.pl
@@ -13,40 +13,6 @@ sub gem_install {
     push( @gem_install_intermediate, $gem );
 }
 
-my sub install_rbenv {
-
-    # TODO Update rbenv itself when option --update given
-    log_wait('Installing rbenv ...');
-
-    git_clone_internal( 'https://github.com/rbenv/rbenv.git',
-        'master', "${ENV{RBENV_ROOT}}" );
-    git_clone_internal( 'https://github.com/rbenv/ruby-build.git',
-        'master', "${ENV{RBENV_ROOT}}/plugins/ruby-build" );
-}
-
-my sub ruby_stable_version {
-    my $rbenv_proc;
-    my $stable_version;
-
-    # TODO Think about a case when --dry-run and --update options given
-    open $rbenv_proc, '-|', qw(rbenv install -l);
-    while (<$rbenv_proc>) {
-        $stable_version = $1 if ( $_ =~ /^\s*((?:\d+\.){2}\d+)$/ );
-    }
-    close $rbenv_proc;
-
-    return $stable_version;
-}
-
-my sub install_ruby {
-    my $stable_version = &ruby_stable_version;
-
-    log_wait("Installing Ruby ${stable_version} ...");
-
-    Command::run( "${ENV{RBENV_ROOT}}/bin/rbenv", qw(install -s), $stable_version );
-    Command::run( "${ENV{RBENV_ROOT}}/bin/rbenv", 'global',       $stable_version );
-}
-
 my sub gem_install_reducer {
     return if ( scalar(@gem_install_intermediate) eq 0 );
 
@@ -58,7 +24,7 @@ my sub gem_install_reducer {
 
     error('gem not found.') unless ( -x $gem or &is_dry_run );
 
-    my @gem_args = qw(install -q --silent);
+    my @gem_args = qw(install -q --silent --norc);
     push( @gem_args, qw(--conservative --minimal-deps) ) unless (&do_update);
     Command::run( $gem, @gem_args, @gem_install_intermediate );
 }
