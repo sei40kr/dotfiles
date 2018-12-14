c2da44b6300c9513179b98e6aeacf6f08bef46dc
Seong Yong-ju <sei40kr@gmail.com>
2018-12-14 17:58:43 +0900

    refactor(installer): update pyenv installer
    Stop using $PYENV_ROOT.

--- install-scripts/modules/pyenv.pl
+++ install-scripts/modules/pyenv.pl
@@ -5,10 +5,10 @@ use utf8;
 use strict;
 use warnings;
 
-git_clone( 'https://github.com/pyenv/pyenv.git', "${ENV{PYENV_ROOT}}" );
+git_clone( 'https://github.com/pyenv/pyenv.git', "${ENV{HOME}}/.pyenv" );
 git_clone(
     'https://github.com/pyenv/pyenv-virtualenv.git',
-    "${ENV{PYENV_ROOT}}/plugins/pyenv-virtualenv"
+    "${ENV{HOME}}/.pyenv/plugins/pyenv-virtualenv"
 );
 
 1;
