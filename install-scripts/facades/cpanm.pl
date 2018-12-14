ccc2860bc3ce05417133e7f6e998e75fbf68bd99
Seong Yong-ju <sei40kr@gmail.com>
2018-12-14 18:03:21 +0900

    chore(installer): update cpanm facade
    Remove perlbrew existence check.

--- install-scripts/facades/cpanm.pl
+++ install-scripts/facades/cpanm.pl
@@ -13,17 +13,25 @@ sub cpanm {
     push( @cpanm_intermediate, $module );
 }
 
+my sub find_cpanm_exec {
+    foreach my $dirpath ( "${ENV{HOME}}/perl5/perlbrew/bin", "/usr/local/bin",
+        "/usr/bin" )
+    {
+        return "${dirpath}/cpanm" if ( -x "${dirpath}/cpanm" );
+    }
+}
+
 my sub cpanm_reducer {
     return if ( scalar(@cpanm_intermediate) eq 0 );
 
-    error('perlbrew not found.')
-      unless ( -x "${ENV{PERLBREW_ROOT}}/bin/perlbrew" or &is_dry_run );
-    my $cpanm = "${ENV{PERLBREW_ROOT}}/bin/cpanm";
-    error('cpanm not found.') unless ( -x $cpanm or &is_dry_run );
-
     log_wait('Installing CPAN modules ...');
 
-    Command::run( $cpanm, '-q', @cpanm_intermediate );
+    my $cpanm_exec = &find_cpanm_exec;
+    error('cpanm is not installed.') unless ( defined($cpanm_exec) );
+
+    log_warn("cpanm can't skip checking updates.") unless (&do_update);
+
+    Command::run( $cpanm_exec, '-q', @cpanm_intermediate );
 }
 
 register_reducer( 61, \&cpanm_reducer );
