d469cf13c7b8479c1a323422e4673be6d2c80516
Seong Yong-ju <sei40kr@gmail.com>
2018-12-15 11:30:44 +0900

    feat(spacemacs): remove sql-format-buffer

--- spacemacs/my-format-all.el
+++ spacemacs/my-format-all.el
@@ -4,12 +4,6 @@
   (interactive)
   (format-all-buffer))
 
-(defun spacemacs/sql-format-buffer ()
-  (interactive)
-  (format-all-buffer))
-
 (defun my/config-format-all ()
   (spacemacs/set-leader-keys-for-major-mode 'sh-mode
-    "=" #'spacemacs/sh-format-buffer)
-  (spacemacs/set-leader-keys-for-major-mode 'sql-mode
-    "=" #'spacemacs/sql-format-buffer))
+    "=" #'spacemacs/sh-format-buffer))
