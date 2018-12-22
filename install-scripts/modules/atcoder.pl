10c0775f6e70e6e858105707861d09884d9fb773
Seong Yong-ju <sei40kr@gmail.com>
2018-12-22 19:25:09 +0900

    feat(installer): update atcoder installer
    Install Go, Node, Python, Ruby.

--- install-scripts/modules/atcoder.pl
+++ install-scripts/modules/atcoder.pl
@@ -5,6 +5,21 @@ use utf8;
 use strict;
 use warnings;
 
+# cf https://language-test-201603.contest.atcoder.jp
+
+# Go
+goenv_install('1.6');
+
+# Node
+nvm_install('v5.12.0');
+
+# Python
+pyenv_install('3.4.3');
+
+# Ruby
+rbenv_install('2.3.3');
+
+# Rust
 rustup_toolchain_install('1.15.1');
 rustup_component_add('rust-src', '1.15.1');
 
