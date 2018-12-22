3dba4fefb8605b8ae5f020292cafc2ab8688cc01
Seong Yong-ju <sei40kr@gmail.com>
2018-12-22 19:20:07 +0900

    feat(installer): add atcoder environment installer

---
+++ install-scripts/modules/atcoder.pl
@@ -0,0 +1,11 @@
+# atcoder.pl
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+rustup_toolchain_install('1.15.1');
+rustup_component_add('rust-src', '1.15.1');
+
+1;
