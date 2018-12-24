b0a891dc46a6899084d8fe9750b48464b8bda67b
Seong Yong-ju <sei40kr@gmail.com>
2018-12-24 19:59:14 +0900

    Update the file template for Python

--- spacemacs/templates/99:.*.py
+++ spacemacs/templates/99:.*.py
@@ -1,19 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
-"""
-`(file-name-nondirectory (buffer-file-name))` --- $0
-"""
-
-__author__ = 'Seong Yong-ju'
-__copyright__ = 'Copyright (c) 2018 Seong Yong-ju.'
-__maintainer__ = 'Seong Yong-ju'
-__email__ = 'sei40kr@gmail.com'
 
 import sys
-
 
 def main():
     """
     """
-    if __name__ == '__main__':
-        sys.exit(main(sys.argv[1:]))
+
+if __name__ == '__main__':
+    main()
