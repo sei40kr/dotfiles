1d3b590251534743125e0be013357c822632511f
Seong Yong-ju <sei40kr@gmail.com>
2019-01-13 02:18:56 +0900

    refactor(fish): refactor alias config
    Remove redundant block.

--- fish/conf.d/alias.fish
+++ fish/conf.d/alias.fish
@@ -2,13 +2,11 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 function save_alias_funcs
-    begin
-        set -l dir (realpath (dirname (status filename))'/../alias_defs')
+    set -l dir (realpath (dirname (status filename))'/../alias_defs')
 
-        source $dir'/alias_def.fish'
+    source $dir'/alias_def.fish'
 
-        for name in arch docker docker_compose go homebrew java nmap node perl python rsync ruby
-            source $dir'/alias_def_'$name'.fish'
-        end
+    for name in arch docker docker_compose go homebrew java nmap node perl python rsync ruby
+        source $dir'/alias_def_'$name'.fish'
     end
 end
