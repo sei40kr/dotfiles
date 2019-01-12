c5672835e4aab3e803f03317f73d5537024d4aad
Seong Yong-ju <sei40kr@gmail.com>
2019-01-12 20:00:07 +0900

    feat(installer): add spotify installer

---
+++ install-scripts/modules/spotify.pl
@@ -0,0 +1,12 @@
+# spotify.pl --- Spotify installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_cask_install('spotify');
+}
+
+1;
