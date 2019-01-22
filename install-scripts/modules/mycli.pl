883e13e16670ebc7afb74d6d259ac4d4ddbad605
Seong Yong-ju <sei40kr@gmail.com>
2019-01-22 19:38:17 +0900

    feat(installer): add mycli installer

---
+++ install-scripts/modules/mycli.pl
@@ -0,0 +1,14 @@
+# mycli.pl --- mycli installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/install-scripts/lib";
+use InstallHelper::Path;
+
+pip3_install('mycli');
+ln( dotfile('mycli/myclirc'), "${ENV{HOME}}/.myclirc" );
+
+1;
