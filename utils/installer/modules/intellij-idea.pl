1e63548fe90ec399ed8f28e34b02fb9cdb4809d6
Seong Yong-ju <sei40kr@gmail.com>
2019-01-30 01:15:56 +0900

    feat(intellij-idea): add and install .ideavimrc

--- utils/installer/modules/intellij-idea.pl
+++ utils/installer/modules/intellij-idea.pl
@@ -1,8 +1,16 @@
 # intellij-idea.pl --- IntelliJ IDEA
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/utils/installer/lib";
+use InstallHelper::Path;
+
 if (&is_macos) {
     brew_cask_install('intellij-idea');
+    ln( dotfile('intellij-idea/ideavimrc'), "${ENV{HOME}}/.ideavimrc" );
 }
 
 1;
