b9bbd8e4554b6cfb88b97299b7f1f3eeaf5240fe
Seong Yong-ju <sei40kr@gmail.com>
2019-01-31 10:58:04 +0900

    refactor(spacemacs): format plantuml config

--- spacemacs/my-plantuml.el
+++ spacemacs/my-plantuml.el
@@ -2,7 +2,7 @@
 
 (defun my/init-plantuml ()
   (if (eq system-type 'darwin)
-      (setq
-       plantuml-jar-path "/usr/local/opt/plantuml/libexec/plantuml.jar"
-       org-plantuml-jar-path "/usr/local/opt/plantuml/libexec/plantuml.jar"))
+      (setq plantuml-jar-path "/usr/local/opt/plantuml/libexec/plantuml.jar"
+            org-plantuml-jar-path "/usr/local/opt/plantuml/libexec/plantuml.jar")
+    )
   (add-to-list 'auto-mode-alist '("\\.pu\\'" . plantuml-mode)))
