aac919bf2122cf911705d005374c7b93461c0eb8
Seong Yong-ju <sei40kr@gmail.com>
2019-02-03 10:41:24 +0900

    feat(spacemacs): change the font

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -353,7 +353,7 @@ It should only modify the values of Spacemacs settings."
 
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
-   dotspacemacs-default-font '("Input"
+   dotspacemacs-default-font '("Menlo for Powerline"
                                :size 18
                                :weight normal
                                :width normal
