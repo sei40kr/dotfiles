486e689a7ebf03cb833820b7455e608647f047e1
Seong Yong-ju <sei40kr@gmail.com>
2019-02-11 13:45:44 +0900

    feat(zsh): improve shescape

--- zsh/func_defs.zsh
+++ zsh/func_defs.zsh
@@ -1,17 +1,6 @@
 # func_defs.zsh
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-# @kawaz's shell string escaping
-# https://qiita.com/kawaz/items/f8d68f11d31aa3ea3d1c
 shescape() {
-    local s
-    local bufs=()
-    local q="'"
-    local qq='"'
-
-    for s in "$@"; do
-        bufs+=("'${s//$q/$q$qq$q$qq$q}'")
-    done
-
-    echo "${bufs[*]}"
+    printf '%q' "$@"
 }
