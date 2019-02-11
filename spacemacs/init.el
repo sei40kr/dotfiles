8b39e4957d8416c562f1adf174e93fae6dc2adde
Seong Yong-ju <sei40kr@gmail.com>
2019-02-11 18:53:39 +0900

    feat(spacemacs): magit shows zplugin plugin repos

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -138,6 +138,7 @@ values."
                                    ("Path" 99 magit-repolist-column-path nil))
           magit-repository-directories '(("~/.dotfiles" . 0)
                                          ("~/.zsh/plugins" . 1)
+                                         ("~/.zplugin/plugins" . 1)
                                          ("~/.config/fish/plugins" . 1)
                                          ("~/.emacs.d" . 0)
                                          ("~/.emacs.d/private/layers" . 1)
