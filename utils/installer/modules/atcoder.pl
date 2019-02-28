1c4d9ceab2e88efae68fde12057d6838655d5c93
Seong Yong-ju <sei40kr@gmail.com>
2019-03-01 01:00:53 +0900

    feat(atcoder): add atcoder-tools

--- utils/installer/modules/atcoder.pl
+++ utils/installer/modules/atcoder.pl
@@ -4,6 +4,9 @@
 use utf8;
 use strict;
 use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/utils/installer/lib";
+use Install::PathResolver;
 
 # cf https://language-test-201603.contest.atcoder.jp
 
@@ -21,8 +24,15 @@ rbenv_install('2.3.3');
 
 # Rust
 rustup_toolchain_install('1.15.1');
-rustup_component_add('rls-preview', '1.15.1');
-rustup_component_add('rust-analysis', '1.15.1');
-rustup_component_add('rust-src', '1.15.1');
+rustup_component_add( 'rls-preview',   '1.15.1' );
+rustup_component_add( 'rust-analysis', '1.15.1' );
+rustup_component_add( 'rust-src',      '1.15.1' );
+
+# atcoder-tools
+pip3_install('atcoder-tools');
+ln(
+    dotfile('atcoder-tools/atcodertools.toml'),
+    "${ENV{HOME}}/.atcodertools.toml"
+);
 
 1;
