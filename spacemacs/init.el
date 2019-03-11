26de6e524f6da95814dc721682eeb46d26e5fe06
Seong Yong-ju <sei40kr@gmail.com>
2019-03-11 23:24:42 +0900

    feat(spacemacs): use hybrid key bindings

--- spacemacs/init.el
+++ spacemacs/init.el
@@ -293,7 +293,7 @@ It should only modify the values of Spacemacs settings."
    ;; with `:variables' keyword (similar to layers). Check the editing styles
    ;; section of the documentation for details on available variables.
    ;; (default 'vim)
-   dotspacemacs-editing-style 'vim
+   dotspacemacs-editing-style 'hybrid
 
    ;; If non-nil output loading progress in `*Messages*' buffer. (default nil)
    dotspacemacs-verbose-loading nil
--- spacemacs/my-spacemacs-evil.el
+++ spacemacs/my-spacemacs-evil.el
@@ -8,8 +8,10 @@
 
 (defun my/config-spacemacs-evil ()
   (define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
-  (bind-key (kbd "C-s") nil)
-  (with-eval-after-load 'company
-    (bind-key (kbd "C-w") nil company-active-map))
-  (with-eval-after-load 'helm
-    (bind-key (kbd "C-w") #'backward-kill-word helm-map)))
+
+  (eval-after-load 'clean-aindent-mode
+    '(bind-key (kbd "C-w") #'clean-aindent--bsunindent))
+  (eval-after-load 'company
+    '(bind-key (kbd "C-w") nil company-active-map))
+  (eval-after-load 'helm
+    '(bind-key (kbd "C-w") #'backward-kill-word helm-map)))
