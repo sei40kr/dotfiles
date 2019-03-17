e192927e425379effe8443e394f771ba5016ef7c
Seong Yong-ju <sei40kr@gmail.com>
2019-03-17 15:12:16 +0900

    feat(spacemacs): set default options for org-export

--- spacemacs/my-org.el
+++ spacemacs/my-org.el
@@ -1,11 +1,13 @@
 ;; -*- lexical-binding: t -*-
 
 (defun my/init-org ()
-  (setq
-   org-confirm-babel-evaluate nil
-   ;; org-bullets
-   org-bullets-bullet-list '("" "" "" "" "" "" "" "" "" "")
-   ;; ox-reveal
-   org-reveal-root (concat (getenv "HOME") "/org/reveal-js"))
+  (setq org-confirm-babel-evaluate nil
+        org-export-with-section-numbers nil
+        org-export-with-toc nil
+        org-export-preserve-breaks t
+        ;; org-bullets
+        org-bullets-bullet-list '("" "" "" "" "" "" "" "" "" "")
+        ;; ox-reveal
+        org-reveal-root (concat (getenv "HOME") "/org/reveal-js"))
   ;; Display/update images in the buffer after I evaluate
   (add-hook 'org-babel-after-execute-hook #'org-display-inline-images t))
