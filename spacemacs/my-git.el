6af7f9fde5f3c9e53e27662893de8868ad576805
Seong Yong-ju <sei40kr@gmail.com>
2019-03-26 00:07:05 +0900

    chore(spacemacs): remove unused magit repository dirs

--- spacemacs/my-git.el
+++ spacemacs/my-git.el
@@ -5,12 +5,9 @@
                                  ("Version" 25 magit-repolist-column-version nil)
                                  ("Path" 99 magit-repolist-column-path nil))
         magit-repository-directories '(("~/.dotfiles" . 0)
-                                       ("~/.zsh/plugins" . 1)
                                        ("~/.zplugin/plugins" . 1)
                                        ("~/.config/fish/plugins" . 1)
                                        ("~/.emacs.d" . 0)
-                                       ("~/.emacs.d/private/layers" . 1)
-                                       ("~/.emacs.d/private/local" . 1)
                                        ("~/develop/workspace" . 1))
         magit-revision-insert-related-refs nil))
 
