ca8360752b0ea49e00b58e384e20586d899e77e3
Seong Yong-ju <sei40kr@gmail.com>
2019-04-01 00:04:16 +0900

    feat(spacemacs): configure java layer

--- spacemacs/my-java.el
+++ spacemacs/my-java.el
@@ -1,8 +1,19 @@
+(defun my//java-setup-company ()
+  (set (make-local-variable 'company-minimum-prefix-length) 1))
+
 (defun my/init-java ()
-  (setq lsp-java-save-action-organize-imports t
+  (setq lsp-java-vmargs (list "-noverify"
+                              "-Xmx1G"
+                              "-XX:+UseG1GC"
+                              "-XX:+UseStringDeduplication"
+                              (concat "-javaagent:" dotspacemacs-directory "/lombok-1.18.6.jar")
+                              (concat "-Xbootclasspath/a:" dotspacemacs-directory "/lombok-1.18.6.jar"))
+        lsp-java-save-action-organize-imports t
         lsp-java-auto-build nil
         lsp-java-format-enabled t
         lsp-java-format-settings-url "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml"
         lsp-java-import-maven-enabled t
         lsp-java-progress-report nil
-        lsp-java-completion-guess-arguments t))
+        lsp-java-completion-guess-arguments t)
+
+  (add-hook 'java-mode-hook #'my//java-setup-company))
