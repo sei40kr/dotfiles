7edf293f2025267cbe17125a13c0c8e5e3a45090
Seong Yong-ju <sei40kr@gmail.com>
2019-04-10 12:06:46 +0900

    feat(installer): add grafana and node_exporter installer

---
+++ utils/installer/modules/grafana.pl
@@ -0,0 +1,10 @@
+# grafana.pl --- Grafana installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+brew_install('grafana');
+
+1;
---
+++ utils/installer/modules/node_exporter.pl
@@ -0,0 +1,10 @@
+# node_exporter.pl --- node_exporter installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+brew_install('node_exporter');
+
+1;
