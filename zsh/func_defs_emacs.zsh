0d0a0fe2366c805bd05d2720abf2d523e63fc31e
Seong Yong-ju <sei40kr@gmail.com>
2019-04-11 11:29:02 +0900

    fix(spacemacs): fix _emacsfun

--- zsh/func_defs_emacs.zsh
+++ zsh/func_defs_emacs.zsh
@@ -2,15 +2,13 @@
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
 _emacsfun() {
-    local emacs_path="$(/usr/bin/which emacs)"
-
     if displays_graphic; then
-        emacsclient -ca "$emacs_path" \
+        emacsclient -ca '' \
                     -s ~/.emacs.d/server/server \
                     "$@"
     else
         env TERM=xterm-256color emacsclient \
-            -tca "$emacs_path" \
+            -tca '' \
             -s ~/.emacs.d/server-tui/server \
             "$@"
     fi
