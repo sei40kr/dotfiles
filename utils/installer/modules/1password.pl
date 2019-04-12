73a631baef19248b848bc3fea434ca5daddf07d8
Seong Yong-ju <sei40kr@gmail.com>
2019-04-12 11:04:34 +0900

    feat(installer): add 1password installer

---
+++ utils/installer/modules/1password.pl
@@ -0,0 +1,12 @@
+# 1password.pl --- 1Password installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_cask_install('1password');
+}
+
+1;
