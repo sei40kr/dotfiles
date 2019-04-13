280c2d94ec9dc3fd63b1b7ca73c46492480f954d
Seong Yong-ju <sei40kr@gmail.com>
2019-04-13 23:24:57 +0900

    feat(spacemacs): enable javascript-eslint checker for typescript

--- spacemacs/my-typescript.el
+++ spacemacs/my-typescript.el
@@ -8,4 +8,10 @@
   (add-hook 'typescript-mode-hook #'spacemacs/toggle-camel-case-motion-on)
   (add-hook 'typescript-tsx-mode-hook #'spacemacs/toggle-camel-case-motion-on)
 
-  (add-hook 'typescript-tsx-mode-hook #'my//typescript-setup-emmet))
+  ;; enable emmet
+  (add-hook 'typescript-tsx-mode-hook #'my//typescript-setup-emmet)
+
+  ;; enable javascript-eslint checker
+  (with-eval-after-load 'flycheck
+    (flycheck-add-mode 'javascript-eslint 'typescript-mode)
+    (flycheck-add-mode 'javascript-eslint 'typescript-tsx-mode)))
