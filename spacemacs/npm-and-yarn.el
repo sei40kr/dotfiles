b2945d296988c380534ce6ca86cd4baaf6e5ca6e
Seong Yong-ju <sei40kr@gmail.com>
2019-04-13 23:39:53 +0900

    feat(spacemacs): improve npm-and-yarn
    Do flycheck buffer after running `npm install`/`yarn add`.

--- spacemacs/npm-and-yarn.el
+++ spacemacs/npm-and-yarn.el
@@ -3,23 +3,35 @@
 
 (defun npm-and-yarn/npm-install ()
   (interactive)
-  (if-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
-      (npm-and-yarn//npm-exec (concat "install --save " (shell-quote-argument dep)))))
+  (when-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
+    (npm-and-yarn//npm-exec (concat "install --save " (shell-quote-argument dep)))
+
+    (if (bound-and-true-p flycheck-mode)
+        (flycheck-buffer))))
 
 (defun npm-and-yarn/npm-install-dev ()
   (interactive)
-  (if-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
-      (npm-and-yarn//npm-exec (concat "install --save-dev " (shell-quote-argument dep)))))
+  (when-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
+    (npm-and-yarn//npm-exec (concat "install --save-dev " (shell-quote-argument dep)))
+
+    (if (bound-and-true-p flycheck-mode)
+        (flycheck-buffer))))
 
 (defun npm-and-yarn//yarn-exec (cmd)
   (async-shell-command (concat "yarn " cmd) nil "*yarn-error*"))
 
 (defun npm-and-yarn/yarn-add ()
   (interactive)
-  (if-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
-      (npm-and-yarn//yarn-exec (concat "add " (shell-quote-argument dep)))))
+  (when-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
+    (npm-and-yarn//yarn-exec (concat "add " (shell-quote-argument dep)))
+
+    (if (bound-and-true-p flycheck-mode)
+        (flycheck-buffer))))
 
 (defun npm-and-yarn/yarn-add-dev ()
   (interactive)
-  (if-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
-      (npm-and-yarn//yarn-exec (concat "add --dev " (shell-quote-argument dep)))))
+  (when-let* ((dep (read-string "Enter package name: " (thing-at-point 'symbol))))
+    (npm-and-yarn//yarn-exec (concat "add --dev " (shell-quote-argument dep)))
+
+    (if (bound-and-true-p flycheck-mode)
+        (flycheck-buffer))))
