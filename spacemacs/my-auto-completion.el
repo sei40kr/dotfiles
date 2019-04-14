27b7f98e8ac274f6d6b96c1b2eea7e6d7c385eec
Seong Yong-ju <sei40kr@gmail.com>
2019-04-14 13:45:27 +0900

    fix(spacemacs): fix yas-new-snippet-default

--- spacemacs/my-auto-completion.el
+++ spacemacs/my-auto-completion.el
@@ -7,7 +7,8 @@
 # contributor : Seong Yong-ju <sei40kr@gmail.com>
 # key: ${2:${1:$(yas--key-from-desc yas-text)}}
 # --
-$0`(yas-escape-text yas-selected-text)`"))
+
+$0"))
 
 (defun my/config-auto-completion ()
   (evil-global-set-key 'insert (kbd "TAB") #'hippie-expand)
