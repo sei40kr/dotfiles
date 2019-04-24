12ec0dc5ffe7a7b70b47f5dabf05c4767de09b23
Seong Yong-ju <sei40kr@gmail.com>
2019-04-24 23:47:27 +0900

    feat(installer): remove unused settings for google-chrome

--- utils/installer/modules/google-chrome.pl
+++ utils/installer/modules/google-chrome.pl
@@ -7,21 +7,6 @@ use warnings;
 
 if (&is_macos) {
     brew_cask_install('google-chrome');
-
-    # Disable the all too sensitive backswipe on trackpads
-    defaults_write_bool( 'com.google.Chrome',
-        'AppleEnableSwipeNavigateWithScrolls', 0 );
-
-    # Disable the all too sensitive backswipe on Magic Mouse
-    defaults_write_bool( 'com.google.Chrome',
-        'AppleEnableMouseSwipeNavigateWithScrolls', 0 );
-
-    # Use the system-native print preview dialog
-    defaults_write_bool( 'com.google.Chrome', 'DisablePrintPreview', 1 );
-
-    # Expand the print dialog by default
-    defaults_write_bool( 'com.google.Chrome',
-        'PMPrintingExpandedStateForPrint2', 1 );
 }
 elsif (&is_arch) {
     trizen_sync('google-chrome');
