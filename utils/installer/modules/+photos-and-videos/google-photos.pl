7d50bc83ee83d3d97bf175b44ecffb8d0dda6185
Seong Yong-ju <sei40kr@gmail.com>
2019-04-29 15:43:50 +0900

    feat(installer): add google-photos module

---
+++ utils/installer/modules/+photos-and-videos/google-photos.pl
@@ -0,0 +1,12 @@
+# google-photos.pl --- Google Photos installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_cask_install('google-photos');
+}
+
+1;
