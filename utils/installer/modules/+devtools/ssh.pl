8afc8594ebdf48931da8966128bea2b552f32cfe
Seong Yong-ju <sei40kr@gmail.com>
2019-05-09 12:19:48 +0900

    feat(ssh, installer): add ssh config

---
+++ utils/installer/modules/+devtools/ssh.pl
@@ -0,0 +1,13 @@
+# ssh.pl
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+use FindBin;
+use lib "${FindBin::Bin}/utils/installer/lib";
+use Install::PathResolver;
+
+ln( dotfile('ssh/config'), "${ENV{HOME}}/.ssh/config" );
+
+1;
