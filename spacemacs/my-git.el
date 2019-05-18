9c3e3f5a9febc4863752be64dcc6725ef41641d8
Seong Yong-ju <sei40kr@gmail.com>
2019-05-19 01:35:08 +0900

    feat(spacemacs): clean up the git config

--- spacemacs/my-git.el
+++ spacemacs/my-git.el
@@ -1,12 +1,10 @@
 (defun my/init-git ()
-  (setq magit-clone-default-directory "~/develop/workspace"
-        magit-refresh-status-buffer nil
+  (setq magit-refresh-status-buffer nil
         magit-repolist-columns '(("Name" 25 magit-repolist-column-ident nil)
                                  ("Version" 25 magit-repolist-column-version nil)
                                  ("Path" 99 magit-repolist-column-path nil))
         magit-repository-directories '(("~/.dotfiles" . 0)
                                        ("~/.zplugin/plugins" . 1)
-                                       ("~/.config/fish/plugins" . 1)
                                        ("~/.emacs.d" . 0)
                                        ("~/develop/workspace" . 1))
         magit-revision-insert-related-refs nil))
