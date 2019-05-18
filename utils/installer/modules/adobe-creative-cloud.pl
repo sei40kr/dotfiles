9567fc0a3b9b8ff2d00fc2cf26519f24a265a351
Seong Yong-ju <sei40kr@gmail.com>
2019-05-18 23:55:17 +0900

    feat(installer): add adobe-creative-cloud installer

---
+++ utils/installer/modules/adobe-creative-cloud.pl
@@ -0,0 +1,12 @@
+# adobe-creative-cloud.pl --- Adobe Creative Cloud installer
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+use utf8;
+use strict;
+use warnings;
+
+if (&is_macos) {
+    brew_cask_install('adobe-creative-cloud');
+}
+
+1;
