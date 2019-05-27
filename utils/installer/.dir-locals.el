cbf413a25a122b9c29d1762da2ec417a88bc17c7
Seong Yong-ju <sei40kr@gmail.com>
2019-05-27 23:34:54 +0900

    feat: remove the installer

--- utils/installer/.dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((cperl-mode
-  (eval . (setq flycheck-perl-include-path (list
-                                            (concat (projectile-project-root)
-                                                    "/utils/installer/lib"))))))
