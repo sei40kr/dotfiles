fddfba8927b0fd6aab869598f1f18cb36acbf655
Seong Yong-ju <sei40kr@gmail.com>
2019-08-27 18:55:35 +0900

    feat(ubersicht): update clock widget

--- ubersicht/widgets/clock.widget/clock.jsx
+++ ubersicht/widgets/clock.widget/clock.jsx
@@ -3,23 +3,42 @@ export const command = "date '+%Y/%m/%d %H:%M'";
 export const refreshFrequency = 1000;
 
 export const render = ({ output }) => {
-    if (!output) {
-        return null;
-    }
+  if (!output) {
+    return null;
+  }
 
-    return output;
+  return (
+    <div className="clock">
+      <span className="clock__icon"></span>
+      <span className="clock__datetime">{output}</span>
+    </div>
+  );
 };
 
 export const className = `
 color: #bfbfbf;
-font-family: Menlo, monospace;
+font-family: 'Input Mono', monospace;
 font-size: 16px;
-height: 32px;
-line-height: 32px;
 position: absolute;
 right: 1vw;
 text-align: right;
 top: 0;
-width: 31vw;
 z-index: 100;
+
+.clock {
+  border-bottom: 3px solid #bfbfbf;
+  height: 29px;
+  line-height: 29px;
+  padding: 0 0.5em;
+
+  &__icon {
+    -webkit-font-smoothing: antialiased;
+    font-family: 'Font Awesome 5 Free Solid';
+    font-size: 1.1em;
+  }
+
+  &__datetime {
+    margin-left: 0.5em;
+  }
+}
 `;
