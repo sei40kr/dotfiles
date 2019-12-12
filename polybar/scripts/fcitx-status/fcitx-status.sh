7fb7e7faa7a97708c074fbadb7604216e05bb482
Seong Yong-ju <sei40kr@gmail.com>
2019-12-12 23:11:52 +0900

    chore(polybar): adjust the spacing around fcitx-status

--- polybar/scripts/fcitx-status/fcitx-status.sh
+++ polybar/scripts/fcitx-status/fcitx-status.sh
@@ -6,7 +6,7 @@
 dbus-monitor --profile path=/inputmethod,interface=org.freedesktop.DBus.Properties,member=PropertiesChanged |
     while read -r line; do
     if [[ "$(fcitx-remote status)" == 2 ]]; then
-        echo 'あ'
+        echo '%{O3}あ%{O2}'
     else
         echo 'Aa'
     fi
