6a2e1eebbc3c886b253888df9fd0925061495010
Seong Yong-ju <sei40kr@gmail.com>
2020-02-19 02:14:30 +0900

    refactor(zsh): custom command history

--- zsh/custom-history.zsh
+++ zsh/custom-history.zsh
@@ -1,25 +1,26 @@
 # custom-history.zsh
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-typeset -g __history_last_cmd
+typeset -g __history_line
 
-__remember_history_line() {
+__custom_history_zshaddhistory() {
     __history_line="${1%%$'\n'}"
     return 2
 }
 
-zshaddhistory_functions+=( __remember_history_line )
+zshaddhistory_functions+=( __custom_history_zshaddhistory )
 
-__save_history_line() {
+__custom_history_precmd() {
     local exit_code="$?"
     local history_line="${__history_line}"
+    __history_line=''
 
-    if [[ "$exit_code" != 0 ]]; then
+    if [[ "$exit_code" != 0 && "$exit_code" != 130 ]]; then
         return
     fi
 
     # HIST_IGNORE_SPACE
-    if [[ "$__history_line" = ' '* ]]; then
+    if [[ "$history_line" = ' '* ]]; then
         return
     fi
 
@@ -29,4 +30,4 @@ __save_history_line() {
     print -sr -- "$history_line"
 }
 
-precmd_functions+=( __save_history_line )
+precmd_functions+=( __custom_history_precmd )
