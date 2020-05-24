dd5e315468bc4b7e401b5884d30017f6301026dd
Seong Yong-ju <sei40kr@gmail.com>
2020-05-24 11:46:06 +0900

    chore(install): add assertion functions

---
+++ install/lib/assertions.bash
@@ -0,0 +1,38 @@
+# author: Seong Yong-ju <sei40kr@gmail.com>
+
+# assert_archlinux
+#
+# Assert the running platform is Arch Linux. If not, the process exits with
+# status code 1.
+#
+assert_archlinux() {
+    if ! is_arch; then
+        unsupported_platform_error
+    fi
+}
+
+# assert_macos
+#
+# Assert the running platform is macOS. If not, the process exits with
+# status code 1.
+#
+assert_macos() {
+    if ! is_macos; then
+        unsupported_platform_error
+    fi
+}
+
+# assert_command_exists COMMAND [ERROR]
+#
+# Assert a command exists. If the command is not found, the process exits with
+# status code 127.
+#
+assert_command_exists() {
+    local command="$1"
+    local error="$2"
+
+    if ! command_exists "$command"; then
+        tui-error "${error:-${command} is not found. Aborting.}"
+        exit 127
+    fi
+}
