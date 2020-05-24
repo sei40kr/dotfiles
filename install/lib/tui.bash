8c2bde35bdd3099cd941941d6aabacf0c816ad38
Seong Yong-ju <sei40kr@gmail.com>
2020-05-24 11:25:43 +0900

    refactor(install): merge install_helpers functions into tui

--- install/lib/tui.bash
+++ install/lib/tui.bash
@@ -1,6 +1,48 @@
 # tui.bash
 # author: Seong Yong-ju
 
+COLUMNS="$(tput cols)"
+
+BOLD="$(tput bold)"
+RED="$(tput setaf 1)"
+GREEN="$(tput setaf 2)"
+YELLOW="$(tput setaf 3)"
+BLUE="$(tput setaf 4)"
+PURPLE="$(tput setaf 5)"
+CYAN="$(tput setaf 6)"
+WHITE="$(tput setaf 7)"
+RESET="$(tput sgr0)"
+
+
+print_line() {
+    local empty_line="$(printf "%${COLUMNS}s")"
+
+    echo "${empty_line// /-}"
+}
+
+print_title() {
+    local title="$1"
+
+    clear
+    print_line
+    echo "# ${BOLD}${title}${RESET}"
+    print_line
+    echo ''
+}
+
+print_desc() {
+    local desc="$1"
+
+    echo "$desc"
+    echo ''
+}
+
+menu_item() {
+    local title="$1"
+
+    echo "${BOLD}${title}${RESET}"
+}
+
 print-list-item() {
     local title="$1"
 
@@ -30,4 +72,9 @@ ask-yesno() {
             tui-print-prompt "Invalid answer. ${prompt} [yn]"
         fi
     done
+}
+
+pause() {
+    print_line
+    read -sn 1 -p 'Press enter to continue ...'
 }
