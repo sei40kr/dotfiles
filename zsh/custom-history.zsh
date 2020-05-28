5ed3e2d495ca30827d1d456c77d6620e8db723fd
Seong Yong-ju <sei40kr@gmail.com>
2020-05-29 00:38:34 +0900

    refactor(zsh): minor custom-history refactoring

--- zsh/custom-history.zsh
+++ zsh/custom-history.zsh
@@ -1,33 +1,33 @@
 # custom-history.zsh
 # author: Seong Yong-ju <sei40kr@gmail.com>
 
-typeset -g __history_line
+typeset -g custom_history__last_line
 
-__custom_history_zshaddhistory() {
-    __history_line="${1%%$'\n'}"
+custom_history__zshaddhistory() {
+    custom_history__last_line="${1%%$'\n'}"
     return 2
 }
 
-zshaddhistory_functions+=( __custom_history_zshaddhistory )
+zshaddhistory_functions+=( custom_history__zshaddhistory )
 
-__custom_history_precmd() {
+custom_history__precmd() {
     local exit_code="$?"
-    local history_line="${__history_line}"
-    __history_line=''
+    local line="${custom_history__last_line}"
+    custom_history__last_line=''
 
     if [[ "$exit_code" != 0 && "$exit_code" != 130 ]]; then
         return
     fi
 
     # HIST_IGNORE_SPACE
-    if [[ "$history_line" = ' '* ]]; then
+    if [[ "$line" = ' '* ]]; then
         return
     fi
 
     # HIST_REDUCE_BLANKS
-    history_line="${(z)history_line}"
+    line="${(z)line}"
 
-    print -sr -- "$history_line"
+    print -sr -- "$line"
 }
 
-precmd_functions+=( __custom_history_precmd )
+precmd_functions+=( custom_history__precmd )
