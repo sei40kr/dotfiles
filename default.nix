8433d21377e3ebab8843b943b727a1c3750b00cb
Seong Yong-ju <sei40kr@gmail.com>
2020-06-09 15:03:40 +0900

    fix(nix): add missing default.nix

---
+++ default.nix
@@ -0,0 +1,72 @@
+device: username:
+{ config, lib, options, pkgs, ... }:
+
+{
+  imports = [ ./modules ];
+
+  nix.autoOptimiseStore = true;
+  nixpkgs.config.allowUnfree = true;
+
+  # TODO Extract user & module settings to hosts
+
+  my.userName = "sei40kr";
+  my.user = {
+    isNormalUser = true;
+    uid = 1000;
+    extraGroups = [ "wheel" "video" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
+
+  modules = {
+    desktop = {
+      xmonad.enable = true;
+
+      apps = {
+        slack.enable = true;
+        deluge = {
+          enable = true;
+          enableWebUI = true;
+        };
+        rofi.enable = true;
+        thunar.enable = true;
+      };
+
+      term.alacritty.enable = true;
+      tools.psd.enable = true;
+      i18n.japanese.enable = true;
+    };
+
+    dev = {
+      editors = {
+        emacs.enable = true;
+        idea.enable = true;
+      };
+
+      tools = {
+        git.enable = true;
+        zeal.enable = true;
+      };
+
+      cc.enable = true;
+      go.enable = true;
+      groovy.enable = true;
+      haskell.enable = true;
+      java.enable = true;
+      kotlin.enable = true;
+      python.enable = true;
+      r.enable = true;
+      ruby.enable = true;
+      rust.enable = true;
+    };
+
+    shell = {
+      tools.flexget.enable = true;
+
+      zsh.enable = true;
+      tmux.enable = true;
+
+      exa.enable = true;
+      bat.enable = true;
+    };
+  };
+}
