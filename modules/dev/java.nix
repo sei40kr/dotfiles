a3adb9ea035a2c9a26b106719a86e89e26ad14ad
Seong Yong-ju <sei40kr@gmail.com>
2020-06-13 00:36:50 +0900

    fix(nix): install jenv on a writable filesystem

--- modules/dev/java.nix
+++ modules/dev/java.nix
@@ -6,16 +6,27 @@ with lib; {
     default = false;
   };
 
-  config = mkIf config.modules.dev.java.enable
-    # TODO Enable jenv export plugin
-    (let jenv = builtins.fetchGit { url = "https://github.com/jenv/jenv.git"; };
-    in {
-      my = {
-        packages = with pkgs; [ jdk11 maven gradle ];
-        env = rec {
-          JENV_ROOT = jenv.outPath;
-          PATH = [ "${JENV_ROOT}/bin" "${JENV_ROOT}/shims" ];
-        };
+  config = mkIf config.modules.dev.java.enable (let
+    jenv = builtins.fetchGit { url = "https://github.com/jenv/jenv.git"; };
+    jenvRootFiles =
+      [ "available-plugins" "bin" "completions" "fish" "libexec" ];
+    jenvPlugins = [ "export" ];
+  in {
+    my = {
+      home.home.file = (foldl (files: name:
+        files // {
+          ".jenv/${name}".source = "${jenv.outPath}/${name}";
+        }) { } jenvRootFiles) // (foldl (files: name:
+          files // {
+            ".jenv/plugins/${name}".source =
+              "${jenv.outPath}/available-plugins/${name}";
+          }) { } jenvPlugins);
+
+      packages = with pkgs; [ jdk11 maven gradle ];
+      env = rec {
+        JENV_ROOT = "\${HOME}/.jenv";
+        PATH = [ "${JENV_ROOT}/bin" "${JENV_ROOT}/shims" ];
       };
-    });
+    };
+  });
 }
