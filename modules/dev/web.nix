b2c647b4f6b3ab0064cfee9b523ff058a1185a52
Seong Yong-ju <sei40kr@gmail.com>
2020-06-13 22:42:39 +0900

    chore(nix): install nvm in dev/web module

--- modules/dev/web.nix
+++ modules/dev/web.nix
@@ -1,26 +1,39 @@
 { config, lib, options, pkgs, ... }:
 
-with lib; {
+with lib;
+(let
+  nvm = builtins.fetchGit {
+    url = "https://github.com/nvm-sh/nvm.git";
+    rev = "258938ef66a2a49a4a400554a6dce890226ae34c"; # v0.35.3
+  };
+  nvmRootFiles = [ "bash_completion" "nvm-exec" "nvm.sh" ];
+in {
   options.modules.dev.web.enable = mkOption {
     type = types.bool;
     default = false;
   };
 
-  config = mkIf config.modules.dev.web.enable
-    (let nvm = builtins.fetchGit { url = "https://github.com/nvm-sh/nvm.git"; };
-    in {
-      # TODO Install gulp, webpack-cli
-      # TODO Install stylelint-cli, eslint-cli, eslint_d, and tslint
-      # TODO Install prettier, prettier-eslint-cli, and typescript-formatter
-      # TODO Install language servers for HTML, CSS, JavaScript, TypeScript, Vue
-      my.packages = with pkgs; [ nodejs yarn ];
+  config = mkIf config.modules.dev.web.enable {
+    my.home.home.file = foldl (files: name:
+      files // {
+        ".nvm/${name}".source = "${nvm.outPath}/${name}";
+      }) { } nvmRootFiles;
 
-      my.env.PATH = [ "\${HOME}/.yarn/bin" ];
+    # TODO Install gulp, webpack-cli
+    # TODO Install stylelint-cli, eslint-cli, eslint_d, and tslint
+    # TODO Install prettier, prettier-eslint-cli, and typescript-formatter
+    # TODO Install language servers for HTML, CSS, JavaScript, TypeScript, Vue
+    my.packages = with pkgs; [ nodejs yarn ];
 
-      modules.shell.zsh.zinitPluginsInit = ''
-        zinit light sei40kr/zsh-lazy-nvm
+    my.env = {
+      NVM_DIR = [ "\${HOME}/.nvm" ];
+      PATH = [ "\${HOME}/.yarn/bin" ];
+    };
 
-        zinit snippet OMZP::yarn/yarn.plugin.zsh
-      '';
-    });
-}
+    modules.shell.zsh.zinitPluginsInit = ''
+      zinit light sei40kr/zsh-lazy-nvm
+
+      zinit snippet OMZP::yarn/yarn.plugin.zsh
+    '';
+  };
+})
