80c48b581bb3c56211f16b2299727fc23bb04d6f
Seong Yong-ju <sei40kr@gmail.com>
2020-06-13 15:37:30 +0900

    fix(nix): add deluge-client as a flexget dep

--- modules/shell/tools/flexget.nix
+++ modules/shell/tools/flexget.nix
@@ -6,24 +6,25 @@ with lib; {
     default = false;
   };
 
-  config = mkIf config.modules.shell.tools.flexget.enable {
-    my.packages = with pkgs; [ flexget python37Packages.deluge-client ];
-
-    my.home.systemd.user.services.flexget = {
-      Unit.Description = "FlexGet Daemon";
-      Install.WantedBy = [ "multi-user.target" ];
-      Service = {
-        ExecStart = "${pkgs.flexget}/bin/flexget daemon start";
-        ExecStop = "${pkgs.flexget}/bin/flexget daemon stop";
-        ExecReload = "${pkgs.flexget}/bin/flexget daemon reload";
-        Restart = "on-failure";
-        PrivateTmp = true;
+  config = mkIf config.modules.shell.tools.flexget.enable
+    (let package = pkgs.callPackage <packages/flexget.nix> { };
+    in rec {
+      my.packages = with pkgs; [ package ];
+      my.home.xdg.configFile."flexget/config.yml" = {
+        source = <config/flexget/config.yml>;
+        onChange = "systemctl --user reload flexget.service";
       };
-    };
 
-    my.home.xdg.configFile."flexget/config.yml" = {
-      source = <config/flexget/config.yml>;
-      onChange = "systemctl --user restart flexget.service";
-    };
-  };
+      my.home.systemd.user.services.flexget = {
+        Unit.Description = "FlexGet Daemon";
+        Install.WantedBy = [ "multi-user.target" ];
+        Service = {
+          ExecStart = "${pkgs.flexget}/bin/flexget daemon start";
+          ExecStop = "${pkgs.flexget}/bin/flexget daemon stop";
+          ExecReload = "${pkgs.flexget}/bin/flexget daemon reload";
+          Restart = "on-failure";
+          PrivateTmp = true;
+        };
+      };
+    });
 }
