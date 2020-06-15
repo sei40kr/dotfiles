927d28ab63e92002594cb6605be16b24f4261d36
Seong Yong-ju <sei40kr@gmail.com>
2020-06-16 02:27:05 +0900

    refactor(nix): minor refactors for desktop/backends/dbus

--- modules/desktop/backends/dbus.nix
+++ modules/desktop/backends/dbus.nix
@@ -3,14 +3,16 @@
 with lib;
 let cfg = config.modules.desktop.backends.dbus;
 in {
-  options.modules.desktop.backends.dbus.enable = mkOption {
-    type = types.bool;
-    default = false;
-  };
+  options.modules.desktop.backends.dbus = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
 
-  options.modules.desktop.backends.dbus.packages = mkOption {
-    type = with types; listOf package;
-    default = [ ];
+    packages = mkOption {
+      type = with types; listOf package;
+      default = [ ];
+    };
   };
 
   config = mkIf cfg.enable {
