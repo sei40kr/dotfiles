fb39b518192a7b540ac622304f1702ca2758bf07
Seong Yong-ju <sei40kr@gmail.com>
2020-06-17 22:28:53 +0900

    fix(nix): make fcitx works on gtk applications

--- modules/desktop/i18n/japanese.nix
+++ modules/desktop/i18n/japanese.nix
@@ -1,6 +1,32 @@
 { config, lib, options, pkgs, ... }:
 
-with lib; {
+with lib;
+let
+  fcitx-with-plugins =
+    (with pkgs; fcitx.override { plugins = with fcitx-engines; [ mozc ]; });
+  gtk2-immodule-cache = pkgs.stdenv.mkDerivation {
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+    name = "gtk2-immodule.cache";
+    buildInputs = with pkgs; [ gtk2 fcitx-with-plugins ];
+    buildCommand = ''
+      mkdir -p $out/etc/gtk-2.0
+      GTK_PATH=${fcitx-with-plugins}/lib/gtk-2.0 gtk-query-immodules-2.0 >$out/etc/gtk-2.0/immodules.cache
+    '';
+  };
+  gtk3-immodule-cache = pkgs.stdenv.mkDerivation {
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+    name = "gtk3-immodule.cache";
+    buildInputs = with pkgs; [ gtk3 fcitx-with-plugins ];
+    buildCommand = ''
+      mkdir -p $out/etc/gtk-3.0
+      GTK_PATH=${fcitx-with-plugins}/lib/gtk-3.0 gtk-query-immodules-3.0 >$out/etc/gtk-3.0/immodules.cache
+    '';
+  };
+  fontsEnabled = config.modules.desktop.fonts.enable;
+  gtkEnabled = config.modules.desktop.gtk.enable;
+in {
   options.modules.desktop.i18n.japanese.enable = mkOption {
     type = types.bool;
     default = false;
@@ -9,30 +35,34 @@ with lib; {
   config = mkIf config.modules.desktop.i18n.japanese.enable {
     modules.desktop.backends.dbus = {
       enable = mkForce true;
-      packages = with pkgs; [ fcitx ];
+      packages = with pkgs; [ fcitx-with-plugins ];
     };
 
     my.packages = with pkgs;
-      ([
-        (fcitx.override { plugins = with pkgs.fcitx-engines; [ mozc ]; })
-        fcitx-configtool
-      ] ++ optionals config.modules.desktop.fonts.enable [ noto-fonts-cjk ]);
+      ([ fcitx-with-plugins fcitx-configtool ]
+        ++ optionals fontsEnabled [ noto-fonts-cjk ]
+        ++ optionals gtkEnabled [ gtk2-immodule-cache gtk3-immodule-cache ]);
     my.env = {
-      GTK_IM_MODULE = "fcitx";
       QT_IM_MODULE = "fcitx";
       XMODIFIERS = "@im=fcitx";
+    } // optionalAttrs gtkEnabled { GTK_IM_MODULE = mkIf gtkEnabled "fcitx"; };
+    my.home.xdg.configFile = {
+      "fcitx/config".source = <config/fcitx/config>;
+      "fcitx/conf/fcitx-classic-ui.config".text = ''
+        ${readFile <config/fcitx/conf/fcitx-classic-ui.config>}
+        # Skin Name
+        SkinType=${
+          if config.modules.themes.preferDarkTheme then "dark" else "default"
+        }
+      '';
+    } // optionalAttrs fontsEnabled {
+      "fontconfig/conf.d/70-noto-cjk.conf".source =
+        "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/70-noto-cjk.conf";
+    } // optionalAttrs gtkEnabled {
+      "gtk-2.0/immodules.cache".source =
+        "${gtk2-immodule-cache}/etc/gtk-2.0/immodules.cache";
+      "gtk-3.0/immodules.cache".source =
+        "${gtk3-immodule-cache}/etc/gtk-3.0/immodules.cache";
     };
-    my.home.xdg.configFile."fcitx/config".source = <config/fcitx/config>;
-    my.home.xdg.configFile."fcitx/conf/fcitx-classic-ui.config".text = ''
-      ${readFile <config/fcitx/conf/fcitx-classic-ui.config>}
-      # Skin Name
-      SkinType=${
-        if config.modules.themes.preferDarkTheme then "dark" else "default"
-      }
-    '';
-
-    # Fontconfig configuration
-    my.home.xdg.configFile."fontconfig/conf.d/70-noto-cjk.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/70-noto-cjk.conf";
   };
 }
