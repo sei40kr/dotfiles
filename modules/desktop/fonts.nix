0099c475086c6d38cb42479be32212fd709b0d62
Seong Yong-ju <sei40kr@gmail.com>
2020-06-17 22:53:31 +0900

    chore(nix): update fontconfig in desktop/fonts

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -9,18 +9,18 @@ with lib; {
   config = mkIf config.modules.desktop.fonts.enable {
     my.home.fonts.fontconfig.enable = mkForce true;
     my.home.xdg.configFile."fontconfig/conf.d/10-hinting-none.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/10-hinting-none.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/10-hinting-none.conf";
     my.home.xdg.configFile."fontconfig/conf.d/10-sub-pixel-rgb.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/10-sub-pixel-rgb.conf";
     my.home.xdg.configFile."fontconfig/conf.d/11-lcdfilter-default.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/11-lcdfilter-default.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/11-lcdfilter-default.conf";
     my.home.xdg.configFile."fontconfig/conf.d/66-noto-sans.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/66-noto-sans.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/66-noto-sans.conf";
     my.home.xdg.configFile."fontconfig/conf.d/66-noto-serif.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/66-noto-serif.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/66-noto-serif.conf";
     my.home.xdg.configFile."fontconfig/conf.d/66-noto-mono.conf".source =
-      "${pkgs.fontconfig_210}/share/fontconfig/conf.avail/66-noto-mono.conf";
+      "${pkgs.fontconfig}/share/fontconfig/conf.avail/66-noto-mono.conf";
 
-    my.packages = with pkgs; [ fontconfig_210 noto-fonts noto-fonts-emoji ];
+    my.packages = with pkgs; [ fontconfig noto-fonts noto-fonts-emoji ];
   };
 }
