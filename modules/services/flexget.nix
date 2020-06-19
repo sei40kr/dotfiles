080be29e7ed042b1e4a1edf5bf6755824a33b397
Seong Yong-ju <sei40kr@gmail.com>
2020-06-19 21:28:04 +0900

    refactor(nix): override existing flexget package
    Override existing flexget package to install deluge-client. Get rid of
    the package redeclaration.

--- modules/services/flexget.nix
+++ modules/services/flexget.nix
@@ -1,30 +1,35 @@
 { config, lib, options, pkgs, ... }:
 
-with lib; {
+with lib;
+let
+  package = with pkgs;
+    flexget.overrideAttrs (oldAttrs: {
+      propagatedBuildInputs = oldAttrs.propagatedBuildInputs
+        ++ [ python3Packages.deluge-client ];
+    });
+in {
   options.modules.services.flexget.enable = mkOption {
     type = types.bool;
     default = false;
   };
 
-  config = mkIf config.modules.services.flexget.enable
-    (let package = pkgs.callPackage <packages/flexget.nix> { };
-    in rec {
-      my.packages = with pkgs; [ package ];
-      my.home.xdg.configFile."flexget/config.yml" = {
-        source = <config/flexget/config.yml>;
-        onChange = "systemctl --user reload flexget.service";
-      };
+  config = mkIf config.modules.services.flexget.enable {
+    my.packages = with pkgs; [ package ];
+    my.home.xdg.configFile."flexget/config.yml" = {
+      source = <config/flexget/config.yml>;
+      onChange = "systemctl --user reload flexget.service";
+    };
 
-      my.home.systemd.user.services.flexget = {
-        Unit.Description = "FlexGet Daemon";
-        Install.WantedBy = [ "multi-user.target" ];
-        Service = {
-          ExecStart = "${pkgs.flexget}/bin/flexget daemon start";
-          ExecStop = "${pkgs.flexget}/bin/flexget daemon stop";
-          ExecReload = "${pkgs.flexget}/bin/flexget daemon reload";
-          Restart = "on-failure";
-          PrivateTmp = true;
-        };
+    my.home.systemd.user.services.flexget = {
+      Unit.Description = "FlexGet Daemon";
+      Install.WantedBy = [ "multi-user.target" ];
+      Service = {
+        ExecStart = "${pkgs.flexget}/bin/flexget daemon start";
+        ExecStop = "${pkgs.flexget}/bin/flexget daemon stop";
+        ExecReload = "${pkgs.flexget}/bin/flexget daemon reload";
+        Restart = "on-failure";
+        PrivateTmp = true;
       };
-    });
+    };
+  };
 }
