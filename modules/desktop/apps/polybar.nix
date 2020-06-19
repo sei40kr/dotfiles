6cbece5eded42523208cd5f0770f5d452fe2512b
Seong Yong-ju <sei40kr@gmail.com>
2020-06-20 01:43:47 +0900

    refactor(nix): use absolute paths for polybar scripts

--- modules/desktop/apps/polybar.nix
+++ modules/desktop/apps/polybar.nix
@@ -9,7 +9,18 @@ with lib; {
   config = mkIf config.modules.desktop.apps.polybar.enable {
     my.home.services.polybar = {
       enable = true;
-      config."section/base".include-file = "${<config/polybar/config>}";
+      config = {
+        "section/base".include-file = "${<config/polybar/config>}";
+        "module/gnome-pomodoro" = {
+          exec = "${<config/polybar/scripts/gnome-pomodoro.py>}";
+          click-left = "${pkgs.gnome3.pomodoro}/bin/gnome-pomodoro";
+        };
+        "module/fcitx" = {
+          exec = "${<config/polybar/scripts/fcitx.py>}";
+          exec-if = "[ -x ${escapeShellArg "${pkgs.fcitx}/bin/fcitx-remote"} ]";
+          click-left = "${pkgs.fcitx}/bin/fcitx-configtool";
+        };
+      };
       script = ''
         polybar top &
       '';
