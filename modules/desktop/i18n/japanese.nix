728475a1bdd47287050e303efd4933cd0e9b6adb
Seong Yong-ju <sei40kr@gmail.com>
2020-06-27 01:56:49 +0900

    nix: Fix Japanese i18n support

--- modules/desktop/i18n/japanese.nix
+++ modules/desktop/i18n/japanese.nix
@@ -1,8 +1,6 @@
 { config, lib, options, pkgs, ... }:
 
-with lib;
-let fontconfigEnabled = config.modules.desktop.config.fontconfig.enable;
-in {
+with lib; {
   options.modules.desktop.i18n.japanese.enable = mkOption {
     type = types.bool;
     default = false;
@@ -11,8 +9,10 @@ in {
   config = mkIf config.modules.desktop.i18n.japanese.enable {
     modules.desktop.tools.fcitx.enable = mkForce true;
 
-    my.packages = with pkgs; optionals fontconfigEnabled [ noto-fonts-cjk ];
+    my.packages = with pkgs;
+      optionals config.modules.desktop.fonts.enable [ noto-fonts-cjk ];
     my.home.xdg.configFile."fontconfig/conf.d/70-noto-cjk.conf".source =
-      mkIf fontconfigEnabled <config/fontconfig/70-noto-cjk.conf>;
+      mkIf config.modules.desktop.config.fontconfig.enable
+      <config/fontconfig/70-noto-cjk.conf>;
   };
 }
