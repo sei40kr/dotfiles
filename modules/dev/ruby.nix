984b8a5eed475e6153b6134c25788946bd3921c1
sei40kr <sei40kr@gmail.com>
2020-07-24 22:59:06 +0900

    nix: Remove rbenv & minor refactor

--- modules/dev/ruby.nix
+++ modules/dev/ruby.nix
@@ -1,6 +1,8 @@
 { config, lib, options, pkgs, ... }:
 
-with lib; {
+with lib;
+let cfg = config.modules.dev.ruby;
+in {
   options.modules.dev.ruby = {
     enable = mkOption {
       type = types.bool;
@@ -13,42 +15,10 @@ with lib; {
     };
   };
 
-  config = mkIf config.modules.dev.ruby.enable (let
-    cfg = config.modules.dev.ruby;
-    rbenv = builtins.fetchGit { url = "https://github.com/rbenv/rbenv.git"; };
-    rbenvRootFiles = [ "bin" "completions" "libexec" "rbenv.d" "src" ];
-    rubyBuild =
-      builtins.fetchGit { url = "https://github.com/rbenv/ruby-build.git"; };
-  in {
-    my = {
-      home.home.file = foldl (files: name:
-        files // {
-          ".rbenv/${name}".source = "${rbenv.outPath}/${name}";
-        }) { } rbenvRootFiles // {
-          ".rbenv/plugins/ruby-build".source = rubyBuild.outPath;
-        };
-
-      packages = with pkgs;
-        ([
-          ruby
-          rubyPackages.rake
-
-          # NOTE rbenv: Ruby build environment
-          #      See https://github.com/rbenv/ruby-build/wiki#suggested-build-environment
-          gcc10
-          bzip2
-          openssl
-          libyaml
-          libffi
-          readline
-          zlib
-        ] ++ optionals cfg.enableRails [ rubyPackages.rails ]);
-      env = rec {
-        RBENV_ROOT = "\${HOME}/.rbenv";
-        PATH = [ "${RBENV_ROOT}/bin" "${RBENV_ROOT}/shims" ];
-      };
-    };
-
+  config = mkIf config.modules.dev.ruby.enable {
+    my.packages = with pkgs;
+      ([ ruby rubyPackages.rake ]
+        ++ optionals cfg.enableRails [ rubyPackages.rails ]);
     my.zsh.aliases = {
       be = "bundle exec";
       bl = "bundle list";
@@ -60,11 +30,6 @@ with lib; {
       bcn = "bundle clean";
     };
     modules.shell.zsh.zinitPluginsInit = ''
-      zinit ice atclone'rbenv init - --no-rehash zsh >rbenv-init.zsh' \
-                atpull'%atclone' \
-                id-as'rbenv-init'
-      zinit light zdharma/null
-
       zinit snippet OMZP::ruby/ruby.plugin.zsh
       zinit ice as'completion' wait'''
       zinit snippet OMZP::gem/_gem
@@ -73,5 +38,5 @@ with lib; {
     '' + optionals cfg.enableRails ''
       zinit snippet OMZP::rails/rails.plugin.zsh
     '';
-  });
+  };
 }
