5e41f7bae0f37c27c0c5d13fa5a57507d386ad80
sei40kr <sei40kr@gmail.com>
2020-07-25 00:36:32 +0900

    nix: Remove goenv

--- modules/dev/go.nix
+++ modules/dev/go.nix
@@ -6,30 +6,11 @@ with lib; {
     default = false;
   };
 
-  config = mkIf config.modules.dev.go.enable (let
-    goenv = builtins.fetchGit { url = "https://github.com/syndbg/goenv.git"; };
-    goenvRootFiles = [ "bin" "completions" "libexec" "plugins" "src" ];
-  in {
-    my = {
-      home.home.file = foldl (files: name:
-        files // {
-          ".goenv/${name}".source = "${goenv.outPath}/${name}";
-        }) { } goenvRootFiles;
-
-      packages = with pkgs; [ go ];
-      env = rec {
-        GOENV_ROOT = "\${HOME}/.goenv";
-        PATH = [ "${GOENV_ROOT}/bin" "${GOENV_ROOT}/shims" ];
-      };
-    };
+  config = mkIf config.modules.dev.go.enable {
+    my.packages = with pkgs; [ go ];
 
     modules.shell.zsh.zinitPluginsInit = ''
-      zinit ice atclone'goenv init - --no-rehash zsh >goenv-init.zsh' \
-                atpull'%atclone' \
-                id-as'goenv-init'
-      zinit light zdharma/null
-
       zinit snippet OMZP::golang/golang.plugin.zsh
     '';
-  });
+  };
 }
