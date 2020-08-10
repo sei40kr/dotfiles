636a83f9de3bd5571f057c0723eea74b4538b9f9
Seong Yong-ju <sei40kr@gmail.com>
2020-08-10 21:11:48 +0900

    nix: Export Zeal docsets path for dash-docs.el

--- modules/dev/tools/zeal.nix
+++ modules/dev/tools/zeal.nix
@@ -1,12 +1,17 @@
 { config, lib, pkgs, ... }:
 
-with lib; {
+with lib;
+let dataHome = config.home-manager.users."${config.my.userName}".xdg.dataHome;
+in {
   options.modules.dev.tools.zeal.enable = mkOption {
     type = types.bool;
     default = false;
   };
 
   config = mkIf config.modules.dev.tools.zeal.enable {
+    modules.dev.editors.emacs.variables.dash-docs-docsets-path =
+      "${dataHome}/Zeal/Zeal/docsets";
+
     my.packages = with pkgs; [ zeal ];
   };
 }
