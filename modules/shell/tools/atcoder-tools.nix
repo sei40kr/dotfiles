49bd0af17cca38e9168f12bdafc534ff09c64155
Seong Yong-ju <seong_yongju@waku-2.com>
2020-08-11 12:30:34 +0900

    nix: Hide my atcoder-tools code template & generator

--- modules/shell/tools/atcoder-tools.nix
+++ modules/shell/tools/atcoder-tools.nix
@@ -1,6 +1,10 @@
 { config, lib, pkgs, ... }:
 
-with lib; {
+with lib;
+let
+  homeDirectory =
+    config.home-manager.users."${config.my.userName}".home.homeDirectory;
+in {
   options.modules.shell.tools.atcoderTools.enable = mkOption {
     type = types.bool;
     default = false;
@@ -8,11 +12,26 @@ with lib; {
 
   config = mkIf config.modules.shell.tools.atcoderTools.enable {
     my.packages = with pkgs; [ my.python3Packages.atcoder-tools ];
-    my.home.home.file = {
-      ".atcodertools.toml".source = <config/atcoder-tools/atcodertools.toml>;
-      "custom_code_generator.py".source =
-        <config/atcoder-tools/custom_code_generator.py>;
-      "my_template.rs".source = <config/atcoder-tools/my_template.rs>;
+    my.home.home.file.".atcodertools.toml".text = generators.toINI { } {
+      codestyle = {
+        indent_type = "space";
+        indent_width = 4;
+        template_file = "${<config/atcoder-tools/my_template.rs>}";
+        workspace_dir = "${homeDirectory}/projects/sei40kr/hello-atcoder/";
+        lang = "rust";
+        code_generator_file =
+          "${<config/atcoder-tools/custom_code_generator.py>}";
+      };
+
+      postprocess = {
+        exec_on_each_problem_dir = "rustfmt -q main.rs; cargo init -q";
+      };
+
+      etc = {
+        download_without_login = false;
+        parallel_download = false;
+        save_no_session_cache = false;
+      };
     };
   };
 }
