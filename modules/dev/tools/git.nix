058d330799a50dfac1ec2d7068ae2e23c90ee2c5
Seong Yong-ju <sei40kr@gmail.com>
2020-09-15 00:17:15 +0900

    nix: Add delta

--- modules/dev/tools/git.nix
+++ modules/dev/tools/git.nix
@@ -1,7 +1,18 @@
 { config, lib, pkgs, ... }:
 
 with lib;
-(let cfg = config.modules.dev.tools.git;
+(let
+  cfg = config.modules.dev.tools.git;
+  gitConfig = {
+    user = {
+      email = config.my.userEmail;
+      name = config.my.userFullName;
+    };
+  };
+  deltaConfig = {
+    core.pager = "${pkgs.my.delta}/bin/delta";
+    interactive.diffFilter = "${pkgs.my.delta}/bin/delta --color-only";
+  };
 in {
   options.modules.dev.tools.git = {
     enable = mkOption {
@@ -18,27 +29,33 @@ in {
       type = types.bool;
       default = false;
     };
+
+    enableDelta = mkOption {
+      type = types.bool;
+      default = true;
+    };
   };
 
   config = mkIf config.modules.dev.tools.git.enable {
-    modules.shell.zsh.zinitPluginsInit = ''
-      zinit snippet OMZP::git/git.plugin.zsh
-    '' + optionalString cfg.enableGitFlow ''
-      zinit snippet OMZP::git-flow/git-flow.plugin.zsh
-    '';
+    modules.shell = {
+      tools.bat.enable = true;
+      zsh.zinitPluginsInit = ''
+        zinit snippet OMZP::git/git.plugin.zsh
+      '' + optionalString cfg.enableGitFlow ''
+        zinit snippet OMZP::git-flow/git-flow.plugin.zsh
+      '';
+    };
 
     my.packages = with pkgs;
+      with pkgs.my;
       ([ git ] ++ optionals cfg.enableGitFlow [ gitAndTools.gitflow ]
-        ++ optionals cfg.enableGitCrypt [ git-crypt ]);
+        ++ optionals cfg.enableGitCrypt [ git-crypt ]
+        ++ optionals cfg.enableDelta [ delta ]);
 
     my.home.xdg.configFile = {
-      "git/config".text = generators.toINI { } {
+      "git/config".text = generators.toINI { } ({
         include.path = "${<config/git/config>}";
-        user = {
-          email = config.my.userEmail;
-          name = config.my.userFullName;
-        };
-      };
+      } // gitConfig // optionalAttrs cfg.enableDelta deltaConfig);
       "git/ignore".source = <config/git/ignore>;
     };
   };
