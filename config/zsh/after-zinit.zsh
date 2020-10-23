6844bd4f30daa2023accbf0a3859915d7543644b
Seong Yong-ju <sei40kr@gmail.com>
2020-10-23 22:14:42 +0900

    zsh: Use small triangle arrows for pure prompt

--- config/zsh/after-zinit.zsh
+++ config/zsh/after-zinit.zsh
@@ -186,8 +186,8 @@ zinit snippet OMZP::bgnotify/bgnotify.plugin.zsh
 ## Theme & Appearance
 
 PURE_PROMPT_SYMBOL='λ'
-PURE_GIT_DOWN_ARROW='▼'
-PURE_GIT_UP_ARROW='▲'
+PURE_GIT_DOWN_ARROW='▾'
+PURE_GIT_UP_ARROW='▴'
 zstyle :prompt:pure:prompt:success color yellow
 zinit ice compile'(pure|async).zsh' pick'async.zsh' src'pure.zsh'
 zinit light sindresorhus/pure
