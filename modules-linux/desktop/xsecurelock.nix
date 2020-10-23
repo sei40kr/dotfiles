878f26e6e467b9ba84565e14aa5fbb6faa9df9be
Seong Yong-ju <sei40kr@gmail.com>
2020-10-23 18:01:15 +0900

    nix: Major refactors on :desktop/xsecurelock

--- modules-linux/desktop/xsecurelock.nix
+++ modules-linux/desktop/xsecurelock.nix
@@ -1,26 +1,35 @@
 { config, lib, pkgs, ... }:
 
-with lib; {
-  options.modules.desktop.xsecurelock.enable = mkOption {
-    type = types.bool;
-    default = false;
+with lib;
+let cfg = config.modules.desktop.xsecurelock;
+in {
+  options.modules.desktop.xsecurelock = {
+    enable = mkOption {
+      type = types.bool;
+      default = false;
+    };
+
+    length = mkOption {
+      type = types.int;
+      default = 600;
+    };
+
+    period = mkOption {
+      type = types.int;
+      default = 5;
+    };
   };
 
-  config = mkIf config.modules.desktop.xsecurelock.enable {
+  config = mkIf cfg.enable {
     my.packages = with pkgs; [ xss-lock ];
 
     modules.desktop.x11.xsession.init = ''
-      ${pkgs.xorg.xset}/bin/xset s 600 5
-      ${pkgs.xss-lock}/bin/xss-lock -s "''${XDG_SESSION_ID}" \
-                                    -n ${
-                                      escapeShellArg
-                                      "${pkgs.xsecurelock}/libexec/xsecurelock/dimmer"
-                                    } \
-                                    -l \
-                                    -- ${
-                                      escapeShellArg
-                                      "${pkgs.xsecurelock}/bin/xsecurelock"
-                                    } &
+      ${pkgs.xorg.xset}/bin/xset s ${toString cfg.length} ${toString cfg.period}
+      ${pkgs.xss-lock}/bin/xss-lock \
+        -s "$XDG_SESSION_ID" \
+        -n ${escapeShellArg "${pkgs.xsecurelock}/libexec/xsecurelock/dimmer"} \
+        -l \
+        -- ${escapeShellArg "${pkgs.xsecurelock}/bin/xsecurelock"} &
     '';
     modules.desktop.apps.rofi.systemMenuItems."Lock" =
       "${pkgs.systemd}/bin/loginctl lock-session";
