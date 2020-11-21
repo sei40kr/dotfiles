9a3dd85ee77f13830b02a655f2ecbb9e7990f800
Seong Yong-ju <sei40kr@gmail.com>
2020-11-22 03:20:37 +0900

    nix: Refactors on tmux-per-project-session

--- packages/tmux-plugins/default.nix
+++ packages/tmux-plugins/default.nix
@@ -1,18 +1,19 @@
-{ fetchFromGitHub, lib, pkgs, ... }:
+{ fd, fzf, lib, makeWrapper, tmuxPlugins, ... }:
 
 with lib; {
-  per-project-session = pkgs.tmuxPlugins.mkDerivation {
+  per-project-session = tmuxPlugins.mkDerivation {
     pluginName = "per-project-session";
     rtpFilePath = "per-project-session.tmux";
     version = "unstable-2020-05-26";
+
     src = ./tmux-per-project-session;
-    dependencies = with pkgs; [ fd fzf ];
+
+    nativeBuildInputs = [ makeWrapper ];
+    dependencies = [ fd fzf ];
+
     postInstall = ''
-      sed -i \
-          -e 's|command_exists fd|true|;
-              s|fd |${pkgs.fd}/bin/fd |g;
-              s|fzf-tmux |${pkgs.fzf}/bin/fzf-tmux |g' \
-          $target/libexec/goto-session.bash
+      wrapProgram "''${target}/libexec/goto-session.bash" \
+        --prefix PATH : ${makeBinPath [ fd fzf ]}
     '';
   };
 }
