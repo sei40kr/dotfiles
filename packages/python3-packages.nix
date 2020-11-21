277ff814b3953625fa136a83aa633221401179dc
Seong Yong-ju <sei40kr@gmail.com>
2020-11-21 17:23:34 +0900

    nix: Minor refactors on Python package derivations

--- packages/python3-packages.nix
+++ packages/python3-packages.nix
@@ -1,17 +1,17 @@
-{ lib, pkgs, ... }:
+{ lib, pkgs, python3Packages, ... }:
 
 with lib; {
-  atcoder-tools = pkgs.python3Packages.buildPythonApplication rec {
+  atcoder-tools = python3Packages.buildPythonApplication rec {
     pname = "atcoder-tools";
     version = "1.1.7.1";
 
-    src = pkgs.python3Packages.fetchPypi {
+    src = python3Packages.fetchPypi {
       inherit pname version;
       sha256 =
         "cfcee879e87b18f259c488e00553657d577f63dfea1afbf8b01b5401f4346238";
     };
 
-    propagatedBuildInputs = with pkgs.python3Packages; [
+    propagatedBuildInputs = with python3Packages; [
       beautifulsoup4
       colorama
       jinja2
@@ -21,25 +21,25 @@ with lib; {
 
     doCheck = false;
 
-    meta = with lib; {
+    meta = {
       license = licenses.mit;
       description =
         "Convenient modules & tools for AtCoder users, written in Python 3.5";
     };
   };
 
-  protonvpn-cli = pkgs.python3Packages.buildPythonApplication rec {
+  protonvpn-cli = python3Packages.buildPythonApplication rec {
     pname = "protonvpn-cli";
     version = "2.2.4";
 
-    src = pkgs.python3Packages.fetchPypi {
+    src = python3Packages.fetchPypi {
       inherit version;
       pname = "protonvpn_cli";
       sha256 =
         "e018976f38a666b9f7318b3c63bbefb5cf97db94e7963ef6e4abd3c3acd54b89";
     };
 
-    propagatedBuildInputs = with pkgs.python3Packages; [
+    propagatedBuildInputs = with python3Packages; [
       docopt
       jinja2
       pythondialog
@@ -48,7 +48,7 @@ with lib; {
 
     doCheck = false;
 
-    meta = with lib; {
+    meta = {
       license = licenses.gpl3;
       description =
         "Linux command-line client for ProtonVPN. Written in Python.";
