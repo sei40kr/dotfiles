35c87dc0d9cfb014e0e881f07d470cf282638293
Seong Yong-ju <sei40kr@gmail.com>
2020-12-03 02:27:15 +0900

    nix: Move the modules list of my work PC

--- per-host/RLSUU178967M01.local.nix
+++ per-host/RLSUU178967M01.local.nix
@@ -2,4 +2,31 @@
 
 {
   imports = [ ../secrets/per-host/RLSUU178967M01.local.nix ];
+
+  my.user.shell = pkgs.zsh;
+
+  modules = {
+    dev = {
+      editors = {
+        emacs.enable = true;
+        doomEmacs.enable = true;
+        neovim.enable = true;
+      };
+      java = {
+        enable = true;
+        javaPackages."corretto64-11.0.8.10.1" = pkgs.my.corretto_11;
+      };
+      javascript.enable = true;
+      web.enable = true;
+      tools.git.enable = true;
+    };
+    shell = {
+      zsh.enable = true;
+      tmux = {
+        enable = true;
+        autostart = true;
+      };
+      tools.atcoderTools.enable = true;
+    };
+  };
 }
