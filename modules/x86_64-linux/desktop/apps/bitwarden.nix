b420ed8f7f8e7febbe86687e50ddf8d2e8bd0f0d
Seong Yong-ju <sei40kr@gmail.com>
2021-03-06 03:14:21 +0900

    nix: Rewrite :desktop/apps/bitwarden

---
+++ modules/x86_64-linux/desktop/apps/bitwarden.nix
@@ -0,0 +1,10 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.bitwarden;
+in {
+  options.modules.desktop.apps.bitwarden = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable { user.packages = with pkgs; [ bitwarden ]; };
+}
