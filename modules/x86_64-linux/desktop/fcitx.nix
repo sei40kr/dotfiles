1a16277981e625b80dcd8760463fc9cff5d1cdcd
Seong Yong-ju <sei40kr@gmail.com>
2021-03-11 23:24:29 +0900

    nix: Bump fcitx to 5

--- modules/x86_64-linux/desktop/fcitx.nix
+++ modules/x86_64-linux/desktop/fcitx.nix
@@ -4,73 +4,38 @@ with lib;
 with lib.my;
 let
   cfg = config.modules.desktop.fcitx;
-  package = with pkgs;
-    fcitx.override { plugins = with fcitx-engines; [ mozc ]; };
-  gtk2-cache = with pkgs;
-    runCommand "gtk2-immodule.cache" {
-      preferLocalBuild = true;
-      allowSubstitutes = false;
-      buildInputs = [ gtk2 package ];
-    } ''
-      mkdir -p "''${out}/etc/gtk-2.0"
-      GTK_PATH='${package}/lib/gtk-2.0' gtk-query-immodules-2.0 >"''${out}/etc/gtk-2.0/immodules.cache"
-    '';
-
-  gtk3-cache = with pkgs;
-    runCommand "gtk3-immodule.cache" {
-      preferLocalBuild = true;
-      allowSubstitutes = false;
-      buildInputs = [ gtk3 package ];
-    } ''
-      mkdir -p "''${out}/etc/gtk-3.0"
-      GTK_PATH='${package}/lib/gtk-3.0' gtk-query-immodules-3.0 >"''${out}/etc/gtk-3.0/immodules.cache"
-    '';
+  package = pkgs.unstable.fcitx5-with-addons.override {
+    addons = with pkgs; [ unstable.fcitx5-mozc ];
+  };
+  gtk2_cache = pkgs.runCommand "gtk2-immodule.cache" {
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+    buildInputs = [ package pkgs.unstable.gtk2 ];
+  } ''
+    mkdir -p "''${out}/etc/gtk-2.0"
+    GTK_PATH='${package}/lib/gtk-2.0' gtk-query-immodules-2.0 >"''${out}/etc/gtk-2.0/immodules.cache"
+  '';
+  gtk3_cache = pkgs.runCommand "gtk3-immodule.cache" {
+    preferLocalBuild = true;
+    allowSubstitutes = false;
+    buildInputs = [ package pkgs.unstable.gtk3 ];
+  } ''
+    mkdir -p "''${out}/etc/gtk-3.0"
+    GTK_PATH='${package}/lib/gtk-3.0' gtk-query-immodules-3.0 >"''${out}/etc/gtk-3.0/immodules.cache"
+  '';
 in {
   options.modules.desktop.fcitx = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs;
-      ([ package fcitx-configtool ]
-        ++ optionals config.modules.desktop.gtk.enable [
-          gtk2-cache
-          gtk3-cache
-        ]);
+    user.packages = [ package pkgs.unstable.fcitx5-configtool ]
+      ++ optionals config.modules.desktop.gtk.enable [ gtk2_cache gtk3_cache ];
     modules.desktop.env = {
       GTK_IM_MODULE = "fcitx";
       QT_IM_MODULE = "fcitx";
+      SDL_IM_MODULE = "fcitx";
       XMODIFIERS = "@im=fcitx";
     };
-    home.configFile = {
-      "fcitx/config".source = "${configDir}/fcitx/config";
-      "fcitx/conf/fcitx-classic-ui.config".source =
-        "${configDir}/fcitx/conf/fcitx-classic-ui.config";
-    };
-    home-manager.users.${config.user.name}.systemd.user.services = {
-      fcitx = {
-        Unit = {
-          After = [ "graphical-session-pre.target" ];
-          PartOf = [ "graphical-session.target" ];
-          Description = "Flexible Input Method Framework";
-          Documentation = "man:fcitx(1)";
-          X-Restart-Triggers = let configFile = config.home.configFile;
-          in [
-            "${configFile."fcitx/config".source}"
-            "${configFile."fcitx/conf/fcitx-classic-ui.config".source}"
-          ];
-        };
-        Service = {
-          Environment = [
-            "GTK_IM_MODULE=fcitx"
-            "QT_IM_MODULE=fcitx"
-            "XMODIFIERS=@im=fcitx"
-          ];
-          ExecReload = "${package}/bin/fcitx -r";
-          ExecStart = "${package}/bin/fcitx -D";
-          Restart = "on-failure";
-        };
-        Install = { WantedBy = [ "graphical-session.target" ]; };
-      };
-    };
+    home.configFile."fcitx5/config".source = "${configDir}/fcitx5/config";
     services.dbus = {
       enable = true;
       packages = [ package ];
