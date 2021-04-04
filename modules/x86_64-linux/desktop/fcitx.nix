f54b912ac3dd70b77feabf7ad84d520ffc21b893
Seong Yong-ju <sei40kr@gmail.com>
2021-04-04 12:22:07 +0900

    fcitx5: Fix GTK3 immodules cache & systemd service

--- modules/x86_64-linux/desktop/fcitx.nix
+++ modules/x86_64-linux/desktop/fcitx.nix
@@ -7,38 +7,39 @@ let
   package = pkgs.unstable.fcitx5-with-addons.override {
     addons = with pkgs; [ unstable.fcitx5-mozc ];
   };
-  gtk2_cache = pkgs.runCommand "gtk2-immodule.cache" {
-    preferLocalBuild = true;
-    allowSubstitutes = false;
-    buildInputs = [ package pkgs.unstable.gtk2 ];
-  } ''
-    mkdir -p "''${out}/etc/gtk-2.0"
-    GTK_PATH='${package}/lib/gtk-2.0' gtk-query-immodules-2.0 >"''${out}/etc/gtk-2.0/immodules.cache"
-  '';
   gtk3_cache = pkgs.runCommand "gtk3-immodule.cache" {
     preferLocalBuild = true;
     allowSubstitutes = false;
     buildInputs = [ package pkgs.unstable.gtk3 ];
   } ''
-    mkdir -p "''${out}/etc/gtk-3.0"
-    GTK_PATH='${package}/lib/gtk-3.0' gtk-query-immodules-3.0 >"''${out}/etc/gtk-3.0/immodules.cache"
+    mkdir -p "''${out}/lib/gtk-3.0/3.0.0"
+    GTK_PATH='${package}/lib/gtk-3.0' gtk-query-immodules-3.0 >"''${out}/lib/gtk-3.0/3.0.0/immodules.cache"
   '';
 in {
   options.modules.desktop.fcitx = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = [ package pkgs.unstable.fcitx5-configtool ]
-      ++ optionals config.modules.desktop.gtk.enable [ gtk2_cache gtk3_cache ];
-    modules.desktop.env = {
-      GTK_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      SDL_IM_MODULE = "fcitx";
-      XMODIFIERS = "@im=fcitx";
+    user.packages = [ package ];
+
+    home-manager.users.${config.user.name}.systemd.user.services.fcitx5 = {
+      Unit = {
+        After = [ "graphical-session.target" ];
+        Description = "Flexible Input Method Framework";
+        Documentation = [ "man:fcitx5(1)" ];
+      };
+      Service.ExecStart =
+        "${package}/bin/fcitx5 --disable clipboard,kimpanel,quickphrase,spell,unicode,xim";
+      Install.WantedBy = [ "graphical-session.target" ];
     };
-    home.configFile."fcitx5/config".source = "${configDir}/fcitx5/config";
     services.dbus = {
       enable = true;
       packages = [ package ];
+    };
+    modules.desktop.env = {
+      GTK_IM_MODULE = "fcitx";
+      GTK_IM_MODULE_FILE = "${gtk3_cache}/lib/gtk-3.0/3.0.0/immodules.cache";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
     };
   };
 }
