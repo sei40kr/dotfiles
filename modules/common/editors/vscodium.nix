a0a7bf79d82eb8084a466f0449a9ef1095210a1b
Seong Yong-ju <sei40kr@gmail.com>
2021-05-01 13:24:09 +0900

    vscodium: Update extensions

--- modules/common/editors/vscodium.nix
+++ modules/common/editors/vscodium.nix
@@ -10,31 +10,33 @@ in {
     home-manager.users.${config.user.name}.programs.vscode = {
       enable = true;
       package = pkgs.vscodium;
-      extensions = with pkgs; [
-        vscode-extensions.alanz.vscode-hie-server
-        my.vscode-extensions.alefragnani.Bookmarks
-        my.vscode-extensions.castwide.solargraph
-        my.vscode-extensions.ccls-project.ccls
-        my.vscode-extensions.CoenraadS.bracket-pair-colorizer-2
-        my.vscode-extensions.dbaeumer.vscode-eslint
-        my.vscode-extensions.eamodio.gitlens
-        my.vscode-extensions.EditorConfig.EditorConfig
-        my.vscode-extensions.esbenp.prettier-vscode
-        my.vscode-extensions.felixfbecker.php-intellisense
-        my.vscode-extensions.GitHub.vscode-pull-request-github
-        my.vscode-extensions.golang.Go
-        my.vscode-extensions.mechatroner.rainbow-csv
-        vscode-extensions.ms-python.python
-        vscode-extensions.ms-vscode.cpptools
-        my.vscode-extensions.Pivotal.vscode-boot-dev-pack
-        my.vscode-extensions.redhat.vscode-yaml
-        my.vscode-extensions.REditorSupport.r-lsp
-        my.vscode-extensions.rust-lang.rust
-        vscode-extensions.scala-lang.scala
-        vscode-extensions.scalameta.metals
-        my.vscode-extensions.vscjava.vscode-java-pack
-        my.vscode-extensions.vscodevim.vim
-      ];
+      extensions = with pkgs;
+        [
+          vscode-extensions.alanz.vscode-hie-server
+          vscode-extensions.coenraads.bracket-pair-colorizer-2
+          vscode-extensions.dbaeumer.vscode-eslint
+          # vscode-extensions.editorconfig.editorconfig
+          vscode-extensions.esbenp.prettier-vscode
+          vscode-extensions.github.vscode-pull-request-github
+          vscode-extensions.golang.Go
+          vscode-extensions.mechatroner.rainbow-csv
+          vscode-extensions.redhat.vscode-yaml
+          vscode-extensions.scala-lang.scala
+          vscode-extensions.scalameta.metals
+          my.vscode-extensions.Pivotal.vscode-boot-dev-pack
+          my.vscode-extensions.REditorSupport.r-lsp
+          my.vscode-extensions.alefragnani.Bookmarks
+          my.vscode-extensions.castwide.solargraph
+          my.vscode-extensions.ccls-project.ccls
+          my.vscode-extensions.eamodio.gitlens
+          my.vscode-extensions.felixfbecker.php-intellisense
+          my.vscode-extensions.rust-lang.rust
+          my.vscode-extensions.vscjava.vscode-java-pack
+          my.vscode-extensions.vscodevim.vim
+        ] ++ (optionals pkgs.stdenv.isDarwin [
+          vscode-extensions.ms-python.python
+          vscode-extensions.ms-vscode.cpptools
+        ]);
     };
     home.configFile = {
       "VSCodium/User/keybindings.json".source =
--- packages/all/vscode-extensions.nix
+++ packages/all/vscode-extensions.nix
@@ -31,54 +31,12 @@ with lib; {
     meta.license = licenses.mit;
   };
 
-  CoenraadS.bracket-pair-colorizer-2 =
-    pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-      mktplcRef = {
-        name = "bracket-pair-colorizer-2";
-        publisher = "CoenraadS";
-        version = "0.1.4";
-        sha256 = "08sawrq6ib5dr7774i1hsiwxzg1lljdhysrsvazrbdw7ldbsaz30";
-      };
-      meta.license = licenses.mit;
-    };
-
-  dbaeumer.vscode-eslint = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-eslint";
-      publisher = "dbaeumer";
-      version = "2.1.5";
-      sha256 = "1fr8n736pfjnxz7kgi6m86d2w6z1bwxa23xpfcd8sik9nnrnsz59";
-    };
-    meta.license = licenses.mit;
-  };
-
   eamodio.gitlens = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "gitlens";
       publisher = "eamodio";
       version = "10.2.2";
       sha256 = "00fp6pz9jqcr6j6zwr2wpvqazh1ssa48jnk1282gnj5k560vh8mb";
-    };
-    meta.license = licenses.mit;
-  };
-
-  EditorConfig.EditorConfig =
-    pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-      mktplcRef = {
-        name = "EditorConfig";
-        publisher = "EditorConfig";
-        version = "0.15.1";
-        sha256 = "18r19dn1an81l2nw1h8iwh9x3sy71d4ab0s5fvng5y7dcg32zajd";
-      };
-      meta.license = licenses.mit;
-    };
-
-  esbenp.prettier-vscode = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "prettier-vscode";
-      publisher = "esbenp";
-      version = "5.1.3";
-      sha256 = "03i66vxvlyb3msg7b8jy9x7fpxyph0kcgr9gpwrzbqj5s7vc32sr";
     };
     meta.license = licenses.mit;
   };
@@ -94,27 +52,6 @@ with lib; {
       meta.license = licenses.mit;
     };
 
-  GitHub.vscode-pull-request-github =
-    pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-      mktplcRef = {
-        name = "vscode-pull-request-github";
-        publisher = "GitHub";
-        version = "0.17.0";
-        sha256 = "1ga1fs3kihy70sgz2nfzdmq7162p7vav2g1p0kmbas5vszs12g0a";
-      };
-      meta.license = licenses.mit;
-    };
-
-  golang.Go = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "Go";
-      publisher = "golang";
-      version = "0.15.1";
-      sha256 = "1h7r781asl890n9fc0dh81l4ffx8xqd81d4hy2680dji8x390axz";
-    };
-    meta.license = licenses.mit;
-  };
-
   rust-lang.rust = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "rust";
@@ -125,32 +62,12 @@ with lib; {
     meta.license = licenses.mit;
   };
 
-  mechatroner.rainbow-csv = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "rainbow-csv";
-      publisher = "mechatroner";
-      version = "1.7.0";
-      sha256 = "1i5avr2zf6n2xmfidfgxhzmhy5f85gkxva0vaj1k0fv0rznfcq5p";
-    };
-    meta.license = licenses.mit;
-  };
-
   redhat.vscode-xml = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     mktplcRef = {
       name = "vscode-xml";
       publisher = "redhat";
       version = "0.13.0";
       sha256 = "013h12qhj1h1pcjns2l5dn2hb52p8j468f5n5sw62gq4vfr9yvyf";
-    };
-    meta.license = licenses.mit;
-  };
-
-  redhat.vscode-yaml = pkgs.vscode-utils.buildVscodeMarketplaceExtension {
-    mktplcRef = {
-      name = "vscode-yaml";
-      publisher = "redhat";
-      version = "0.8.0";
-      sha256 = "08dy5wm24c3bga698925pjwbymdmxi00a84d6xajj750pax7grz0";
     };
     meta.license = licenses.mit;
   };
