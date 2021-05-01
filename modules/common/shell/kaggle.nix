ddc5a677cdf5e77cc3548bcfc7deda354e97b040
Seong Yong-ju <sei40kr@gmail.com>
2021-05-01 17:04:49 +0900

    kaggle: Skip tests

--- modules/common/shell/kaggle.nix
+++ modules/common/shell/kaggle.nix
@@ -2,9 +2,11 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.shell.kaggle;
+let
+  cfg = config.modules.shell.kaggle;
+  package = pkgs.kaggle.overrideAttrs (_: { doCheck = false; });
 in {
   options.modules.shell.kaggle = { enable = mkBoolOpt false; };
 
-  config = mkIf cfg.enable { user.packages = with pkgs; [ kaggle ]; };
+  config = mkIf cfg.enable { user.packages = [ package ]; };
 }
