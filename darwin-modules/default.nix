26114dc57d5b923eeb83e959388f79b71465075e
Seong Yong-ju <seong_yongju@waku-2.com>
2021-10-28 00:35:46 +0900

    feat: fix & improve darwin app symlinking

--- darwin-modules/default.nix
+++ darwin-modules/default.nix
@@ -1,4 +1,4 @@
-{ inputs, lib, pkgs, ... }:
+{ config, inputs, lib, pkgs, ... }:
 
 with lib;
 with lib.my; {
@@ -6,6 +6,29 @@ with lib.my; {
     ++ (mapModulesRec' (toString ./.) import);
 
   nix.useDaemon = true;
+
+  system.activationScripts.applications.text = let
+    env = pkgs.buildEnv {
+      name = "applications";
+      paths = config.environment.systemPackages
+        ++ config.users.users.${config.user.name}.packages;
+      pathsToLink = "/Applications";
+    };
+  in mkForce ''
+    # Set up applications.
+    echo "setting up ~/Applications/Nix Apps..." >&2
+
+    rm -rf ~/Applications/Nix\ Apps
+    mkdir -p ~/Applications/Nix\ Apps
+
+    srcs=()
+    while read src; do
+      srcs+=("$src")
+    done < <(find ${env}/Applications -maxdepth 1 -type l)
+    if [[ "''${#srcs[@]}" != 0 ]]; then
+      /bin/cp -LR "''${srcs[@]}" ~/Applications/Nix\ Apps
+    fi
+  '';
 
   user.packages = with pkgs; [
     coreutils
