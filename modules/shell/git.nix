a1f4bb1a527a9f8b1054a3347e0bfd3fcc5fca6f
Seong Yong-ju <seong_yongju@waku-2.com>
2021-10-27 21:51:21 +0900

    feat(git): make user name & email variable

--- modules/shell/git.nix
+++ modules/shell/git.nix
@@ -6,18 +6,26 @@ let
   inherit (config.dotfiles) configDir;
   cfg = config.modules.shell.git;
 in {
-  options.modules.shell.git = { enable = mkBoolOpt false; };
+  options.modules.shell.git = with types; {
+    enable = mkBoolOpt false;
+
+    user = {
+      name = mkOpt str "Seong Yong-ju";
+      email = mkOpt str "sei40kr@gmail.com";
+    };
+  };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ git gitAndTools.gitflow ];
+
     home.configFile."git/ignore".source = "${configDir}/git/ignore";
 
     home-manager.users.${config.user.name}.programs.git = {
       enable = true;
       delta.enable = true;
       includes = [{ path = "${configDir}/git/config"; }];
-      userName = "Seong Yong-ju";
-      userEmail = "sei40kr@gmail.com";
+      userName = cfg.user.name;
+      userEmail = cfg.user.email;
     };
   };
 }
