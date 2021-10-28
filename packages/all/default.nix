4340039d5291c8862d1b91eb318310251ccce175
Seong Yong-ju <sei40kr@gmail.com>
2021-10-28 23:19:16 +0900

    chore: remove operator-mono derivation

--- packages/all/default.nix
+++ packages/all/default.nix
@@ -9,7 +9,6 @@
   groovy-language-server = pkgs.callPackage ./groovy-language-server { };
   idea-doom-emacs = pkgs.callPackage ./idea-doom-emacs.nix { };
   kotlin-language-server = pkgs.callPackage ./kotlin-language-server.nix { };
-  operator-mono = pkgs.callPackage ./operator-mono.nix { };
   zsh-fzf-chdir = pkgs.callPackage ./zsh-fzf-chdir.nix { };
   zsh-fzf-docker = pkgs.callPackage ./zsh-fzf-docker.nix { };
   zsh-fzf-projects = pkgs.callPackage ./zsh-fzf-projects.nix { };
