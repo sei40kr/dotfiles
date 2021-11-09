f1498f2b33514e49dd3468b887a2dd9259c85d36
Seong Yong-ju <sei40kr@gmail.com>
2021-11-10 07:47:23 +0900

    feat(ranger): redo ranger

--- modules/shell/ranger.nix
+++ modules/shell/ranger.nix
@@ -2,38 +2,27 @@
 
 with lib;
 with lib.my;
-let cfg = config.modules.shell.ranger;
+let
+  inherit (config.dotfiles) configDir;
+  cfg = config.modules.shell.ranger;
 in {
   options.modules.shell.ranger = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ ranger ];
+    user.packages = with pkgs; [
+      ranger
 
-    home.configFile."ranger/rc.conf".text = ''
-      # ===================================================================
-      # This file contains the default startup commands for ranger.
-      # To change them, it is recommended to create either /etc/ranger/rc.conf
-      # (system-wide) or ~/.config/ranger/rc.conf (per user) and add your custom
-      # commands there.
-      #
-      # If you copy this whole file there, you may want to set the environment
-      # variable RANGER_LOAD_DEFAULT_RC to FALSE to avoid loading it twice.
-      #
-      # The purpose of this file is mainly to define keybindings and settings.
-      # For running more complex python code, please create a plugin in "plugins/" or
-      # a command in "commands.py".
-      #
-      # Each line is a command that will be run before the user interface
-      # is initialized.  As a result, you can not use commands which rely
-      # on the UI such as :delete or :mark.
-      # ===================================================================
+      # Optional dependencies
+      ffmpeg
+    ];
 
+    environment.etc."ranger/rc.conf".text = ''
       # ===================================================================
       # == Options
       # ===================================================================
 
       # How many columns are there, and what are their relative widths?
-      set column_ratios 1,1,1
+      set column_ratios 1
 
       # Show hidden files? You can toggle this by typing 'zh'
       set show_hidden true
@@ -43,24 +32,40 @@ in {
       # With "multiple", ranger will ask only if you delete multiple files at once.
       set confirm_on_delete always
 
+      # Use non-default path for file preview script?
+      # ranger ships with scope.sh, a script that calls external programs (see
+      # README.md for dependencies) to preview images, archives, etc.
+      set preview_script ${pkgs.ranger}/lib/python3.9/site-packages/ranger/data/scope.sh
+
+      # Automatically count files in the directory, even before entering them?
+      set automatically_count_files false
+
       # Which colorscheme to use?  These colorschemes are available by default:
       # default, jungle, snow, solarized
       set colorscheme default
 
       # Preview files on the rightmost column?
       # And collapse (shrink) the last column if there is nothing to preview?
-      set preview_files true
+      set preview_files false
       set preview_directories true
-      set collapse_preview false
 
       # Draw borders around columns? (separators, outline, both, or none)
       # Separators are vertical lines between columns.
       # Outline draws a box around all the columns.
       # Both combines the two.
-      set draw_borders separators
+      set draw_borders both
 
       # Enable the mouse support?
       set mouse_enabled false
+
+      # Display the file size in the main column or status bar?
+      set display_size_in_main_column false
+
+      # Display files tags in all columns or only in main column?
+      set display_tags_in_all_columns false
+
+      # Set a title for the window? Updates both `WM_NAME` and `WM_ICON_NAME`
+      set update_title true
 
       # Show hostname in titlebar?
       set hostname_in_titlebar false
@@ -68,13 +73,8 @@ in {
       # Abbreviate $HOME with ~ in the titlebar (first line) of ranger?
       set tilde_in_titlebar true
 
-      # Use fuzzy tab completion with the "cd" command. For example,
-      # ":cd /u/lo/b<tab>" expands to ":cd /usr/local/bin".
-      set cd_tab_fuzzy true
-
-      # Disable displaying line numbers in main column.
-      # Possible values: false, absolute, relative.
-      set line_numbers relative
+      # Add the highlighted file to the path in the titlebar
+      set show_selection_in_titlebar false
 
       # Start line numbers from 1 instead of 0
       set one_indexed true
