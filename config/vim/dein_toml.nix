285f38c78d25e4720512dc6788eea2156cfdd368
Seong Yong-ju <sei40kr@gmail.com>
2021-12-25 23:52:35 +0900

    feat(neovim): install nvim-treesitter

--- config/vim/dein_toml.nix
+++ config/vim/dein_toml.nix
@@ -1,6 +1,35 @@
 { vimPlugins, ... }:
 
-{
+let
+  nvim-treesitter = vimPlugins.nvim-treesitter.withPlugins (p: [
+    p.tree-sitter-agda
+    p.tree-sitter-bash
+    p.tree-sitter-c
+    p.tree-sitter-c-sharp
+    p.tree-sitter-cpp
+    p.tree-sitter-css
+    p.tree-sitter-go
+    p.tree-sitter-html
+    p.tree-sitter-java
+    p.tree-sitter-javascript
+    p.tree-sitter-jsdoc
+    p.tree-sitter-json
+    p.tree-sitter-julia
+    p.tree-sitter-lua
+    p.tree-sitter-nix
+    p.tree-sitter-ocaml
+    p.tree-sitter-php
+    p.tree-sitter-python
+    p.tree-sitter-ruby
+    p.tree-sitter-rust
+    p.tree-sitter-scala
+    p.tree-sitter-swift
+    p.tree-sitter-typescript
+    p.tree-sitter-tsx
+    p.tree-sitter-vim
+    p.tree-sitter-yaml
+  ]);
+in {
   plugins = [
     { repo = vimPlugins.vim-sensible.rtp; }
     {
@@ -12,6 +41,18 @@
       hook_add = ''
         let g:lightline = { "colorscheme": "onedark" }
       '';
+    }
+    {
+      repo = nvim-treesitter.rtp;
+      hook_post_source = ''
+        lua <<EOF
+        require 'nvim-treesitter.configs'.setup {
+          highlight = { enable = true },
+          indent = { enable = true },
+        }
+        EOF
+      '';
+      "if" = "has('nvim')";
     }
     {
       repo = vimPlugins.onedark-vim.rtp;
