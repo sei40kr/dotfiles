c284b486bee52365e980b9dd542cceec52b1152a
Seong Yong-ju <sei40kr@gmail.com>
2021-12-27 02:26:34 +0900

    fix(fcitx5): disable daemon service

--- nixos-modules/desktop/fcitx5.nix
+++ nixos-modules/desktop/fcitx5.nix
@@ -3,10 +3,8 @@
 with lib;
 with lib.my;
 let
-  inherit (config.dotfiles) configDir;
   inherit (config.i18n.inputMethod) package;
-  desktopCfg = config.modules.desktop;
-  cfg = desktopCfg.fcitx5;
+  cfg = config.modules.desktop.fcitx5;
 in {
   options.modules.desktop.fcitx5 = { enable = mkBoolOpt false; };
 
@@ -15,14 +13,7 @@ in {
       enabled = "fcitx5";
       fcitx5.addons = with pkgs; [ fcitx5-mozc ];
     };
-    environment.sessionVariables = {
-      GTK_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      XMODIFIERS = "@im=fcitx";
 
-      # cf. https://github.com/NixOS/nixpkgs/issues/129442
-      NIX_PROFILES =
-        concatStringsSep " " (reverseList config.environment.profiles);
-    };
+    systemd.user.services.fcitx5-daemon.enable = mkForce false;
   };
 }
