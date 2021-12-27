409014a5aed1c2c53ddfc8d7e5a7450142228121
Seong Yong-ju <sei40kr@gmail.com>
2021-12-27 20:48:51 +0900

    module: add :dev lua

---
+++ modules/dev/lua.nix
@@ -0,0 +1,12 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.lua;
+in {
+  options.modules.dev.lua = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ lua nodePackages.lua-fmt ];
+  };
+}
