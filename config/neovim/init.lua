a1822cbcaad2dd456b93ee3fb709b9ed62b94727
Seong Yong-ju <sei40kr@gmail.com>
2022-01-03 21:46:03 +0900

    feat(neovim): confirm before abandoning unsaved buffers

--- config/neovim/init.lua
+++ config/neovim/init.lua
@@ -7,6 +7,7 @@ vim.opt.backspace = {"indent", "eol", "start"}
 vim.opt.clipboard = {"unnamedplus"}
 vim.opt.colorcolumn = "80"
 vim.opt.completeopt = {"menu", "menuone", "noselect"}
+vim.opt.confirm = true
 vim.opt.display:append {"lastline"}
 vim.opt.encoding = "utf-8"
 vim.opt.expandtab = true
