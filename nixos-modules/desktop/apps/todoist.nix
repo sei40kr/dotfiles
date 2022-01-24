e29c6e59f2be659d5e7ac69e46a7893fb76d792e
Seong Yong-ju <sei40kr@gmail.com>
2022-01-25 00:21:09 +0900

    feat(modules): add :desktop/apps/todoist

---
+++ nixos-modules/desktop/apps/todoist.nix
@@ -0,0 +1,11 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.todoist;
+in {
+  options.modules.desktop.apps.todoist = { enable = mkBoolOpt false; };
+
+  config =
+    mkIf cfg.enable { user.packages = with pkgs; [ my.todoist-electron ]; };
+}
