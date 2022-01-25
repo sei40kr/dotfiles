82a5e3a88be8a8f3e88f6b75c04f20eb1d329f8c
Seong Yong-ju <sei40kr@gmail.com>
2022-01-26 01:29:36 +0900

    refactor(fcitx5): get rid of nixpkgs module

--- nixos-modules/desktop/fcitx5.nix
+++ nixos-modules/desktop/fcitx5.nix
@@ -5,15 +5,39 @@ with lib.my;
 let
   inherit (config.i18n.inputMethod) package;
   cfg = config.modules.desktop.fcitx5;
+
+  fcitx5-with-addons =
+    pkgs.fcitx5-with-addons.override { addons = with pkgs; [ fcitx5-mozc ]; };
+
+  gtk2_cache = pkgs.runCommandLocal "gtk2-immodule.cache" {
+    buildInputs = [ fcitx5-with-addons pkgs.gtk2 ];
+  } ''
+    mkdir -p $out/etc/gtk-2.0
+    GTK_PATH=${fcitx5-with-addons}/lib/gtk-2.0 gtk-query-immodules-2.0 >$out/etc/gtk-2.0/immodules.cache
+  '';
+
+  gtk3_cache = pkgs.runCommandLocal "gtk3-immodule.cache" {
+    buildInputs = [ fcitx5-with-addons pkgs.gtk3 ];
+  } ''
+    mkdir -p $out/etc/gtk-3.0
+    GTK_PATH=${fcitx5-with-addons}/lib/gtk-3.0 gtk-query-immodules-3.0 >$out/etc/gtk-3.0/immodules.cache
+  '';
 in {
   options.modules.desktop.fcitx5 = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    i18n.inputMethod = {
-      enabled = "fcitx5";
-      fcitx5.addons = with pkgs; [ fcitx5-mozc ];
+    user.packages = [ fcitx5-with-addons gtk2_cache gtk3_cache ];
+
+    systemd.user.services.fcitx5-daemon = {
+      enable = true;
+      script = "${fcitx5-with-addons}/bin/fcitx5";
+      wantedBy = [ "graphical-session.target" ];
     };
 
-    systemd.user.services.fcitx5-daemon.enable = mkForce false;
+    environment.variables = {
+      GTK_IM_MODULE = "fcitx";
+      QT_IM_MODULE = "fcitx";
+      XMODIFIERS = "@im=fcitx";
+    };
   };
 }
