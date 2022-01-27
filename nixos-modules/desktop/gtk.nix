13d3584f7891553fb9b9f8cdedf4b8814333d0c3
Seong Yong-ju <sei40kr@gmail.com>
2022-01-27 23:52:09 +0900

    refactor(gtk): get rid of home-manager module

--- nixos-modules/desktop/gtk.nix
+++ nixos-modules/desktop/gtk.nix
@@ -1,4 +1,4 @@
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
@@ -6,6 +6,21 @@ let
   cfg = config.modules.desktop.gtk;
 
   keyTheme = "Emacs";
+
+  formatGtk2Option = n: v:
+    let
+      v' = if isBool v then
+        (if v then "true" else "false")
+      else if isString v then
+        ''"${v}"''
+      else
+        toString v;
+    in "${n} = ${v'}";
+  toGtkIni = lib.generators.toINI {
+    mkKeyValue = n: v:
+      let v' = if isBool v then (if v then "true" else "false") else toString v;
+      in "${n}=${v'}";
+  };
 in {
   options.modules.desktop.gtk = with types; {
     enable = mkBoolOpt false;
@@ -18,26 +33,44 @@ in {
   };
 
   config = mkIf cfg.enable {
-    home-manager.users.${config.user.name} = {
-      gtk = {
-        inherit (cfg) font iconTheme theme;
+    user.packages = with pkgs; [
+      (mkIf (cfg.theme != null) cfg.theme.package)
+      (mkIf (cfg.iconTheme != null) cfg.iconTheme.package)
+    ];
 
-        enable = true;
+    fonts.fonts = with pkgs; [ (mkIf (cfg.font != null) cfg.font.package) ];
 
-        gtk2.extraConfig = ''
-          gtk-key-theme-name = "${keyTheme}"
-        '';
-
-        gtk3.extraConfig = {
+    home.configFile = {
+      "gtk-2.0/gtkrc".text = concatStringsSep "\n"
+        (mapAttrsToList formatGtk2Option ((optionalAttrs (cfg.font != null) {
+          gtk-font-name = "${cfg.font.name} ${toString cfg.font.size}";
+        }) // (optionalAttrs (cfg.theme != null) {
+          gtk-theme-name = cfg.theme.name;
+        }) // (optionalAttrs (cfg.iconTheme != null) {
+          gtk-icon-theme-name = cfg.iconTheme.name;
+        })));
+      "gtk-3.0/settings.ini".text = toGtkIni {
+        Settings = {
           gtk-enable-primary-paste = false;
           gtk-key-theme-name = keyTheme;
-        };
+        } // (optionalAttrs (cfg.font != null) {
+          gtk-font-name = "${cfg.font.name} ${toString cfg.font.size}";
+        }) // (optionalAttrs (cfg.theme != null) {
+          gtk-theme-name = cfg.theme.name;
+        }) // (optionalAttrs (cfg.iconTheme != null) {
+          gtk-icon-theme-name = cfg.iconTheme.name;
+        });
       };
+    };
+
+    environment.sessionVariables = {
+      GTK2_RC_FILES = "${
+          config.home-manager.users.${config.user.name}.xdg.configHome
+        }/gtk-2.0/gtkrc";
     };
 
     modules.desktop.dconf = {
       enable = true;
-
       settings = {
         "org/gnome/desktop/interface" = {
           icon-theme = mkIf (cfg.iconTheme != null) cfg.iconTheme.name;
