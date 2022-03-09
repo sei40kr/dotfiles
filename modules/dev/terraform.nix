fd6262026090a36a0535aeb60f60f7117efeff9c
Seong Yong-ju <sei40kr@gmail.com>
2022-03-10 01:52:49 +0900

    terraform: Install tslint & add shell aliases

--- modules/dev/terraform.nix
+++ modules/dev/terraform.nix
@@ -7,14 +7,21 @@ in {
   options.modules.dev.terraform = { enable = mkBoolOpt false; };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ terraform terraform-ls ];
+    user.packages = with pkgs; [ terraform terraform-ls tflint ];
 
-    modules.shell = {
-      aliases.tf = "terraform";
-      zsh.rcInit = ''
-        zinit ice wait''' lucid as'completion' id-as'OMZP::terraform'
-        zinit snippet ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/terraform/_terraform
-      '';
+    modules.shell.aliases = {
+      tf = "terraform";
+      tfa = "terraform apply";
+      tfd = "terraform destroy";
+      tff = "terraform fmt";
+      tfi = "terraform init";
+      tfp = "terraform plan";
+      tfv = "terraform validate";
     };
+
+    modules.shell.zsh.rcInit = ''
+      zinit ice as'completion' id-as'OMZP::terraform'
+      zinit snippet ${pkgs.oh-my-zsh}/share/oh-my-zsh/plugins/terraform/_terraform
+    '';
   };
 }
