c984534fee6807f9aa40a15e059719a25d3d5fa5
Seong Yong-ju <sei40kr@gmail.com>
2022-03-25 00:58:00 +0900

    hosts: Add taku

---
+++ hosts/nixos/taku/default.nix
@@ -0,0 +1,131 @@
+{ lib, pkgs, ... }:
+
+with lib; {
+  imports = [ ./hardware-configuration.nix ];
+
+  # Set your time zone.
+  time.timeZone = "Asia/Tokyo";
+
+  networking.hostName = "taku"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  networking.interfaces.enp0s31f6.useDHCP = true;
+
+
+  modules = {
+    desktop = {
+      gdm.enable = true;
+      gnome.enable = true;
+
+      apps = {
+        bitwarden.enable = true;
+        calendar.enable = true;
+        discord.enable = true;
+        font-viewer.enable = true;
+        geary.enable = true;
+        gnome-weather.enable = true;
+        notion.enable = true;
+        pomodoro.enable = true;
+        slack.enable = true;
+        zeal.enable = true;
+      };
+
+      browsers = {
+        chrome.enable = true;
+      };
+
+      media = {
+        documents = {
+          enable = true;
+          pdf.enable = true;
+          ebook.enable = true;
+        };
+        evince.enable = true;
+        totem.enable = true;
+      };
+    };
+
+    dev = {
+      ansible.enable = true;
+      aws-cli.enable = true;
+      cc.enable = true;
+      go.enable = true;
+      java.enable = true;
+      jupyter.enable = true;
+      lua.enable = true;
+      r.enable = true;
+      nix.enable = true;
+      python = {
+        enable = true;
+        enablePoetry = true;
+      };
+      rust.enable = true;
+      scala = {
+        enable = true;
+        bloop.enable = true;
+      };
+      shell.enable = true;
+      sql.enable = true;
+      web.enable = true;
+    };
+
+    editors = {
+      datagrip.enable = true;
+      dataspell.enable = true;
+      emacs = {
+        enable = true;
+        doom.enable = true;
+      };
+      idea.enable = true;
+      neovim = {
+        enable = true;
+        manpager.enable = true;
+      };
+    };
+
+    hardware = {
+      audio.enable = true;
+      printing.enable = true;
+    };
+
+    services = {
+      docker = {
+        enable = true;
+        autoPrune.enable = true;
+        compose.enable = true;
+      };
+      # flexget.enable = true;
+      jellyfin = {
+        enable = true;
+        openFirewall = true;
+      };
+      psd.enable = true;
+      sushi.enable = true;
+      ssh.enable = true;
+    };
+
+    shell = {
+      bottom.enable = true;
+      exa.enable = true;
+      ghq.enable = true;
+      prettyping.enable = true;
+      strace.enable = true;
+      tcpdump.enable = true;
+      bat.enable = true;
+      git.enable = true;
+      hugo.enable = true;
+      oj.enable = true;
+      tmux = {
+        enable = true;
+        autoRun = true;
+      };
+      zsh.enable = true;
+    };
+
+    term.alacritty.enable = true;
+  };
+
+  user.name = "sei40kr";
+
+  modules.term.theme.active = "tokyo-night";
+}
---
+++ hosts/nixos/taku/hardware-configuration.nix
@@ -0,0 +1,40 @@
+{ config, lib, ... }:
+
+{
+  boot.initrd = {
+    availableKernelModules = [ "xhci_pci" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
+    kernelModules = [ ];
+  };
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
+
+  hardware = {
+    cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+    opengl.enable = true;
+    # high-resolution display
+    video.hidpi.enable = lib.mkDefault true;
+  };
+
+  fileSystems = {
+    "/" =
+      {
+        device = "/dev/disk/by-uuid/560bee25-b233-4322-8c46-1bfc9db75ec5";
+        fsType = "ext4";
+      };
+
+    "/boot" =
+      {
+        device = "/dev/disk/by-uuid/8FCA-7C43";
+        fsType = "vfat";
+      };
+  };
+
+  swapDevices = [{ device = "/dev/disk/by-uuid/2367e722-f68f-47de-b90d-a3721928b09f"; }];
+
+  time.hardwareClockInLocalTime = true;
+
+  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+}
