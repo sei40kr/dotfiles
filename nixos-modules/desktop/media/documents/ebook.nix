277a2a7b4edb2ad8b53bc52f9e937b87d21c497a
Seong Yong-ju <sei40kr@gmail.com>
2022-04-09 16:32:57 +0900

    modules: Remove :services calibre-web

--- nixos-modules/desktop/media/documents/ebook.nix
+++ nixos-modules/desktop/media/documents/ebook.nix
@@ -2,24 +2,20 @@
 
 with lib;
 with lib.my;
-let
-  cfg = config.modules.desktop.media.documents.ebook;
-
-  catalogs = if config.modules.services.calibre-web.enable then [{
-    title = "Calibre-Web (local)";
-    uri = "http://localhost:8083/opds";
-    preview = "http://localhost:8083/opds/discover";
-  }] else [{
-    title = "Calibre-Web";
-    uri = "https://calibre-web.yong-ju.me/opds";
-    preview = "https://calibre-web.yong-ju.me/opds/discover";
-  }];
-in {
+let cfg = config.modules.desktop.media.documents.ebook;
+in
+{
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ foliate ];
 
     home.dataFile."com.github.johnfactotum.Foliate/catalogs/catalogs.json".text =
-      builtins.toJSON { inherit catalogs; };
+      builtins.toJSON {
+        catalogs = [{
+          title = "Calibre-Web";
+          uri = "https://calibre-web.yong-ju.me/opds";
+          preview = "https://calibre-web.yong-ju.me/opds/discover";
+        }];
+      };
 
     modules.desktop.dconf = {
       enable = true;
