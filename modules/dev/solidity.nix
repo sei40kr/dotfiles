cd30b125e0d02e27b983d377e98ee7fd7f3a1e27
Seong Yong-ju <sei40kr@gmail.com>
2022-04-09 17:01:05 +0900

    modules: Add :dev/solidity

---
+++ modules/dev/solidity.nix
@@ -0,0 +1,13 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.solidity;
+in
+{
+  options.modules.dev.solidity = { enable = mkBoolOpt false; };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ solc nodePackages.ganache-cli ];
+  };
+}
