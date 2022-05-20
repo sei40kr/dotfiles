303ebcd46ca0ca50bfda92ffd45e8d8151c1855f
Seong Yong-ju <sei40kr@gmail.com>
2022-05-21 08:00:00 +0900

    flake: Bump nixpkgs

--- overlays/dash-to-dock/default.nix
+++
@@ -1,8 +0,0 @@
-_self: super: {
-  gnomeExtensions = super.gnomeExtensions // {
-    dash-to-dock = super.gnomeExtensions.dash-to-dock.overrideAttrs
-      ({ patches ? [ ], ... }: {
-        patches = patches ++ [ ./fix-shell-version.patch ];
-      });
-  };
-}
