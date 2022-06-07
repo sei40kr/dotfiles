a9909d85587c87d65f749bdeed12d425e790a80d
Seong Yong-ju <sei40kr@gmail.com>
2022-06-07 18:51:04 +0900

    modules: Add :desktop/apps/zoom

---
+++ nixos-modules/desktop/apps/zoom.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.desktop.apps.zoom;
+in
+{
+  options.modules.desktop.apps.zoom = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ zoom-us ];
+  };
+}
