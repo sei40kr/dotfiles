8e3f4de3849e04cdb3791386f6dc97a9943d67ae
Seong Yong-ju <sei40kr@gmail.com>
2022-06-23 02:57:46 +0900

    feat(ulauncher): enable to set the user theme

--- nixos-modules/desktop/apps/ulauncher.nix
+++ nixos-modules/desktop/apps/ulauncher.nix
@@ -1,26 +1,44 @@
 { config, lib, pkgs, ... }:
 
+with builtins;
 with lib;
 with lib.my;
-let cfg = config.modules.desktop.apps.ulauncher;
+let
+  cfg = config.modules.desktop.apps.ulauncher;
+
+  inherit (cfg) theme;
+  themeName =
+    if theme != null then (fromJSON (readFile "${theme}/manifest.json")).name
+    else null;
 in
 {
-  options.modules.desktop.apps.ulauncher = {
+  options.modules.desktop.apps.ulauncher = with types; {
     enable = mkBoolOpt false;
+
+    theme = mkOpt (nullOr package) null;
   };
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ ulauncher ];
 
+    home.configFile = mkIf (theme != null) {
+      "ulauncher/user-themes/${themeName}" = {
+        recursive = true;
+        source = theme;
+      };
+    };
+
     systemd.user.services.ulauncher = {
       description = "Linux Application Launcher";
       documentation = [ "https://ulauncher.io/" ];
       wantedBy = [ "graphical-session.target" ];
+      reloadTriggers = [ cfg.theme ];
       serviceConfig = {
         Type = "simple";
         Restart = "always";
         RestartSec = 1;
         ExecStart = "${pkgs.ulauncher}/bin/ulauncher --hide-window";
+        ExecReload = "/bin/kill -HUP $MAINPID";
       };
     };
   };
