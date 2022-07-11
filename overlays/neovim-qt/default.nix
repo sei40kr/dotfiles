01f28d854da2ceedd46aa08e876c7ed5f5dca51e
Seong Yong-ju <seong_yongju@waku-2.com>
2022-07-11 15:02:18 +0900

    fix(nvim): fix build failure on darwin

--- overlays/neovim-qt/default.nix
+++ overlays/neovim-qt/default.nix
@@ -1,14 +1,19 @@
-_self: super:
+self: super:
+let inherit (self) lib stdenv;
+in
 {
   neovim-qt = super.neovim-qt.overrideAttrs ({ buildCommand, ... }: {
     buildCommand = ''
       ${buildCommand}
-      rm -r $out/share/applications
-      mkdir -p $out/share/applications
-      cp ${super.neovim-qt-unwrapped}/share/applications/nvim-qt.desktop \
-         $out/share/applications
-      sed -i -e '/\[Desktop Entry\]/a\' -e 'StartupWMClass=.nvim-qt-wrapped' \
-          $out/share/applications/nvim-qt.desktop
+
+      ${lib.optionalString stdenv.isLinux ''
+        rm -r $out/share/applications
+        mkdir -p $out/share/applications
+        cp ${super.neovim-qt-unwrapped}/share/applications/nvim-qt.desktop \
+          $out/share/applications
+        sed -i -e '/\[Desktop Entry\]/a\' -e 'StartupWMClass=.nvim-qt-wrapped' \
+            $out/share/applications/nvim-qt.desktop
+      ''}
     '';
   });
 }
