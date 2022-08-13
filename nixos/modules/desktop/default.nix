b4272a6d1c4ad75523c24d84c76d0f09ab313eaa
Seong Yong-ju <sei40kr@gmail.com>
2022-08-13 14:25:13 +0900

    feat(desktop): add :desktop/sway

--- nixos/modules/desktop/default.nix
+++ nixos/modules/desktop/default.nix
@@ -1,9 +1,23 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 with lib.my;
+let
+  inherit (config.dotfiles) binDir;
+  cfg = config.modules.desktop;
+
+  wayland-sensible-terminal = pkgs.runCommandLocal "wayland-sensible-terminal" { } ''
+    mkdir -p $out/bin
+    cp ${../../../bin}/wayland-sensible-terminal $out/bin
+    patchShebangs --build $out/bin/wayland-sensible-terminal
+  '';
+in
 {
   options.modules.desktop = {
     enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = [ wayland-sensible-terminal ];
   };
 }
---
+++ nixos/modules/desktop/sway.nix
@@ -0,0 +1,61 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  inherit (config.dotfiles) configDir;
+  cfg = config.modules.desktop.sway;
+
+  package = pkgs.sway.override {
+    extraSessionCommands = ''
+      export WLR_NO_HARDWARE_CURSORS=1
+
+      # SDL
+      export SDL_VIDEODRIVER=wayland
+
+      # QT
+      export QT_QPA_PLATFORM=wayland-egl
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION=1
+
+      # Fix for some Java AWT applications
+      export _JAVA_AWT_WM_NONREPARENTING=1
+    '';
+    withGtkWrapper = true;
+  };
+in
+{
+  options.modules.desktop.sway = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = [ package ];
+
+    home.configFile."sway/config".source = "${configDir}/sway/config";
+
+    systemd.user.targets."sway-session" = {
+      description = "sway compositor session";
+      documentation = [ "man:systemd.special(7)" ];
+      bindsTo = [ "graphical-session.target" ];
+      wants = [ "graphical-session-pre.target" ];
+      after = [ "graphical-session-pre.target" ];
+    };
+
+    services.xserver = {
+      enable = true;
+      displayManager.sessionPackages = [ package ];
+    };
+
+    security.pam.services.swaylock = { };
+    security.polkit.enable = true;
+    # For screen sharing (this option only has an effect with xdg.portal.enable):
+    xdg.portal.extraPortals = with pkgs; [ xdg-desktop-portal-wlr ];
+
+    modules.desktop.dconf.enable = true;
+    modules.desktop.fontconfig.enable = true;
+    modules.desktop.gtk.enable = true;
+    modules.desktop.qt.enable = true;
+    modules.desktop.apps.dunst.enable = true;
+    modules.desktop.apps.rofi.enable = true;
+  };
+}
