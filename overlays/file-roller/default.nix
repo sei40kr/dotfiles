ae5bf13dc0ac4bf2f965fb3768f76b938982b285
Seong Yong-ju <sei40kr@gmail.com>
2022-08-20 20:01:23 +0900

    feat(file-roller): use p7zip for unarchiving zip

---
+++ overlays/file-roller/default.nix
@@ -0,0 +1,12 @@
+_self: super:
+{
+  gnome = super.gnome // {
+    file-roller = super.gnome.file-roller.overrideAttrs (attrs: {
+      preFixup = ''
+        gappsWrapperArgs+=(
+          --prefix PATH : ${super.lib.makeBinPath [ super.p7zip ]}
+        )
+      '';
+    });
+  };
+}
