41b89fd3a332f96ab047d9619a3e03dfeb701a23
Seong Yong-ju <sei40kr@gmail.com>
2022-08-25 12:56:15 +0900

    feat(gnome): custom key bindings

--- nixos/modules/desktop/gnome.nix
+++ nixos/modules/desktop/gnome.nix
@@ -89,6 +89,7 @@ in
 
     user.packages = [
       pkgs.gnome.dconf-editor
+      pkgs.my.sensible-browser
       (mkIf (cfg.cursor.theme != null) cfg.cursor.theme.package)
       (mkIf (cfg.shell.theme != null) cfg.shell.theme.package)
     ] ++ exts;
@@ -114,17 +115,160 @@ in
           accel-profile = "flat";
           speed = 0.7;
         };
+        "org/gnome/desktop/wm/keybindings" = {
+          # Activate the window menu
+          activate-window-menu = [ ];
+          # Move window
+          begin-move = [ ];
+          # Resize window
+          begin-resize = [ "<Super>r" ];
+          # Close window
+          close = [ "<Shift><Super>q" ];
+          # Switch windows of an app directly
+          cycle-group = [ ];
+          cycle-group-backward = [ ];
+          # Switch system controls directly
+          cycle-panels = [ ];
+          cycle-panels-backward = [ ];
+          # Switch windows directly
+          cycle-windows = [ ];
+          cycle-windows-backward = [ ];
+          # Maximize window
+          maximize = [ "<Super>Up" "<Super>k" ];
+          # Minimize window
+          minimize = [ ];
+          # Move window one monitor down
+          move-to-monitor-down = [ "<Shift><Control><Super>Down" ];
+          # Move window one monitor left
+          move-to-monitor-left = [ "<Shift><Control><Super>Left" ];
+          # Move window one monitor right
+          move-to-monitor-right = [ "<Shift><Control><Super>Right" ];
+          # Move window one monitor up
+          move-to-monitor-up = [ "<Shift><Control><Super>Up" ];
+          # Move window to workspace 1
+          move-to-workspace-1 = [ "<Shift><Super>exclam" ];
+          # Move window to workspace 2
+          move-to-workspace-2 = [ "<Shift><Super>at" ];
+          # Move window to workspace 3
+          move-to-workspace-3 = [ "<Shift><Super>numbersign" ];
+          # Move window to workspace 4
+          move-to-workspace-4 = [ "<Shift><Super>dollar" ];
+          # Move window to workspace 5
+          move-to-workspace-5 = [ "<Shift><Super>percent" ];
+          # Move window to workspace 6
+          move-to-workspace-6 = [ "<Shift><Super>asciicircum" ];
+          # Move window to workspace 7
+          move-to-workspace-7 = [ "<Shift><Super>ampersand" ];
+          # Move window to workspace 8
+          move-to-workspace-8 = [ "<Shift><Super>asterisk" ];
+          # Move window to workspace 9
+          move-to-workspace-9 = [ "<Shift><Super>parenleft" ];
+          # Move window to workspace 10
+          move-to-workspace-10 = [ "<Shift><Super>parenright" ];
+          # Move window one workspace down
+          move-to-workspace-down = [ ];
+          # Move window to last workspace
+          move-to-workspace-last = [ ];
+          # Move window one workspace to the left
+          move-to-workspace-left = [ ];
+          # Move window one workspace to the right
+          move-to-workspace-right = [ ];
+          # Move window one workspace up
+          move-to-workspace-up = [ ];
+          # Show the run command prompt
+          panel-run-dialog = [ ];
+          # Switch applications
+          switch-applications = [ ];
+          switch-applications-backward = [ ];
+          # Switch to next input source
+          switch-input-source = [ ];
+          # Switch to previous input source
+          switch-input-source-backward = [ ];
+          # Switch system controls
+          switch-panels = [ ];
+          switch-panels-backward = [ ];
+          # Switch to workspace 1
+          switch-to-workspace-1 = [ "<Super>1" ];
+          # Switch to workspace 2
+          switch-to-workspace-2 = [ "<Super>2" ];
+          # Switch to workspace 3
+          switch-to-workspace-3 = [ "<Super>3" ];
+          # Switch to workspace 4
+          switch-to-workspace-4 = [ "<Super>4" ];
+          # Switch to workspace 5
+          switch-to-workspace-5 = [ "<Super>5" ];
+          # Switch to workspace 6
+          switch-to-workspace-6 = [ "<Super>6" ];
+          # Switch to workspace 7
+          switch-to-workspace-7 = [ "<Super>7" ];
+          # Switch to workspace 8
+          switch-to-workspace-8 = [ "<Super>8" ];
+          # Switch to workspace 9
+          switch-to-workspace-9 = [ "<Super>9" ];
+          # Switch to workspace 10
+          switch-to-workspace-10 = [ "<Super>0" ];
+          # Switch to workspace 11
+          switch-to-workspace-11 = [ "<Control><Super>1" ];
+          # Switch to workspace 12
+          switch-to-workspace-12 = [ "<Control><Super>2" ];
+          # Switch to workspace down
+          switch-to-workspace-down = [ ];
+          # Switch to last workspace
+          switch-to-workspace-last = [ ];
+          # Switch to workspace left
+          switch-to-workspace-left = [ "<Alt><Super>Left" ];
+          # Switch to workspace right
+          switch-to-workspace-right = [ "<Alt><Super>Right" ];
+          # Switch to workspace up
+          switch-to-workspace-up = [ ];
+          # Switch windows
+          switch-windows = [ "<Super>Tab" ];
+          switch-windows-backward = [ "<Shift><Super>Tab" ];
+          # Toggle fullscreen mode
+          toggle-fullscreen = [ "<Super>f" ];
+          # Toggle maximization state
+          toggle-maximized = [ ];
+          # Restore window
+          unmaximize = [ "<Super>Down" "<Super>j" ];
+        };
+        "org/gnome/mutter/keybindings" = {
+          # View split on left
+          toggle-tiled-left = [ "<Super>Left" "<Super>h" ];
+          # View split on right
+          toggle-tiled-right = [ "<Super>Right" "<Super>l" ];
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/help" = {
+          # Settings
+          control-center = [ "<Super>c" ];
+          # Eject
+          eject = [ "Eject" ];
+          # Launch help browser
+          help = [ ];
+          # Home folder
+          home = [ "<Shift><Super>n" ];
+          # Log out
+          logout = [ "<Shift><Super>e" ];
+          # Lock screen
+          screensaver = [ ];
+          # Search
+          search = [ "<Alt><Super>space" ];
+          # Volume down
+          volume-down = [ "AudioLowerVolume" ];
+          # Volume mute/unmute
+          volume-mute = [ "AudioMute" ];
+          # Volume up
+          volume-up = [ "AudioRaiseVolume" ];
+          # Launch web browser
+          www = [ "<Shift><Super>Return" ];
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          binding = "<Super>Return";
+          command = "${pkgs.my.sensible-terminal-wayland}/bin/sensible-terminal-wayland";
+          name = "Launch terminal";
+        };
         "org/gnome/settings-daemon/plugins/power" = {
           power-button-action = "suspend";
           sleep-inactive-ac-type = "nothing";
-        };
-        "org/gnome/desktop/wm/keybindings" = {
-          switch-applications = [ ];
-          switch-applications-backward = [ ];
-          switch-windows = [ "<Super>Tab" ];
-          switch-windows-backward = [ "<Shift><Super>Tab" ];
-        } // optionalAttrs config.modules.editors.emacs.doom.enable {
-          activate-window-menu = [ ];
         };
         "org/gnome/shell/extensions" = {
           disable-user-extensions = false;
@@ -135,8 +279,40 @@ in
         # };
         "org/gnome/shell/extensions/dash-to-dock" = {
           dock-fixed = true;
+          # Disable keyboard shortcuts to activate apps
+          hot-keys = false;
           # Hide "Show Applications" button
           show-show-apps-button = true;
+        };
+        "org/gnome/shell/keybindings" = {
+          # Focus the active notification
+          focus-active-notification = [ ];
+          # Open the application menu
+          open-application-menu = [ ];
+          # Switch to application 1
+          switch-to-application-1 = [ ];
+          # Switch to application 2
+          switch-to-application-2 = [ ];
+          # Switch to application 3
+          switch-to-application-3 = [ ];
+          # Switch to application 4
+          switch-to-application-4 = [ ];
+          # Switch to application 5
+          switch-to-application-5 = [ ];
+          # Switch to application 6
+          switch-to-application-6 = [ ];
+          # Switch to application 7
+          switch-to-application-7 = [ ];
+          # Switch to application 8
+          switch-to-application-8 = [ ];
+          # Switch to application 9
+          switch-to-application-9 = [ ];
+          # Show all applications
+          toggle-application-view = [ ];
+          # Show the notification list
+          toggle-message-tray = [ "<Super>n" ];
+          # Show the overview
+          toggle-overview = [ ];
         };
         "org/gnome/shell/window-switcher" = {
           current-workspace-only = false;
