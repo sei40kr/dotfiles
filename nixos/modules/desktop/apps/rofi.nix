044a714051bfa0371bea26bbe5ea03500d6d1017
Seong Yong-ju <sei40kr@gmail.com>
2022-09-11 21:03:17 +0900

    feat(rofi): configure keybinds & theme

--- nixos/modules/desktop/apps/rofi.nix
+++ nixos/modules/desktop/apps/rofi.nix
@@ -2,23 +2,27 @@
 
 with lib;
 with lib.my;
-let inherit (config.dotfiles) configDir;
-  cfg = config.modules.desktop.apps.rofi;
+let
+  inherit (config.dotfiles) configDir;
+  desktopCfg = config.modules.desktop;
+  cfg = desktopCfg.apps.rofi;
+  inherit (desktopCfg) fonts;
 in
 {
   options.modules.desktop.apps.rofi = {
     enable = mkBoolOpt false;
-
-    package = mkOption {
-      default = pkgs.rofi;
-      type = types.package;
-      visible = false;
-    };
   };
 
   config = mkIf cfg.enable {
-    user.packages = [ cfg.package ];
+    user.packages = with pkgs; [ rofi ];
 
-    environment.etc."rofi.rasi".source = "${configDir}/rofi/rofi.rasi";
+    home.configFile = {
+      "rofi/config.rasi".source = "${configDir}/rofi/rofi.rasi";
+      "rofi/themes/mytheme.rasi".source = pkgs.substituteAll {
+        src = ../../../../config/rofi/themes/mytheme.rasi;
+        fontName = fonts.ui.name;
+        fontSize = toString fonts.ui.size;
+      };
+    };
   };
 }
--- shells/default.nix
+++ shells/default.nix
@@ -1,6 +1,8 @@
 { pkgs }:
 
 {
+  rofi = pkgs.callPackage ./rofi.nix { };
+
   waybar = pkgs.callPackage ./waybar.nix { };
 
   yonmux = pkgs.callPackage ./yonmux.nix { };
