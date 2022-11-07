3acc1862065131de1d5232153d43fdf3f8903f56
Seong Yong-ju <sei40kr@gmail.com>
2022-11-07 20:13:28 +0900

    feat(qbittorrent-ee): update 4.4.3.10 -> 4.4.5.10

--- packages/qbittorrent-ee/default.nix
+++ packages/qbittorrent-ee/default.nix
@@ -1,23 +1,23 @@
 { lib, fetchFromGitHub, qbittorrent }:
 
 qbittorrent.overrideAttrs (attrs:
-  if attrs.version != "4.4.3" then
+  if attrs.version != "4.4.5" then
     throw "Unsupported qBittorrent version: ${attrs.version}"
   else {
     pname = "qbittorrent-ee";
-    version = "4.4.3.12";
+    version = "4.4.5.10";
 
     src = fetchFromGitHub {
       owner = "c0re100";
       repo = "qBittorrent-Enhanced-Edition";
-      rev = "release-4.4.3.12";
-      sha256 = "sha256-c2+QK+ieXIhh8/K04U3kCIqFXf5yce9xZelsnVMS3qk=";
+      rev = "release-4.4.5.10";
+      sha256 = "sha256-3kJEMLUThABAkDBbErrwEKz9bAg/Gd9/ZsWzmFmLLFg=";
     };
 
     meta = with lib; {
       description = "qBittorrent Enhanced, based on qBittorrent";
       homepage = "https://github.com/c0re100/qBittorrent-Enhanced-Edition";
-      changelog = "https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/release-4.4.3.12/Changelog";
+      changelog = "https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/release-4.4.5.10/Changelog";
       license = licenses.gpl2Plus;
       platforms = platforms.linux;
     };
