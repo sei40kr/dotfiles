0b6d2dae61cbb48954e277fa0878968d9651ca32
Seong Yong-ju <sei40kr@gmail.com>
2023-01-01 01:43:01 +0900

    whitesur: minor refactoring for whitesur-cursors

--- packages/whitesur-cursors/default.nix
+++ packages/whitesur-cursors/default.nix
@@ -1,15 +1,22 @@
-{ fetchFromGitHub }:
+{ fetchFromGitHub, lib }:
 
 fetchFromGitHub {
   owner = "vinceliuice";
   repo = "WhiteSur-cursors";
   rev = "2cb7219ba1ac6483219285a887061bfe516d813e";
-  sha256 = "sha256-/eUH6EIWqzwUgHw1ZV8mws9Kip7W4t8RkInd0K22Nv4=";
-  leaveDotGit = true;
+  hash = "sha256-/eUH6EIWqzwUgHw1ZV8mws9Kip7W4t8RkInd0K22Nv4=";
+
   postFetch = ''
     shopt -s extglob
     mkdir -p $out/share/icons
     cp -r $out/dist $out/share/icons/WhiteSur-cursors
-    rm -rf $out/{.git,!(share)}
+    rm -rf $out/!(share)
   '';
+
+  meta = with lib; {
+    description = "WhiteSur cursors theme for linux desktops";
+    homepage = "https://github.com/vinceliuice/WhiteSur-cursors";
+    license = licenses.gpl3Plus;
+    platforms = platforms.linux;
+  };
 }
