9a9f3568aeae6a63dfcd8c1eb4f400432437f5e6
Seong Yong-ju <sei40kr@gmail.com>
2023-01-02 00:50:04 +0900

    hosts: work-mbp

---
+++ darwin/hosts/work-mbp/default.nix
@@ -0,0 +1,48 @@
+{ lib, pkgs', ... }:
+
+with lib;
+let
+  system = "aarch64-darwin";
+  pkgs = pkgs'.${system};
+in
+{
+  inherit system;
+
+  stateVersion = "22.05";
+
+  modules.desktop.apps.alfred.enable = true;
+  modules.desktop.apps.rectangle.enable = true;
+  modules.desktop.apps.zoom.enable = true;
+
+  modules.dev.java.enable = true;
+  modules.dev.javascript.enable = true;
+  modules.dev.nix.enable = true;
+  modules.dev.rust.enable = true;
+  modules.dev.web.enable = true;
+
+  modules.editors.nvim.enable = true;
+  modules.editors.emacs = {
+    enable = true;
+    doom.enable = true;
+  };
+  modules.editors.idea.enable = true;
+  modules.editors.datagrip.enable = true;
+
+  modules.shell.zsh.enable = true;
+  modules.shell.tmux = {
+    enable = true;
+    autoRun = true;
+  };
+  modules.shell.git.enable = true;
+  modules.shell.ghq.enable = true;
+
+  modules.term.theme.active = "doom-one";
+  modules.term.font = {
+    package = pkgs.nerdfonts.override { fonts = [ "Iosevka" ]; };
+    name = "Iosevka Nerd Font";
+    size = 24;
+  };
+  modules.term.alacritty.enable = true;
+
+  user.name = "sei40kr";
+}
