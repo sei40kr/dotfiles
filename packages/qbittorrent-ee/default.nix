ac7ad4300918847fd14100f596f13893c2bbf159
Seong Yong-ju <sei40kr@gmail.com>
2023-02-23 01:17:21 +0900

    flake: bump nixpkgs
    NixOS/nixpkgs@9269a0e15385ce1a1b007b3c901499eb499549cc -> NixOS/nixpkgs@0c9aadc8eff6daaa5149d2df9e6c49baaf44161c

--- packages/qbittorrent-ee/default.nix
+++ packages/qbittorrent-ee/default.nix
@@ -1,23 +1,27 @@
 { lib, fetchFromGitHub, qbittorrent }:
 
+let
+  version = "4.5.0";
+  eeVersion = "4.5.0.10";
+in
 qbittorrent.overrideAttrs (attrs:
-  if attrs.version != "4.4.5" then
+  if attrs.version != version then
     throw "Unsupported qBittorrent version: ${attrs.version}"
   else {
     pname = "qbittorrent-ee";
-    version = "4.4.5.10";
+    version = eeVersion;
 
     src = fetchFromGitHub {
       owner = "c0re100";
       repo = "qBittorrent-Enhanced-Edition";
-      rev = "release-4.4.5.10";
-      sha256 = "sha256-3kJEMLUThABAkDBbErrwEKz9bAg/Gd9/ZsWzmFmLLFg=";
+      rev = "release-${eeVersion}";
+      hash = "sha256-sC1MklSypIpZ0LR4/NdpowABKIutfYkzUb39Kgty9Lc=";
     };
 
     meta = with lib; {
       description = "qBittorrent Enhanced, based on qBittorrent";
       homepage = "https://github.com/c0re100/qBittorrent-Enhanced-Edition";
-      changelog = "https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/release-4.4.5.10/Changelog";
+      changelog = "https://github.com/c0re100/qBittorrent-Enhanced-Edition/blob/release-${eeVersion}/Changelog";
       license = licenses.gpl2Plus;
       platforms = platforms.linux;
     };
