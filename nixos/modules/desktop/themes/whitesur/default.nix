cca65edab8a097f1a4063ea937782782ef8a709d
Seong Yong-ju <sei40kr@gmail.com>
2023-02-23 01:18:18 +0900

    whitesur: use alternative icons

--- nixos/modules/desktop/themes/whitesur/default.nix
+++ nixos/modules/desktop/themes/whitesur/default.nix
@@ -37,6 +37,7 @@ in
       };
       iconTheme = {
         package = pkgs.whitesur-icon-theme.override {
+          alternativeIcons = true;
           boldPanelIcons = true;
         };
         name = "WhiteSur";
