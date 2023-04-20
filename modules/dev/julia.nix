279737bdb10acef950df1459ee54f174124a54e5
Seong Yong-ju <sei40kr@gmail.com>
2023-04-20 22:15:56 +0900

    modules: add :dev/julia

---
+++ modules/dev/julia.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.julia;
+in
+{
+  options.modules.dev.julia = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ julia ];
+  };
+}
