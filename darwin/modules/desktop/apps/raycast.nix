744ce037b2e730d3d314402d069310aea6f2da64
Seong Yong-ju <sei40kr@gmail.com>
2023-04-25 17:15:01 +0900

    modules: add :desktop/apps/raycast

---
+++ darwin/modules/desktop/apps/raycast.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.raycast;
+in
+{
+  options.modules.desktop.apps.raycast = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ raycast ];
+  };
+}
