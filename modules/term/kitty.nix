70f4e1fc9447f3615e90ada43c17a888333bbeeb
Seong Yong-ju <sei40kr@gmail.com>
2023-05-19 01:23:47 +0900

    kitty: use different font for nerd symbols

--- modules/term/kitty.nix
+++ modules/term/kitty.nix
@@ -16,7 +16,10 @@ in
   };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ kitty ];
+    user.packages = with pkgs; [
+      kitty
+      (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; })
+    ];
 
     home.configFile."kitty/kitty.conf".text = ''
       confirm_os_window_close 0
@@ -24,6 +27,8 @@ in
       ## Fonts
       font_family       ${font.name}
       font_size         ${toString font.size}.0
+      # Nerd Fonts v2.3.3
+      symbol_map        U+23FB-U+23FE,U+2665,U+26A1,U+2B58,U+E000-U+E00A,U+E0A0-U+E0A3,U+E0B0-U+E0D4,U+E200-U+E2A9,U+E300-U+E3E3,U+E5FA-U+E6AA,U+E700-U+E7C5,U+EA60-U+EBEB,U+F000-U+F2E0,U+F300-U+F32F,U+F400-U+F4A9,U+F500-U+F8FF,U+F0001-U+F1AF0 Symbols Nerd Font Mono
       disable_ligatures always
 
       ## Cursor customization
