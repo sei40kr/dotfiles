633c8f4d2181b12419453cf66226a2e0a6c71b15
Seong Yong-ju <sei40kr@gmail.com>
2023-05-23 01:12:40 +0900

    modules: add :desktop/apps/just-focus for darwin

---
+++ darwin/modules/desktop/apps/just-focus.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.just-focus;
+in
+{
+  options.modules.desktop.apps.just-focus = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = {
+      enable = true;
+      masApps = { JustFocus = 1142151959; };
+    };
+  };
+}
