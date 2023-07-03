1ac5f17818d7eb53ff31094d918e3e9b244260c7
Seong Yong-ju <sei40kr@gmail.com>
2023-07-03 20:15:53 +0900

    modules: add :dev/tools/mutagen

---
+++ modules/dev/tools/mutagen.nix
@@ -0,0 +1,29 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  dockerCfg = config.modules.services.docker;
+  cfg = config.modules.dev.tools.mutagen;
+in
+{
+  options.modules.dev.tools.mutagen = {
+    enable = mkBoolOpt false;
+
+    compose.enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    assertions = [{
+      assertion = !cfg.compose.enable || pkgs.stdenv.isDarwin ||
+        (cfg.enable && cfg.compose.enable);
+      message = "Docker Compose should be enabled when using Mutagen Compose.";
+    }];
+
+    user.packages = with pkgs;
+      [
+        mutagen
+        (mkIf cfg.compose.enable mutagen-compose)
+      ];
+  };
+}
