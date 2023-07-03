098887784205101df65cb30111eb450bc9df7c1d
Seong Yong-ju <sei40kr@gmail.com>
2023-07-03 19:22:22 +0900

    modules: add :desktop/apps/bitwarden for darwin

---
+++ darwin/modules/desktop/apps/bitwarden.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.bitwarden;
+in
+{
+  options.modules.desktop.apps.bitwarden = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = {
+      enable = true;
+      casks = [ "bitwarden" ];
+    };
+  };
+}
