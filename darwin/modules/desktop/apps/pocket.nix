afc6b166f172c7eb6f80a61b26976a7e690eb03e
Seong Yong-ju <sei40kr@gmail.com>
2023-07-03 19:23:01 +0900

    modules: add :desktop/apps/pocket for darwin

---
+++ darwin/modules/desktop/apps/pocket.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.apps.pocket;
+in
+{
+  options.modules.desktop.apps.pocket = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    homebrew = {
+    enable=true;
+    masApps={Pocket = 568494494;};
+    };
+  };
+}
