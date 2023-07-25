2b11fa8f0568dbed48c30323e1740a1d369afaf2
Seong Yong-ju <sei40kr@gmail.com>
2023-07-25 15:10:08 +0900

    aws-cli: add options to install additional tools
    Add options to install additional AWS tools:
    - CloudFormation Linter
    - cfn_nag
    - Copilot CLI
    - SAM CLI

--- modules/dev/tools/aws-cli.nix
+++ modules/dev/tools/aws-cli.nix
@@ -7,9 +7,21 @@ in
 {
   options.modules.dev.tools.aws-cli = {
     enable = mkBoolOpt false;
+
+    cfn.enable = mkBoolOpt false;
+
+    copilot.enable = mkBoolOpt false;
+
+    sam.enable = mkBoolOpt false;
   };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [ awscli2 ];
+    user.packages = with pkgs; [
+      awscli2
+      (mkIf cfg.cfn.enable python3Packages.cfn-lint)
+      (mkIf cfg.cfn.enable cfn-nag)
+      (mkIf cfg.copilot.enable copilot-cli)
+      (mkIf cfg.sam.enable aws-sam-cli)
+    ];
   };
 }
