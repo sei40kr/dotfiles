dd38f5977c999c94483f245e7dd438bf19bad84a
Seong Yong-ju <sei40kr@gmail.com>
2023-08-26 16:54:59 +0900

    modules: add :desktop/apps/amphetamine for darwin

--- darwin/modules/desktop/apps/amphetamine.nix
+++ darwin/modules/desktop/apps/amphetamine.nix
@@ -8,12 +8,31 @@ in
 {
   options.modules.desktop.apps.amphetamine = {
     enable = mkBoolOpt false;
+
+    ehnancer.enable = mkBoolOpt true;
   };
 
   config = mkIf cfg.enable {
     homebrew = {
       enable = true;
       masApps.Amphetamine = 937984704;
+    };
+
+    launchd.user.agents = mkIf cfg.ehnancer.enable {
+      amphetamine-enhancer-cdmManager = {
+        serviceConfig = {
+          Label = "com.if.AmphetamineEnhancer-CDMManager";
+          ProgramArguments = [ "/bin/sh" "${pkgs.my.amphetamine-enhancer}/Applications/Amphetamine Enhancer.app/Contents/Resources/amphetamine-enhancer-cdmManager.sh" ];
+          StartInterval = 10;
+        };
+      };
+      amphetamine-enhancer-allProcesses = {
+        serviceConfig = {
+          Label = "com.if.AmphetamineHelper-AllProcesses";
+          ProgramArguments = [ "/bin/sh" "${pkgs.my.amphetamine-enhancer}/Applications/Amphetamine Enhancer.app/Contents/Resources/amphetamine-enhancer-allProcesses.sh" ];
+          StartInterval = 10;
+        };
+      };
     };
   };
 }
