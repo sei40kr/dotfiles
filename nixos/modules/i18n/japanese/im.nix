90ce14137db98050e7b876330042590523beabd5
Seong Yong-ju <sei40kr@gmail.com>
2023-11-05 14:53:32 +0900

    im: rewrite the whole module with i18n.inputMethod

--- nixos/modules/i18n/japanese/im.nix
+++ nixos/modules/i18n/japanese/im.nix
@@ -5,42 +5,17 @@ with lib.my;
 let
   cfg = config.modules.i18n.japanese;
 
-  package = pkgs.fcitx5-with-addons.override {
-    addons = with pkgs; [ fcitx5-mozc ];
-  };
-  gtk2-cache = pkgs.runCommand "gtk2-immodule.cache"
-    {
-      preferLocalBuild = true;
-      allowSubstitutes = false;
-      buildInputs = [ pkgs.gtk2 package ];
-    }
-    ''
-      mkdir -p $out/etc/gtk-2.0
-      GTK_PATH=${package}/lib/gtk-2.0 gtk-query-immodules-2.0 >$out/etc/gtk-2.0/immodules.cache
-    '';
-
-  gtk3-cache = pkgs.runCommand "gtk3-immodule.cache"
-    {
-      preferLocalBuild = true;
-      allowSubstitutes = false;
-      buildInputs = [ pkgs.gtk3 package ];
-    }
-    ''
-      mkdir -p $out/etc/gtk-3.0
-      GTK_PATH=${package}/lib/gtk-3.0 gtk-query-immodules-3.0 >$out/etc/gtk-3.0/immodules.cache
-    '';
+  fcitx5Package = config.i18n.inputMethod.package;
 in
 {
   config = mkIf (cfg.enable && config.modules.desktop.enable) {
-    user.packages = [ package gtk2-cache gtk3-cache ];
-
-    environment.variables = {
-      GTK_IM_MODULE = "fcitx";
-      QT_IM_MODULE = "fcitx";
-      XMODIFIERS = "@im=fcitx";
-      QT_PLUGIN_PATH = [ "${package}/${pkgs.qt6.qtbase.qtPluginPrefix}" ];
-      GLFW_IM_MODULE = "ibus";
+    i18n.inputMethod = {
+      enabled = "fcitx5";
+      fcitx5.addons = with pkgs; [ fcitx5-mozc ];
     };
+
+    # This still works because Fcitx5 can simulate IBus protocol
+    environment.variables.GLFW_IM_MODULE = "ibus";
 
     systemd.user.services.fcitx5-daemon = {
       description = "Fcitx5 input method editor";
@@ -49,7 +24,7 @@ in
       wantedBy = [ "autostart.target" ];
       aliases = [ "input-method.service" ];
       serviceConfig = {
-        ExecStart = "${package}/bin/fcitx5";
+        ExecStart = "${fcitx5Package}/bin/fcitx5";
       };
     };
   };
