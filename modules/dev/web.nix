9faab305a02a51bf5cf584b5936827e423c8c2b2
Seong Yong-ju <yongju.seong@codetakt.com>
2023-11-25 03:34:46 +0900

    web: make bun optional

--- modules/dev/web.nix
+++ modules/dev/web.nix
@@ -7,16 +7,23 @@ in
 {
   options.modules.dev.web = {
     enable = mkBoolOpt false;
+
+    bun.enable = mkOption {
+      type = types.bool;
+      default = false;
+      description = ''
+        Whether to install Bun.
+      '';
+    };
   };
 
   config = mkIf cfg.enable {
     # TODO stylelint-cli
     user.packages = with pkgs; [
       nodejs_20
-      bun
       nodePackages.vscode-html-languageserver-bin
       nodePackages.vscode-css-languageserver-bin
       nodePackages.vue-language-server
-    ];
+    ] ++ (if cfg.bun.enable then [ bun ] else [ ]);
   };
 }
