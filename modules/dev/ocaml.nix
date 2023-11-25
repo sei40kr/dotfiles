87bd9d3b569805797aa81bcd69652791fb8eda18
Seong Yong-ju <yongju.seong@codetakt.com>
2023-11-25 09:01:35 +0900

    ocaml: add utop

--- modules/dev/ocaml.nix
+++ modules/dev/ocaml.nix
@@ -21,7 +21,31 @@ in
       ocamlPackages.findlib
       dune_3
       ocamlformat
+      ocamlPackages.utop
       ocamlPackages.ocaml-lsp
     ];
+
+    # TODO: Extract these colors from a Neovim theme
+    home.configFile."utoprc".text = ''
+      profile:                dark
+      identifier.foreground:  none
+      module.foreground:      cyan
+      comment.foreground:     x-chocolate1
+      doc.foreground:         x-light-salmon
+      constant.foreground:    yellow
+      keyword.foreground:     magenta
+      symbol.foreground:      magenta
+      string.foreground:      green
+      char.foreground:        green
+      quotation.foreground:   cyan
+      error.foreground:       red
+      directive.foreground:   x-lightsteelblue
+      parenthesis.foreground: none
+    '';
+
+    home.configFile."utop/init.ml".text = ''
+      #utop_prompt_dummy;;
+      UTop.set_show_box false
+    '';
   };
 }
