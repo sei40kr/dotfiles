af8ccfac1a1ff5077d4d23aef80fb7965376315e
Seong Yong-ju <yongju.seong@codetakt.com>
2023-11-28 23:17:02 +0900

    modules: add :dev/tools/marp

---
+++ modules/dev/tools/marp.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.dev.tools.marp;
+in
+{
+  options.modules.dev.tools.marp = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    user.packages = with pkgs; [ marp-cli ];
+  };
+}
