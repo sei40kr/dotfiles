936a05f9b3a60d13b620d8b7758476dfa6742efa
Seong Yong-ju <sei40kr@gmail.com>
2023-12-06 04:37:01 +0900

    modules: add :desktop/hyprland for nixos systems

---
+++ nixos/modules/desktop/hyprland.nix
@@ -0,0 +1,16 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let
+  cfg = config.modules.desktop.hyprland;
+in
+{
+  options.modules.desktop.hyprland = {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    programs.hyprland.enable = true;
+  };
+}
