f30e1052a6568b94220d509b1e159b4bea2cfac8
Seong Yong-ju <sei40kr@gmail.com>
2023-12-09 14:02:59 +0900

    themes: remove material-design desktop theme

--- nixos/modules/desktop/themes/default.nix
+++ nixos/modules/desktop/themes/default.nix
@@ -3,6 +3,6 @@
 with lib;
 with lib.my; {
   options.modules.desktop.theme = {
-    active = mkOpt (types.enum [ "graphite" "material-design" "orchis" "whitesur" ]) null;
+    active = mkOpt (types.enum [ "graphite" "orchis" "whitesur" ]) null;
   };
 }
