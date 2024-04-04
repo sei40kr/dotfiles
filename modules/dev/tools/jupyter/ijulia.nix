fc1cee58d09e6adf71b8ebbad1a58a4d12cccf64
Seong Yong-ju <sei40kr@gmail.com>
2024-04-05 00:55:03 +0900

    jupyter: add ijulia kernel

---
+++ modules/dev/tools/jupyter/ijulia.nix
@@ -0,0 +1,32 @@
+{ lib, config, pkgs, ... }:
+
+let
+  inherit (builtins) readFile;
+  inherit (lib) mkIf;
+
+  juliaEnv = pkgs.unstable.julia-stable-bin.withPackages [ "IJulia" "Plots" ];
+  # Run a command to get the package directory of IJulia
+  ijulia = readFile (pkgs.runCommand "${juliaEnv.name}-ijulia-pkgdir"
+    {
+      buildInputs = [ juliaEnv ];
+    } ''
+    ${juliaEnv}/bin/julia -e 'using IJulia; print(pkgdir(IJulia))' >$out
+  '');
+in
+{
+  config = mkIf config.modules.dev.lang.julia.enable {
+    modules.dev.tools.jupyter.kernels.ijulia = {
+      displayName = "IJulia";
+      argv = [
+        "${juliaEnv}/bin/julia"
+        "-i"
+        "--color=yes"
+        "${ijulia}/src/kernel.jl"
+        "{connection_file}"
+      ];
+      language = "julia";
+      logo32 = "${ijulia}/deps/logo-32x32.png";
+      logo64 = "${ijulia}/deps/logo-64x64.png";
+    };
+  };
+}
