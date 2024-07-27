0c856b131cbd0194b877bfa486634871bc9ae8bf
Seong Yong-ju <sei40kr@gmail.com>
2024-07-28 00:38:19 +0900

    flake: bump yonvim
    sei40kr/yonvim@343afc4488f5ef716c90aa0e641e6dcd2798c6c7 -> sei40kr/yonvim@775210d6e50e3d624c46d96c8cbbc67be61fc9b1

--- modules/editors/nvim.nix
+++ modules/editors/nvim.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, inputs', pkgs, ... }:
 
 with lib;
 with lib.my;
@@ -6,7 +6,9 @@ let
   editorsCfg = config.modules.editors;
   cfg = editorsCfg.nvim;
 
-  package = pkgs.yonvim;
+  # NOTE: Yonvim requires Neovim 0.10.x
+  yonvim = pkgs.yonvim.override { inherit (pkgs.unstable) neovim-unwrapped; };
+  yonvim-qt = pkgs.yonvim-qt.override { inherit yonvim; };
 in
 {
   options.modules.editors.nvim = with types; {
@@ -17,15 +19,12 @@ in
   };
 
   config = mkIf cfg.enable {
-    user.packages = [
-      package
-      (pkgs.yonvim-qt.override { yonvim = package; })
-    ];
+    user.packages = [ yonvim yonvim-qt ];
 
     environment.variables = {
-      PAGER = mkIf cfg.pager.enable "${package}/bin/yonvim -c PAGER -";
-      MANPAGER = mkIf cfg.manpager.enable "${package}/bin/yonvim +'Man!'";
-      EDITOR = "${package}/bin/yonvim";
+      PAGER = mkIf cfg.pager.enable "${yonvim}/bin/yonvim -c PAGER -";
+      MANPAGER = mkIf cfg.manpager.enable "${yonvim}/bin/yonvim +'Man!'";
+      EDITOR = "${yonvim}/bin/yonvim";
     };
 
     modules.shell.aliases = { vim = "nvim"; };
