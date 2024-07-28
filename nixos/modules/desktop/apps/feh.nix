daf812bccda5b80b7e21f08d7603d286f2a7e357
Seong Yong-ju <sei40kr@gmail.com>
2024-07-28 14:50:41 +0900

    modules: add :desktop/apps/feh

---
+++ nixos/modules/desktop/apps/feh.nix
@@ -0,0 +1,37 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  desktopCfg = config.modules.desktop;
+  deCfg = desktopCfg.de;
+  cfg = desktopCfg.apps.feh;
+
+  bgMode =
+    if deCfg.background.image == null then null
+    else if deCfg.background.image.mode == "stretch" then "scale"
+    else if deCfg.background.image.mode == "fill" then "fill"
+    else if deCfg.background.image.mode == "fit" then "max"
+    else if deCfg.background.image.mode == "center" then "center"
+    else if deCfg.background.image.mode == "tile" then "tile"
+    else throw "Invalid background mode: ${deCfg.background.image.mode}";
+in
+{
+  options.modules.desktop.apps.feh = {
+    enable = mkEnableOption "Feh";
+  };
+
+  config = mkIf (cfg.enable && deCfg.background.image != null) {
+    environment.systemPackages = with pkgs; [ feh ];
+
+    systemd.user.services.feh = {
+      description = "Feh";
+      wantedBy = [ "graphical-session.target" ];
+      partOf = [ "graphical-session.target" ];
+      script = "${pkgs.feh}/bin/feh --bg-${bgMode} --no-fehbg ${deCfg.background.image.path}";
+      serviceConfig = {
+        type = "oneshot";
+        restart = "on-failure";
+      };
+    };
+  };
+}
