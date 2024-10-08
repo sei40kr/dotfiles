ca4e3b2c1e354d0cb8326a2034f14d9d145a440b
Seong Yong-ju <sei40kr@gmail.com>
2024-10-09 00:23:17 +0900

    feat(lean): add dev/lang/lean

---
+++ modules/dev/lang/lean.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.dev.lang.lean;
+in
+{
+  options.modules.dev.lang.lean = {
+    enable = mkEnableOption "Lean language support";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ lean4 ];
+  };
+}
