1a96e03d3f835e0b233fe27cf379137cb47653bd
Seong Yong-ju <sei40kr@gmail.com>
2024-10-19 13:08:53 +0900

    feat(yazi): add yazi

---
+++ modules/shell/yazi.nix
@@ -0,0 +1,20 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit (lib) mkEnableOption mkIf;
+  cfg = config.modules.shell.yazi;
+in
+{
+  options.modules.shell.yazi = {
+    enable = mkEnableOption "Yazi";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ yazi ];
+
+    home.configFile."yazi/yazi.toml".text = ''
+      [manager]
+      show_hidden = true
+    '';
+  };
+}
