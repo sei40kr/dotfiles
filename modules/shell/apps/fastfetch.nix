5dfcfe4264970e9c4cab1deca4e19a29813bfb62
Seong Yong-ju <sei40kr@gmail.com>
2024-10-26 16:57:16 +0900

    feat(fastfetch): use custom logo image

--- modules/shell/apps/fastfetch.nix
+++ modules/shell/apps/fastfetch.nix
@@ -1,8 +1,19 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 let
   inherit (lib) mkIf mkEnableOption;
+  inherit (config.dotfiles) configDir;
   cfg = config.modules.shell.apps.fastfetch;
+
+  config_jsonc = pkgs.substituteAll {
+    src = ../../../config/fastfetch/config.jsonc;
+    logo = "${configDir}/fastfetch/logo.png";
+  };
 in
 {
   options.modules.shell.apps.fastfetch = {
@@ -11,5 +22,7 @@ in
 
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ fastfetch ];
+
+    home.configFile."fastfetch/config.jsonc".source = config_jsonc;
   };
 }
