ac896baa335dda6098536da6af332ff14114abf5
Seong Yong-ju <sei40kr@gmail.com>
2024-11-22 12:44:51 +0900

    refactor(docker): use nixpkgs module

--- modules/services/docker.nix
+++ modules/services/docker.nix
@@ -5,57 +5,19 @@
   ...
 }:
 
-with lib;
-with lib.my;
 let
+  inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.services.docker;
 in
 {
   options.modules.services.docker = {
-    compose.enable = mkBoolOpt false;
+    compose.enable = mkEnableOption "Compose";
   };
 
   config = mkIf cfg.enable {
-    user.packages = with pkgs; [
-      docker
-      (mkIf cfg.compose.enable docker-compose)
-    ];
+    virtualisation.docker.enable = true;
 
-    users.groups.docker = { };
-
-    boot.kernelModules = [
-      "bridge"
-      "veth"
-    ];
-    boot.kernel.sysctl = {
-      "net.ipv4.conf.all.forwarding" = mkOverride 98 true;
-      "net.ipv4.conf.default.forwarding" = mkOverride 98 true;
-    };
-
-    systemd.services.docker = {
-      wantedBy = [ "multi-user.target" ];
-      after = [
-        "network.target"
-        "docker.socket"
-      ];
-      requires = [ "docker.socket" ];
-      serviceConfig = {
-        Type = "notify";
-        ExecStart = "${pkgs.docker}/bin/dockerd";
-        ExecReload = "${pkgs.procps}/bin/kill -s HUP $MAINPID";
-      };
-      path = [ pkgs.kmod ];
-    };
-    systemd.sockets.docker = {
-      description = "Docker Socket for the API";
-      wantedBy = [ "sockets.target" ];
-      socketConfig = {
-        ListenStream = "/run/docker.sock";
-        SocketMode = "0660";
-        SocketUser = "root";
-        SocketGroup = "docker";
-      };
-    };
+    environment.systemPackages = with pkgs; [ (mkIf cfg.compose.enable docker-compose) ];
 
     user.extraGroups = [ "docker" ];
   };
