f8c865dae4640869a7d5188919fe5f37681a245f
Seong Yong-ju <sei40kr@gmail.com>
2024-11-24 14:30:54 +0900

    build: bump home-manager
    nix-community/home-manager@f3a4512 -> nix-community/home-manager@f8831cc

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-24.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
