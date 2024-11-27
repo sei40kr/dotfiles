40f43a5987fd2a65840580fc2b42129b71deebaf
Seong Yong-ju <sei40kr@gmail.com>
2024-11-27 15:47:01 +0900

    feat(torrent): use open nvidia gpu driver

--- hosts/torrent/_hardware-configuration.nix
+++ hosts/torrent/_hardware-configuration.nix
@@ -46,4 +46,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.xserver.videoDrivers = [ "nvidia" ];
+  hardware.nvidia.open = true;
 }
