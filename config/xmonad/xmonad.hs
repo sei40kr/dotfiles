6cbb010b49a031c2bfb4f4166338cb9838916236
Seong Yong-ju <sei40kr@gmail.com>
2024-12-28 17:08:08 +0900

    feat(xmonad): add more key bindings

--- config/xmonad/xmonad.hs
+++ config/xmonad/xmonad.hs
@@ -1,8 +1,9 @@
 import Data.Map qualified as M
 import Data.Monoid (All)
+import System.Exit (exitSuccess)
 import XMonad
 import XMonad.Actions.CopyWindow (copy)
-import XMonad.Actions.CycleWS (nextWS, prevWS)
+import XMonad.Actions.CycleWS (nextWS, prevWS, shiftToNext, shiftToPrev)
 import XMonad.Actions.Submap (submap)
 import XMonad.Actions.ToggleFullFloat (toggleFullFloat, toggleFullFloatEwmhFullscreen)
 import XMonad.Config.Desktop
@@ -24,6 +25,7 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.TabBarDecoration (simpleTabBar)
 import XMonad.Layout.Tabbed (simpleTabbedAlways)
 import XMonad.Layout.ToggleLayouts (toggleLayouts)
+import XMonad.Prelude (find)
 import XMonad.StackSet qualified as W
 import XMonad.Util.EZConfig (additionalKeys, additionalKeysP)
 import XMonad.Util.Hacks (fixSteamFlicker)
@@ -44,6 +46,82 @@ myTerminal = "sensible-terminal"
 
 myModMask :: KeyMask
 myModMask = mod4Mask
+
+myKeys :: XConfig Layout -> M.Map (KeyMask, KeySym) (X ())
+myKeys conf@(XConfig{XMonad.modMask = myModMask}) =
+  M.fromList $
+    [ -- Launch - Application
+      ((myModMask, xK_space), spawn "rofi -modi drun -show drun")
+    , -- Launch - Browser
+      ((myModMask .|. shiftMask, xK_Return), spawn "sensible-browser")
+    , -- Launch - Terminal
+      ((myModMask, xK_Return), spawn myTerminal)
+    , -- Modify - Window Position
+      ((myModMask .|. shiftMask, xK_h), moveToGroupUp False)
+    , ((myModMask .|. shiftMask, xK_j), swapUp)
+    , ((myModMask .|. shiftMask, xK_k), swapDown)
+    , ((myModMask .|. shiftMask, xK_l), moveToGroupDown False)
+    , ((myModMask .|. shiftMask, xK_Left), moveToGroupUp False)
+    , ((myModMask .|. shiftMask, xK_Down), swapDown)
+    , ((myModMask .|. shiftMask, xK_Up), swapUp)
+    , ((myModMask .|. shiftMask, xK_Right), moveToGroupDown False)
+    , -- Modify - Containing Workspace
+      ((myModMask .|. controlMask .|. shiftMask, xK_Left), shiftToPrev)
+    , ((myModMask .|. controlMask .|. shiftMask, xK_Right), shiftToNext)
+    , -- Modify - Tile/Float Focus Toggle
+      ((myModMask .|. shiftMask, xK_t), toggleFocusFloating)
+    , -- Modify - Window Floating Toggle
+      ((myModMask .|. shiftMask, xK_f), toggleFloat)
+    , -- Modify - Window Fullscreen Toggle
+      ((myModMask, xK_f), withFocused toggleFullFloat)
+    , -- Navigate - Next Workspace
+      ((myModMask .|. mod1Mask, xK_Right), nextWS)
+    , ((myModMask, xK_Tab), nextWS)
+    , -- Navigate - Previous Workspace
+      ((myModMask .|. mod1Mask, xK_Left), prevWS)
+    , ((myModMask .|. shiftMask, xK_Tab), prevWS)
+    , -- Navigate - Relative Window
+      ((myModMask, xK_h), focusGroupUp)
+    , ((myModMask, xK_j), focusDown)
+    , ((myModMask, xK_k), focusUp)
+    , ((myModMask, xK_l), focusGroupDown)
+    , ((myModMask, xK_Left), focusGroupUp)
+    , ((myModMask, xK_Down), focusDown)
+    , ((myModMask, xK_Up), focusUp)
+    , ((myModMask, xK_Right), focusGroupDown)
+    , -- Session - Exit App
+      ((myModMask .|. shiftMask, xK_q), kill)
+    , -- Session - Logout
+      ((myModMask .|. shiftMask, xK_e), io exitSuccess)
+    , -- Session - Refresh Session
+      ((myModMask .|. shiftMask, xK_r), rescreen)
+    , -- Session - Restart XMonad
+      ((myModMask .|. controlMask, xK_r), restart "xmonad" True)
+    ]
+      ++ [ ((m .|. myModMask, k), windows $ f i)
+         | (i, k) <- zip myWorkspaces [xK_1 ..]
+         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask), (copy, controlMask)]
+         ]
+ where
+  toggleFloat = withFocused $ \w -> windows $ \s ->
+    if M.member w (W.floating s)
+      then W.sink w s
+      else W.float w (W.RationalRect (1 / 4) (1 / 4) (1 / 2) (1 / 2)) s
+  toggleFocusFloating = windows $ \s ->
+    case W.peek s of
+      Nothing -> s
+      Just w ->
+        let currentWindows = maybe [] W.integrate . W.stack . W.workspace . W.current $ s
+         in if M.member w (W.floating s)
+              then fromMaybe s $ do
+                t <- find (\w' -> not $ M.member w' (W.floating s)) currentWindows
+                return $ W.focusWindow t s
+              else fromMaybe s $ do
+                t <- find (\w' -> M.member w' (W.floating s)) currentWindows
+                return $ W.focusWindow t s
+   where
+    fromMaybe x Nothing = x
+    fromMaybe _ (Just x) = x
 
 myLogHook = fadeInactiveLogHook 0.9
 
@@ -93,38 +171,5 @@ main =
         , manageHook = myManageHook
         , handleEventHook = myHandleEventHook
         , focusFollowsMouse = False
+        , keys = myKeys
         }
-        `additionalKeys` [
-                           -- Exit app
-                           ((myModMask .|. shiftMask, xK_q), kill)
-                         , -- Restart XMonad
-                           ((myModMask .|. controlMask, xK_r), restart "xmonad" True)
-                         , -- Window Fullscreen Toggle
-                           ((myModMask, xK_f), withFocused toggleFullFloat)
-                         , -- Window Position
-                           ((myModMask, xK_h), focusGroupUp)
-                         , ((myModMask, xK_j), focusDown)
-                         , ((myModMask, xK_k), focusUp)
-                         , ((myModMask, xK_l), focusGroupDown)
-                         , ((myModMask, xK_Left), focusGroupUp)
-                         , ((myModMask, xK_Down), focusDown)
-                         , ((myModMask, xK_Up), focusUp)
-                         , ((myModMask, xK_Right), focusGroupDown)
-                         , ((myModMask, xK_Tab), nextWS)
-                         , ((myModMask .|. shiftMask, xK_Tab), prevWS)
-                         , ((myModMask .|. shiftMask, xK_h), moveToGroupUp False)
-                         , ((myModMask .|. shiftMask, xK_j), swapUp)
-                         , ((myModMask .|. shiftMask, xK_k), swapDown)
-                         , ((myModMask .|. shiftMask, xK_l), moveToGroupDown False)
-                         , ((myModMask .|. shiftMask, xK_Left), moveToGroupUp False)
-                         , ((myModMask .|. shiftMask, xK_Down), swapDown)
-                         , ((myModMask .|. shiftMask, xK_Up), swapUp)
-                         , ((myModMask .|. shiftMask, xK_Right), moveToGroupDown False)
-                         , ((myModMask, xK_space), spawn "rofi -modi drun -show drun")
-                         , ((myModMask .|. shiftMask, xK_Return), spawn myTerminal)
-                         , ((myModMask, xK_Return), spawn "sensible-terminal")
-                         ]
-        `additionalKeys` [ ((m .|. myModMask, k), windows $ f i)
-                         | (i, k) <- zip myWorkspaces [xK_1 ..]
-                         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask), (copy, controlMask)]
-                         ]
