01492b329b44f353c476c97388d686928ee57681
Seong Yong-ju <sei40kr@gmail.com>
2025-02-09 00:44:53 +0900

    feat(polybar): show gnome pomodoro timer on top-right corner

--- modules/desktop/apps/polybar.nix
+++ modules/desktop/apps/polybar.nix
@@ -6,8 +6,37 @@
 }:
 
 let
-  inherit (lib) generators mkEnableOption mkIf;
+  inherit (lib) makeBinPath mkEnableOption mkIf;
   cfg = config.modules.desktop.apps.polybar;
+
+  polybar-gnome-pomodoro =
+    pkgs.runCommand "polybar-gnome-pomodoro"
+      {
+        src = ../../../bin/polybar-gnome-pomodoro;
+        nativeBuildInputs = [
+          pkgs.gnused
+          pkgs.makeWrapper
+        ];
+        buildInputs = [ pkgs.jq ];
+      }
+      ''
+        mkdir -p $out/bin
+        cp $src $out/bin/polybar-gnome-pomodoro
+        sed -i '1,2c#!/usr/bin/env bash' $out/bin/polybar-gnome-pomodoro
+        patchShebangs $out
+        wrapProgram $out/bin/polybar-gnome-pomodoro \
+          --prefix PATH : ${
+            makeBinPath [
+              pkgs.my.gnome-pomodoro-watcher
+              pkgs.jq
+            ]
+          }
+      '';
+
+  configIni = pkgs.substituteAll {
+    src = ../../../config/polybar/config.ini;
+    polybar_gnome_pomodoro = "${polybar-gnome-pomodoro}/bin/polybar-gnome-pomodoro";
+  };
 in
 {
   options.modules.desktop.apps.polybar = {
@@ -15,7 +44,10 @@ in
   };
 
   config = mkIf cfg.enable {
-    environment.systemPackages = with pkgs; [ polybarFull ];
+    environment.systemPackages = with pkgs; [
+      polybarFull
+      my.gnome-pomodoro-watcher
+    ];
 
     fonts.packages = with pkgs; [ (nerdfonts.override { fonts = [ "NerdFontsSymbolsOnly" ]; }) ];
 
@@ -23,21 +55,17 @@ in
       description = "Polybar top bar as systemd service";
       wantedBy = [ "graphical-session.target" ];
       partOf = [ "graphical-session.target" ];
-      restartTriggers = [ ../../../config/polybar/config.ini ];
+      restartTriggers = [ configIni ];
       script = "${pkgs.polybarFull}/bin/polybar top";
     };
     systemd.user.services.polybar-bottom = {
       description = "Polybar bottom bar as systemd service";
       wantedBy = [ "graphical-session.target" ];
       partOf = [ "graphical-session.target" ];
-      restartTriggers = [ ../../../config/polybar/config.ini ];
+      restartTriggers = [ configIni ];
       script = "${pkgs.polybarFull}/bin/polybar bottom";
     };
 
-    environment.etc."xdg/polybar/config.ini".text = generators.toINIWithGlobalSection { } {
-      globalSection = {
-        include-file = "${../../../config/polybar/config.ini}";
-      };
-    };
+    environment.etc."xdg/polybar/config.ini".source = configIni;
   };
 }
