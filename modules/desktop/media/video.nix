d0ba37d305f608a71e62b9a6b8d8f2b4de907317
Seong Yong-ju <sei40kr@gmail.com>
2025-03-03 22:51:54 +0900

    feat(video): add handbrake

--- modules/desktop/media/video.nix
+++ modules/desktop/media/video.nix
@@ -13,9 +13,14 @@ in
 {
   options.modules.desktop.media.video = {
     vlc.enable = mkBoolOpt true;
+
+    handbrake.enable = mkEnableOption "HandBrake";
   };
 
   config = {
-    user.packages = with pkgs; [ (mkIf cfg.vlc.enable vlc) ];
+    user.packages = with pkgs; [
+      (mkIf cfg.vlc.enable vlc)
+      (mkIf cfg.handbrake.enable handbrake)
+    ];
   };
 }
