7950d9110d85b0a15ca8470eed2e554ae247810f
Seong Yong-ju <sei40kr@gmail.com>
2025-07-05 21:07:05 +0900

    feat(steam): disable dpms and screensaver during game mode

--- modules/desktop/apps/steam.nix
+++ modules/desktop/apps/steam.nix
@@ -8,6 +8,15 @@
 let
   inherit (lib) mkEnableOption mkIf;
   cfg = config.modules.desktop.apps.steam;
+
+  start = pkgs.writeShellScriptBin "start" ''
+    ${pkgs.xorg.xset}/bin/xset -dpms
+    ${pkgs.xorg.xset}/bin/xset s off
+  '';
+  end = pkgs.writeShellScriptBin "end" ''
+    ${pkgs.xorg.xset}/bin/xset +dpms
+    ${pkgs.xorg.xset}/bin/xset s on
+  '';
 in
 {
   options.modules.desktop.apps.steam = {
@@ -18,6 +27,17 @@ in
     programs.steam = {
       enable = true;
       extraCompatPackages = with pkgs; [ proton-ge-bin ];
+    };
+
+    programs.gamemode = {
+      enable = true;
+      settings = {
+        custom = {
+          start = "${start}/bin/start";
+          end = "${end}/bin/end";
+          timeout = 10;
+        };
+      };
     };
   };
 }
