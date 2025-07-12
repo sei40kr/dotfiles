367db9e579ecb3cc404ee7d2778a2296dffb9a15
Seong Yong-ju <sei40kr@gmail.com>
2025-07-12 17:37:27 +0900

    feat(torrent): disable split lock detection for gaming performance

--- hosts/torrent/_hardware-configuration.nix
+++ hosts/torrent/_hardware-configuration.nix
@@ -22,6 +22,7 @@
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelParams = [ "split_lock_detect=off" ];
   boot.extraModulePackages = [ ];
 
   fileSystems."/" = {
