63c9b5bc46a50b9b672a96508d751115d3a7b402
Seong Yong-ju <sei40kr@gmail.com>
2025-08-21 17:00:17 +0900

    feat(claude-code): add module

---
+++ modules/ai/claude-code.nix
@@ -0,0 +1,52 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    ;
+  cfg = config.modules.ai.claude-code;
+in
+{
+  options.modules.ai.claude-code = {
+    enable = mkEnableOption "Claude Code";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs; [ unstable.claude-code ];
+
+    home.file.".claude/settings.json".text = builtins.toJSON {
+      env = {
+        CLAUDE_CODE_IDE_SKIP_AUTO_INSTALL = 1;
+        DISABLE_AUTOUPDATER = 1;
+      };
+      hooks = {
+        Notification = [
+          {
+            hooks = [
+              {
+                type = "command";
+                command = "${pkgs.libnotify}/bin/notify-send 'Claude Code' 'Action required or input idle.'";
+              }
+            ];
+          }
+        ];
+        Stop = [
+          {
+            hooks = [
+              {
+                type = "command";
+                command = "${pkgs.libnotify}/bin/notify-send 'Claude Code' 'Response complete!'";
+              }
+            ];
+          }
+        ];
+      };
+    };
+  };
+}
