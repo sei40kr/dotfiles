a610a70e903b736cf7acee74682d8a3ce47821f5
Seong Yong-ju <sei40kr@gmail.com>
2025-10-14 00:05:31 +0900

    fix(jupyter): add missing kernel imports

--- modules/dev/tools/jupyter/default.nix
+++ modules/dev/tools/jupyter/default.nix
@@ -19,6 +19,12 @@ let
   kernels = pkgs.jupyter-kernel.create { definitions = cfg.kernels; };
 in
 {
+  imports = [
+    ./IRkernel.nix
+    ./ijulia.nix
+    ./ipykernel.nix
+  ];
+
   options.modules.dev.tools.jupyter = {
     enable = mkEnableOption "Jupyter";
 
@@ -32,7 +38,8 @@ in
   config = mkIf cfg.enable {
     user.packages = with pkgs; [ python3Packages.notebook ];
 
-    home.file.".jupyter/jupyter_console_config.py".source = "${configDir}/jupyter/jupyter_console_config.py";
+    home.file.".jupyter/jupyter_console_config.py".source =
+      "${configDir}/jupyter/jupyter_console_config.py";
     home.dataFile."jupyter/kernels".source = "${kernels}/kernels";
   };
 }
