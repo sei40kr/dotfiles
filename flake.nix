5e265e3b9851fdb6f78a7cb0df69c4c583bebdf2
Seong Yong-ju <sei40kr@gmail.com>
2025-11-01 16:31:08 +0900

    feat: use nixos-unstable channel for nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.05";
