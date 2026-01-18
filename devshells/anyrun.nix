4fb975034ce4755cf342c077e659665b32bfb015
Seong Yong-ju <sei40kr@gmail.com>
2026-01-18 16:50:09 +0900

    refactor(devshells): use home-manager generated config for anyrun devshell

--- devshells/anyrun.nix
+++ devshells/anyrun.nix
@@ -1,41 +1,61 @@
-{ pkgs, perSystem, ... }:
+{
+  pkgs,
+  perSystem,
+  inputs,
+  ...
+}:
 
 let
-  package = perSystem.anyrun.anyrun-with-all-plugins;
+  hmConfig = inputs.home-manager.lib.homeManagerConfiguration {
+    inherit pkgs;
+    modules = [
+      {
+        _module.args = {
+          inherit perSystem;
+        };
+      }
+      ../modules/home/anyrun
+      {
+        home = {
+          username = "dummy";
+          homeDirectory = "/tmp/dummy";
+          stateVersion = "25.11";
+        };
+
+        modules.desktop.apps.anyrun.enable = true;
+      }
+    ];
+  };
+
+  inherit (hmConfig.config.modules.desktop.apps.anyrun) package;
+  generatedConfigs = "${hmConfig.activationPackage}/home-files/.config/anyrun";
+
   anyrun-dev = pkgs.writeShellScriptBin "anyrun" ''
-    # Check if we're in the dotfiles root directory
     if [ ! -d "$PWD/modules/home/anyrun" ]; then
       echo "Error: modules/home/anyrun directory not found in $PWD"
       echo "Please run this command from the dotfiles root directory"
       exit 1
     fi
 
-    # Create temp config directory if it doesn't exist
     export ANYRUN_CONFIG_DIR
-    mkdir -p "$ANYRUN_CONFIG_DIR"
-
-    # Symlink style.css
+    rm -rf $ANYRUN_CONFIG_DIR/*
+    cp ${generatedConfigs}/*.ron $ANYRUN_CONFIG_DIR/
     ln -sf "$PWD/modules/home/anyrun/style.css" "$ANYRUN_CONFIG_DIR/style.css"
-
-    # Process config.ron (replace @package@ with anyrun path)
-    ${pkgs.gnused}/bin/sed "s|@package@|${package}|g" \
-      "$PWD/modules/home/anyrun/config.ron" > "$ANYRUN_CONFIG_DIR/config.ron"
-
-    # Copy applications.ron
-    cp "$PWD/modules/home/anyrun/applications.ron" "$ANYRUN_CONFIG_DIR/applications.ron"
 
     echo "Config synced to $ANYRUN_CONFIG_DIR"
 
-    # Run anyrun with dev config directory
-    exec ${package}/bin/anyrun -c "$ANYRUN_CONFIG_DIR" "$@"
+    ${package}/bin/anyrun -c $ANYRUN_CONFIG_DIR "$@"
   '';
 in
 pkgs.mkShell {
   buildInputs = [ anyrun-dev ];
 
   shellHook = ''
-    export ANYRUN_CONFIG_DIR="''${ANYRUN_CONFIG_DIR:-$(mktemp -d)}"
-    mkdir -p "$ANYRUN_CONFIG_DIR"
+    export ANYRUN_CONFIG_DIR="$(mktemp -d)"
+    mkdir -p $ANYRUN_CONFIG_DIR
+
+    # Cleanup temp directory when shell exits
+    trap "rm -rf $ANYRUN_CONFIG_DIR" EXIT
 
     echo "Anyrun development shell"
     echo "========================"
